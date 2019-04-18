26676699cf304ddb13b6729596e8f680eb71f513
Ryan Lopopolo <rjl@hyperbo.la>
2019-04-17 22:20:11 -0700

    Add macro for implementing TryFromMrb on nilable types

--- mruby/src/convert/array.rs
+++ mruby/src/convert/array.rs
@@ -1,27 +1,23 @@
 use crate::convert::fixnum::Int;
 
-// primitive ruby types
-mrb_vec_impl!(bool as array_of_bool);
-mrb_vec_impl!(Vec<bool> as array_of_array_of_bool);
+mrb_array_impl!(bool as array_of_bool);
+mrb_array_impl!(Option<bool> as array_of_nilable_bool);
+mrb_array_impl!(Vec<bool> as array_of_array_of_bool);
+mrb_array_impl!(Vec<Option<bool>> as array_of_array_of_nilable_bool);
 
-mrb_vec_impl!(Vec<u8> as array_of_bytes);
-mrb_vec_impl!(Vec<Vec<u8>> as array_of_array_of_bytes);
+mrb_array_impl!(Vec<u8> as array_of_byte_strings);
+mrb_array_impl!(Option<Vec<u8>> as array_of_nilable_byte_strings);
+mrb_array_impl!(Vec<Vec<u8>> as array_of_array_of_byte_strings);
+mrb_array_impl!(Vec<Option<Vec<u8>>> as array_of_array_of_nilable_byte_strings);
 
-mrb_vec_impl!(Int as array_of_fixnum);
-mrb_vec_impl!(Vec<Int> as array_of_array_of_fixnum);
+// TODO: array of float
 
-mrb_vec_impl!(String as array_of_string);
-mrb_vec_impl!(Vec<String> as array_of_array_of_string);
+mrb_array_impl!(Int as array_of_fixnum);
+mrb_array_impl!(Option<Int> as array_of_nilable_fixnum);
+mrb_array_impl!(Vec<Int> as array_of_array_of_fixnum);
+mrb_array_impl!(Vec<Option<Int>> as array_of_array_of_nilable_fixnum);
 
-// nilable types
-//mrb_vec_impl!(Option<bool> as array_of_nilable_bool);
-//mrb_vec_impl!(Vec<Option<bool>> as array_of_array_of_nilable_bool);
-
-//mrb_vec_impl!(Option<Vec<u8>> as array_of_nilable_bytes);
-//mrb_vec_impl!(Option<Vec<Vec<u8>>> as array_of_array_of_nilable_bytes);
-
-mrb_vec_impl!(Option<Int> as array_of_nilable_fixnum);
-mrb_vec_impl!(Vec<Option<Int>> as array_of_array_of_nilable_fixnum);
-
-//mrb_vec_impl!(Option<String> as array_of_nilable_string);
-//mrb_vec_impl!(Vec<Option<String>> as array_of_array_of_nilable_string);
+mrb_array_impl!(String as array_of_string);
+mrb_array_impl!(Option<String> as array_of_nilable_string);
+mrb_array_impl!(Vec<String> as array_of_array_of_string);
+mrb_array_impl!(Vec<Option<String>> as array_of_array_of_nilable_string);
--- mruby/src/convert/macros.rs
+++ mruby/src/convert/macros.rs
@@ -1,5 +1,5 @@
 #[macro_export]
-macro_rules! mrb_vec_impl {
+macro_rules! mrb_array_impl {
     ($type:ty as $wrapper_module:ident) => {
         #[allow(clippy::use_self)]
         impl $crate::TryFromMrb<std::vec::Vec<$type>> for $crate::Value {
@@ -8,7 +8,7 @@ macro_rules! mrb_vec_impl {
 
             unsafe fn try_from_mrb(
                 mrb: *mut $crate::sys::mrb_state,
-                value: Vec<$type>,
+                value: std::vec::Vec<$type>,
             ) -> std::result::Result<Self, $crate::convert::Error<Self::From, Self::To>> {
                 use std::convert::TryFrom;
                 let size = i64::try_from(value.len()).map_err(|_| $crate::convert::Error {
@@ -25,12 +25,12 @@ macro_rules! mrb_vec_impl {
                     let inner = ary_item.inner();
                     $crate::sys::mrb_ary_set(mrb, array, idx, inner);
                 }
-                Ok(Self::new(array))
+                std::result::Result::Ok(Self::new(array))
             }
         }
 
         #[allow(clippy::use_self)]
-        impl $crate::TryFromMrb<$crate::Value> for Vec<$type> {
+        impl $crate::TryFromMrb<$crate::Value> for std::vec::Vec<$type> {
             type From = $crate::Ruby;
             type To = $crate::Rust;
 
@@ -57,9 +57,9 @@ macro_rules! mrb_vec_impl {
                                 $crate::Value::new($crate::sys::mrb_ary_ref(mrb, inner, idx));
                             vec.push(<$type>::try_from_mrb(mrb, item)?);
                         }
-                        Ok(vec)
+                        std::result::Result::Ok(vec)
                     }
-                    type_tag => Err($crate::convert::Error {
+                    type_tag => std::result::Result::Err($crate::convert::Error {
                         from: type_tag,
                         to: $crate::Rust::Vec,
                     }),
@@ -69,68 +69,206 @@ macro_rules! mrb_vec_impl {
 
         #[cfg(test)]
         mod $wrapper_module {
-            use quickcheck_macros::quickcheck;
-            use std::convert::TryFrom;
+            mod tests {
+                use quickcheck_macros::quickcheck;
+                use std::convert::TryFrom;
 
-            use $crate::convert::*;
-            use $crate::value::*;
+                use $crate::convert::*;
+                use $crate::value::*;
 
-            #[test]
-            fn fail_covert() {
-                unsafe {
-                    let mrb = mrb_open();
-                    let value = Value::new(mrb_sys_true_value());
-                    let expected = Error {
-                        from: Ruby::Bool,
-                        to: Rust::Vec,
-                    };
-                    let result = <Vec<i64>>::try_from_mrb(mrb, value).map(|_| ());
-                    mrb_close(mrb);
-                    assert_eq!(result, Err(expected));
+                #[test]
+                fn fail_convert() {
+                    unsafe {
+                        let mrb = mrb_open();
+                        let value = Value::new(mrb_sys_true_value());
+                        let expected = Error {
+                            from: Ruby::Bool,
+                            to: Rust::Vec,
+                        };
+                        let result = <std::vec::Vec<$type>>::try_from_mrb(mrb, value).map(|_| ());
+                        mrb_close(mrb);
+                        assert_eq!(result, Err(expected));
+                    }
+                }
+
+                #[allow(clippy::needless_pass_by_value)]
+                #[quickcheck]
+                fn convert_to_value(v: std::vec::Vec<$type>) -> bool
+                where
+                    $type: std::clone::Clone
+                        + std::cmp::PartialEq
+                        + TryFromMrb<Value, From = Ruby, To = Rust>,
+                    Value: TryFromMrb<std::vec::Vec<$type>, From = Rust, To = Ruby>,
+                    std::vec::Vec<$type>:
+                        std::clone::Clone + TryFromMrb<Value, From = Ruby, To = Rust>,
+                {
+                    unsafe {
+                        let mrb = mrb_open();
+                        let value = match Value::try_from_mrb(mrb, v.clone()) {
+                            std::result::Result::Ok(value) => value,
+                            // we don't care about inner conversion failures for `T`
+                            std::result::Result::Err(_) => return true,
+                        };
+                        let inner = value.inner();
+                        let size = i64::try_from(v.len()).expect("vec size");
+                        let good = mrb_sys_ary_len(inner) == size;
+                        mrb_close(mrb);
+                        good
+                    }
+                }
+
+                #[allow(clippy::needless_pass_by_value)]
+                #[quickcheck]
+                fn roundtrip(v: std::vec::Vec<$type>) -> bool
+                where
+                    $type: std::clone::Clone
+                        + std::cmp::PartialEq
+                        + TryFromMrb<Value, From = Ruby, To = Rust>,
+                    Value: TryFromMrb<std::vec::Vec<$type>, From = Rust, To = Ruby>,
+                    std::vec::Vec<$type>:
+                        std::clone::Clone + TryFromMrb<Value, From = Ruby, To = Rust>,
+                {
+                    unsafe {
+                        let mrb = mrb_open();
+                        let value = match Value::try_from_mrb(mrb, v.clone()) {
+                            std::result::Result::Ok(value) => value,
+                            // we don't care about inner conversion failures for `T`
+                            std::result::Result::Err(_) => return true,
+                        };
+                        let good =
+                            <std::vec::Vec<$type>>::try_from_mrb(mrb, value).expect("convert") == v;
+                        mrb_close(mrb);
+                        good
+                    }
                 }
             }
+        }
+    };
+}
 
-            #[allow(clippy::needless_pass_by_value)]
-            #[quickcheck]
-            fn convert_to_value(v: Vec<$type>) -> bool
-            where
-                $type: Clone + PartialEq + TryFromMrb<Value, From = Ruby, To = Rust>,
-                Value: TryFromMrb<Vec<$type>, From = Rust, To = Ruby>,
-                Vec<$type>: Clone + TryFromMrb<Value, From = Ruby, To = Rust>,
-            {
-                unsafe {
-                    let mrb = mrb_open();
-                    let value = match Value::try_from_mrb(mrb, v.clone()) {
-                        Ok(value) => value,
-                        // we don't care about inner conversion failures for `T`
-                        Err(_) => return true,
-                    };
-                    let inner = value.inner();
-                    let size = i64::try_from(v.len()).expect("vec size");
-                    let good = mrb_sys_ary_len(inner) == size;
-                    mrb_close(mrb);
-                    good
+#[macro_export]
+macro_rules! mrb_nilable_impl {
+    ($type:ty as $wrapper_module:ident) => {
+        #[allow(clippy::use_self)]
+        impl $crate::TryFromMrb<std::option::Option<$type>> for $crate::Value {
+            type From = $crate::Rust;
+            type To = $crate::Ruby;
+
+            unsafe fn try_from_mrb(
+                mrb: *mut $crate::sys::mrb_state,
+                value: std::option::Option<$type>,
+            ) -> std::result::Result<Self, $crate::convert::Error<Self::From, Self::To>> {
+                match value {
+                    std::option::Option::Some(value) => Self::try_from_mrb(mrb, value),
+                    std::option::Option::None => {
+                        std::result::Result::Ok(Self::new($crate::sys::mrb_sys_nil_value()))
+                    }
                 }
             }
+        }
 
-            #[allow(clippy::needless_pass_by_value)]
-            #[quickcheck]
-            fn roundtrip(v: Vec<$type>) -> bool
-            where
-                $type: Clone + PartialEq + TryFromMrb<Value, From = Ruby, To = Rust>,
-                Value: TryFromMrb<Vec<$type>, From = Rust, To = Ruby>,
-                Vec<$type>: Clone + TryFromMrb<Value, From = Ruby, To = Rust>,
-            {
-                unsafe {
-                    let mrb = mrb_open();
-                    let value = match Value::try_from_mrb(mrb, v.clone()) {
-                        Ok(value) => value,
-                        // we don't care about inner conversion failures for `T`
-                        Err(_) => return true,
-                    };
-                    let good = <Vec<$type>>::try_from_mrb(mrb, value).expect("convert") == v;
-                    mrb_close(mrb);
-                    good
+        #[allow(clippy::use_self)]
+        impl $crate::TryFromMrb<$crate::Value> for std::option::Option<$type> {
+            type From = $crate::Ruby;
+            type To = $crate::Rust;
+
+            unsafe fn try_from_mrb(
+                mrb: *mut $crate::sys::mrb_state,
+                value: $crate::Value,
+            ) -> std::result::Result<Self, $crate::convert::Error<Self::From, Self::To>> {
+                match value.ruby_type() {
+                    $crate::Ruby::Nil => std::result::Result::Ok(None),
+                    _ => <$type>::try_from_mrb(mrb, value).map(std::option::Option::Some),
+                }
+            }
+        }
+
+        #[cfg(test)]
+        mod $wrapper_module {
+            mod tests {
+                use quickcheck_macros::quickcheck;
+
+                use $crate::convert::*;
+                use $crate::value::*;
+
+                #[test]
+                fn fail_convert() {
+                    unsafe {
+                        let mrb = mrb_open();
+                        let context = mrbc_context_new(mrb);
+                        // get a mrb_value that can't be converted to a
+                        // primitive type.
+                        let code = "Object.new";
+                        let value = mrb_load_nstring_cxt(
+                            mrb,
+                            code.as_ptr() as *const i8,
+                            code.len(),
+                            context,
+                        );
+                        let value = Value::new(value);
+                        let result =
+                            <std::option::Option<$type>>::try_from_mrb(mrb, value).map(|_| ());
+                        mrb_close(mrb);
+                        assert_eq!(
+                            result.map_err(|e| e.from),
+                            std::result::Result::Err(Ruby::Object)
+                        );
+                    }
+                }
+
+                #[allow(clippy::clone_on_copy)]
+                #[quickcheck]
+                fn convert_to_value(v: std::option::Option<$type>) -> bool
+                where
+                    $type: std::clone::Clone
+                        + std::cmp::PartialEq
+                        + TryFromMrb<Value, From = Ruby, To = Rust>,
+                    Value: TryFromMrb<std::option::Option<$type>, From = Rust, To = Ruby>,
+                    std::option::Option<$type>:
+                        std::clone::Clone + TryFromMrb<Value, From = Ruby, To = Rust>,
+                {
+                    unsafe {
+                        let mrb = mrb_open();
+                        let value = match Value::try_from_mrb(mrb, v.clone()) {
+                            std::result::Result::Ok(value) => value,
+                            // we don't care about inner conversion failures for `T`
+                            std::result::Result::Err(_) => return true,
+                        };
+                        let good = if let std::option::Option::Some(v) = v {
+                            <$type>::try_from_mrb(mrb, value).expect("convert") == v
+                        } else {
+                            let inner = value.inner();
+                            mrb_sys_value_is_nil(inner)
+                        };
+                        mrb_close(mrb);
+                        good
+                    }
+                }
+
+                #[allow(clippy::clone_on_copy)]
+                #[quickcheck]
+                fn roundtrip(v: std::option::Option<$type>) -> bool
+                where
+                    $type: std::clone::Clone
+                        + std::cmp::PartialEq
+                        + TryFromMrb<Value, From = Ruby, To = Rust>,
+                    Value: TryFromMrb<std::option::Option<$type>, From = Rust, To = Ruby>,
+                    std::option::Option<$type>:
+                        std::clone::Clone + TryFromMrb<Value, From = Ruby, To = Rust>,
+                {
+                    unsafe {
+                        let mrb = mrb_open();
+                        let value = match Value::try_from_mrb(mrb, v.clone()) {
+                            std::result::Result::Ok(value) => value,
+                            // we don't care about inner conversion failures for `T`
+                            std::result::Result::Err(_) => return true,
+                        };
+                        let good = <std::option::Option<$type>>::try_from_mrb(mrb, value)
+                            .expect("convert")
+                            == v;
+                        mrb_close(mrb);
+                        good
+                    }
                 }
             }
         }
