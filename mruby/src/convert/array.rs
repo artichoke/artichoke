44801db5346a1c9f221c998d865c04c012a1e134
Ryan Lopopolo <lopopolo@stripe.com>
2019-04-19 17:20:22 -0700

    rename test wrapper modules for nilable and array to be less verbose

--- mruby/src/convert/array.rs
+++ mruby/src/convert/array.rs
@@ -1,27 +1,27 @@
 use crate::convert::fixnum::Int;
 use crate::convert::float::Float;
 
-mrb_array_impl!(bool as array_of_bool);
-mrb_array_impl!(Option<bool> as array_of_nilable_bool);
-mrb_array_impl!(Vec<bool> as array_of_array_of_bool);
-mrb_array_impl!(Vec<Option<bool>> as array_of_array_of_nilable_bool);
+mrb_array_impl!(bool as bool);
+mrb_array_impl!(Option<bool> as nilable_bool);
+mrb_array_impl!(Vec<bool> as bool_array);
+mrb_array_impl!(Vec<Option<bool>> as nilable_bool_array);
 
-mrb_array_impl!(Vec<u8> as array_of_byte_strings);
-mrb_array_impl!(Option<Vec<u8>> as array_of_nilable_byte_strings);
-mrb_array_impl!(Vec<Vec<u8>> as array_of_array_of_byte_strings);
-mrb_array_impl!(Vec<Option<Vec<u8>>> as array_of_array_of_nilable_byte_strings);
+mrb_array_impl!(Vec<u8> as bytes);
+mrb_array_impl!(Option<Vec<u8>> as nilable_bytes);
+mrb_array_impl!(Vec<Vec<u8>> as bytes_array);
+mrb_array_impl!(Vec<Option<Vec<u8>>> as nilable_bytes_array);
 
-mrb_array_impl!(Float as array_of_float);
-mrb_array_impl!(Option<Float> as array_of_nilable_float);
-mrb_array_impl!(Vec<Float> as array_of_array_of_float);
-mrb_array_impl!(Vec<Option<Float>> as array_of_array_of_nilable_float);
+mrb_array_impl!(Float as float);
+mrb_array_impl!(Option<Float> as nilable_float);
+mrb_array_impl!(Vec<Float> as float_array);
+mrb_array_impl!(Vec<Option<Float>> as nilable_float_array);
 
-mrb_array_impl!(Int as array_of_fixnum);
-mrb_array_impl!(Option<Int> as array_of_nilable_fixnum);
-mrb_array_impl!(Vec<Int> as array_of_array_of_fixnum);
-mrb_array_impl!(Vec<Option<Int>> as array_of_array_of_nilable_fixnum);
+mrb_array_impl!(Int as fixnum);
+mrb_array_impl!(Option<Int> as nilable_fixnum);
+mrb_array_impl!(Vec<Int> as fixnum_array);
+mrb_array_impl!(Vec<Option<Int>> as nilable_fixnum_array);
 
-mrb_array_impl!(String as array_of_string);
-mrb_array_impl!(Option<String> as array_of_nilable_string);
-mrb_array_impl!(Vec<String> as array_of_array_of_string);
-mrb_array_impl!(Vec<Option<String>> as array_of_array_of_nilable_string);
+mrb_array_impl!(String as string);
+mrb_array_impl!(Option<String> as nilable_string);
+mrb_array_impl!(Vec<String> as string_array);
+mrb_array_impl!(Vec<Option<String>> as nilable_string_array);
--- mruby/src/convert/nilable.rs
+++ mruby/src/convert/nilable.rs
@@ -1,8 +1,10 @@
 use crate::convert::fixnum::Int;
 use crate::convert::float::Float;
 
-mrb_nilable_impl!(bool as nilable_bool);
-mrb_nilable_impl!(Vec<u8> as nilable_byte_string);
-mrb_nilable_impl!(Int as nilable_fixnum);
-mrb_nilable_impl!(Float as nilable_float with eq = |a: Float, b: Float| (a - b).abs() < std::f64::EPSILON);
-mrb_nilable_impl!(String as nilable_string);
+mrb_nilable_impl!(bool as bool);
+mrb_nilable_impl!(Vec<u8> as bytes);
+mrb_nilable_impl!(Int as fixnum);
+mrb_nilable_impl!(Float as float with eq = |a: Float, b: Float| {
+    (a - b).abs() < std::f64::EPSILON
+});
+mrb_nilable_impl!(String as string);
