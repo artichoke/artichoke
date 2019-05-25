0e261f8ff35524de12bb6040993aef36369ea59a
Ryan Lopopolo <rjl@hyperbo.la>
2019-05-25 16:52:39 -0700

    Add more complete docs on limitations of Request::to_env

--- nemesis/src/request.rs
+++ nemesis/src/request.rs
@@ -49,21 +49,30 @@ impl<'a> Request<'a> {
     pub fn to_env(&self, interp: &Mrb) -> Result<Value, Error> {
         // The keys in the environment hash are required by the Rack spec:
         // https://www.rubydoc.info/github/rack/rack/file/SPEC#label-The+Environment
+        //
+        // This implementation is incomplete (GH-61):
+        // TODO: Set SCRIPT_NAME from Rocket mount path.
+        // TODO: Set SERVER_NAME instead of hardcoding 'localhost'.
+        // TODO: Set SERVER_PORT instead of hardcoding it to 8000.
+        // TODO: Set HTTP_VERSION instead of hardcoding it to '1.1'.
+        // TODO: Set RACK_URL_SCHEME instead of hardcoding it to 'http'.
+        // TODO: Set RACK_INPUT and RACK_ERRORS once IO is implemented. See GH-9.
+        // TODO: RUN_ONCE should be true if in shared nothing execution mode
         interp
             .eval(format!(
                 r#"
                 {{
                     Rack::REQUEST_METHOD => '{method}',
-                    Rack::SCRIPT_NAME => '', # TODO: Rocket mount path
+                    Rack::SCRIPT_NAME => '',
                     Rack::PATH_INFO => '{path}',
                     Rack::QUERY_STRING => '{query}',
-                    Rack::SERVER_NAME => 'localhost', # TODO: set this correctly
-                    Rack::SERVER_PORT => 8000, # TODO: set this correctly
-                    Rack::HTTP_VERSION => '1.1', # TODO: set this correctly
+                    Rack::SERVER_NAME => 'localhost',
+                    Rack::SERVER_PORT => 8000,
+                    Rack::HTTP_VERSION => '1.1',
                     Rack::RACK_VERSION => Rack::VERSION,
-                    Rack::RACK_URL_SCHEME => 'http', # TODO: set this correctly
-                    Rack::RACK_INPUT => nil, # TODO: implement IO
-                    Rack::RACK_ERRORS => nil, # TODO: implement IO
+                    Rack::RACK_URL_SCHEME => 'http',
+                    Rack::RACK_INPUT => nil,
+                    Rack::RACK_ERRORS => nil,
                     Rack::RACK_MULTITHREAD => false,
                     Rack::RACK_MULTIPROCESS => false,
                     Rack::RACK_RUNONCE => false,
