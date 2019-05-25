15762264eb5dc21c5996e84336bb77952cff0ffe
Ryan Lopopolo <rjl@hyperbo.la>
2019-05-25 15:41:58 -0700

    Fixup comments in nemesis::init

--- nemesis/src/lib.rs
+++ nemesis/src/lib.rs
@@ -14,10 +14,15 @@ use rubygems::nemesis;
 pub fn init(interp: &Mrb) -> Result<(), MrbError> {
     rack::init(interp)?;
     nemesis::init(interp)?;
-    // TODO: properly implement Module#autoload and remove this hack to allow
-    // access to Rack constants defined in rack.rb.
+    // The Rack module makes heavy use of `Module#autoload` for dynamically
+    // resolving constants. For now, we don't care about this and manually
+    // require the bits of Rack we need. Create a stub implementation of
+    // `Module#autoload` that to allow the module to successfully eval on
+    // require.
+    //
+    // TODO: remove this stub once GH-12 is resolved.
     interp.eval(r#"class Module; def autoload(*args); end; end"#)?;
-    // Preload required gems
+    // Preload required gem sources
     interp.eval("require 'rack'")?;
     interp.eval("require 'rack/builder'")?;
     interp.eval("require 'nemesis/response'")?;
