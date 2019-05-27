0f9fda8b758f19023fb2d51d57afcc3243faa94f
Ryan Lopopolo <rjl@hyperbo.la>
2019-05-27 16:20:23 -0700

    Convert data leak test to Arg extractor

--- mruby/tests/leak_mrb_tt_data_rc.rs
+++ mruby/tests/leak_mrb_tt_data_rc.rs
@@ -15,15 +15,18 @@
 #[macro_use]
 extern crate mruby;
 
+use mruby::convert::TryFromMrb;
 use mruby::def::{rust_data_free, ClassLike, Define};
 use mruby::eval::MrbEval;
 use mruby::file::MrbFile;
-use mruby::interpreter::{Interpreter, Mrb};
+use mruby::interpreter::{Interpreter, Mrb, MrbApi};
 use mruby::load::MrbLoadSources;
 use mruby::sys;
+use mruby::value::Value;
 use mruby::MrbError;
 use std::cell::RefCell;
-use std::ffi::{c_void, CStr, CString};
+use std::ffi::c_void;
+use std::io::Write;
 use std::mem;
 use std::rc::Rc;
 
@@ -39,36 +42,50 @@ struct Container {
     inner: String,
 }
 
-impl MrbFile for Container {
-    fn require(interp: Mrb) -> Result<(), MrbError> {
-        extern "C" fn initialize(
-            mrb: *mut sys::mrb_state,
-            mut slf: sys::mrb_value,
-        ) -> sys::mrb_value {
-            unsafe {
-                let interp = interpreter_or_raise!(mrb);
+impl Container {
+    unsafe extern "C" fn initialize(
+        mrb: *mut sys::mrb_state,
+        mut slf: sys::mrb_value,
+    ) -> sys::mrb_value {
+        struct Args {
+            inner: String,
+        }
 
-                let string = mem::uninitialized::<*const std::os::raw::c_char>();
-                let argspec = CString::new(sys::specifiers::CSTRING).expect("argspec");
-                sys::mrb_get_args(mrb, argspec.as_ptr(), &string);
-                let string = CStr::from_ptr(string).to_string_lossy().to_string();
-                let cont = Container { inner: string };
-                let data = Rc::new(RefCell::new(cont));
-                let ptr = mem::transmute::<Rc<RefCell<Container>>, *mut c_void>(data);
-
-                let spec = class_spec_or_raise!(interp, Container);
-                let borrow = spec.borrow();
-                sys::mrb_sys_data_init(&mut slf, ptr, borrow.data_type());
-
-                slf
+        impl Args {
+            unsafe fn extract(interp: &Mrb) -> Result<Self, MrbError> {
+                let inner = mem::uninitialized::<sys::mrb_value>();
+                let mut argspec = vec![];
+                argspec
+                    .write_all(sys::specifiers::OBJECT.as_bytes())
+                    .map_err(|_| MrbError::ArgSpec)?;
+                argspec.write_all(b"\0").map_err(|_| MrbError::ArgSpec)?;
+                sys::mrb_get_args(interp.borrow().mrb, argspec.as_ptr() as *const i8, &inner);
+                let inner = Value::new(interp, inner);
+                let inner =
+                    String::try_from_mrb(&interp, inner).map_err(MrbError::ConvertToRust)?;
+                Ok(Self { inner })
             }
         }
 
+        let interp = interpreter_or_raise!(mrb);
+        let args = unwrap_or_raise!(interp, Args::extract(&interp), interp.nil().inner());
+
+        let container = Self { inner: args.inner };
+        let data = Rc::new(RefCell::new(container));
+        let ptr = mem::transmute::<Rc<RefCell<Self>>, *mut c_void>(data);
+        let spec = class_spec_or_raise!(interp, Self);
+        sys::mrb_sys_data_init(&mut slf, ptr, spec.borrow().data_type());
+
+        slf
+    }
+}
+impl MrbFile for Container {
+    fn require(interp: Mrb) -> Result<(), MrbError> {
         let spec = {
             let mut api = interp.borrow_mut();
             let spec = api.def_class::<Self>("Container", None, Some(rust_data_free::<Self>));
             spec.borrow_mut()
-                .add_method("initialize", initialize, sys::mrb_args_req(1));
+                .add_method("initialize", Self::initialize, sys::mrb_args_req(1));
             spec.borrow_mut().mrb_value_is_rust_backed(true);
             spec
         };
