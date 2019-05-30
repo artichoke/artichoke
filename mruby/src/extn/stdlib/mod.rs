d262f1a99a0325a48ab6322d9eedb750aeaa66b9
Ryan Lopopolo <rjl@hyperbo.la>
2019-05-30 13:34:10 -0700

    Thread is loaded by default

--- mruby/src/extn/stdlib/mod.rs
+++ mruby/src/extn/stdlib/mod.rs
@@ -1,3 +1,4 @@
+use crate::eval::MrbEval;
 use crate::interpreter::Mrb;
 use crate::MrbError;
 
@@ -7,5 +8,7 @@ pub mod thread;
 pub fn patch(interp: &Mrb) -> Result<(), MrbError> {
     monitor::init(interp)?;
     thread::init(interp)?;
+    // https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Lint/UnneededRequireStatement
+    interp.eval("require 'thread'")?;
     Ok(())
 }
