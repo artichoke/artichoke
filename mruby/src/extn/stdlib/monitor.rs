584e759d778a8ee432a54c4c0e4f43587df916c9
Ryan Lopopolo <rjl@hyperbo.la>
2019-05-31 00:11:03 -0700

    Add monitor test from ruby/spec

--- mruby/src/extn/stdlib/monitor.rs
+++ mruby/src/extn/stdlib/monitor.rs
@@ -10,3 +10,49 @@ pub fn init(interp: &Mrb) -> Result<(), MrbError> {
 }
 
 pub struct Monitor;
+
+// Monitor tests from ruby/spec
+// https://github.com/ruby/spec/tree/master/library/monitor
+#[cfg(test)]
+mod tests {
+    use crate::convert::TryFromMrb;
+    use crate::eval::MrbEval;
+    use crate::interpreter::Interpreter;
+
+    #[test]
+    fn mon_initialize() {
+        let spec = r#"
+cls = Class.new do
+  include MonitorMixin
+
+  def initialize(*array)
+    mon_initialize
+    @array = array
+  end
+
+  def to_a
+    synchronize { @array.dup }
+  end
+
+  def initialize_copy(other)
+    mon_initialize
+
+    synchronize do
+      @array = other.to_a
+    end
+  end
+end
+
+
+instance = cls.new(1, 2, 3)
+copy = instance.dup
+copy != instance
+# The below requires mspec
+# copy.should_not equal(instance)
+"#;
+        let interp = Interpreter::create().expect("mrb init");
+        interp.eval("require 'monitor'").expect("require");
+        let result = interp.eval(spec).expect("spec");
+        assert!(unsafe { bool::try_from_mrb(&interp, result) }.expect("convert"));
+    }
+}
