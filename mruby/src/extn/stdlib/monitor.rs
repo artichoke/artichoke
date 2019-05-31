311c85aef3f67421042520a818b7102c6f311449
Ryan Lopopolo <rjl@hyperbo.la>
2019-05-30 23:43:47 -0700

    inline include_str for extn Ruby sources

--- mruby/src/extn/stdlib/monitor.rs
+++ mruby/src/extn/stdlib/monitor.rs
@@ -2,8 +2,6 @@ use crate::interpreter::Mrb;
 use crate::load::MrbLoadSources;
 use crate::MrbError;
 
-const SRC: &str = include_str!("monitor.rb");
-
 pub fn init(interp: &Mrb) -> Result<(), MrbError> {
-    interp.def_rb_source_file("monitor.rb", SRC)
+    interp.def_rb_source_file("monitor.rb", include_str!("monitor.rb"))
 }
