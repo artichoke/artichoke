b0dafe521aacc1de209c879509e2d083fc290dc7
Ryan Lopopolo <rjl@hyperbo.la>
2019-05-30 23:45:23 -0700

    def_class for Monitor, Thread, Mutex

--- mruby/src/extn/stdlib/monitor.rs
+++ mruby/src/extn/stdlib/monitor.rs
@@ -3,5 +3,10 @@ use crate::load::MrbLoadSources;
 use crate::MrbError;
 
 pub fn init(interp: &Mrb) -> Result<(), MrbError> {
+    interp
+        .borrow_mut()
+        .def_class::<Monitor>("Monitor", None, None);
     interp.def_rb_source_file("monitor.rb", include_str!("monitor.rb"))
 }
+
+pub struct Monitor;
--- mruby/src/extn/stdlib/thread.rs
+++ mruby/src/extn/stdlib/thread.rs
@@ -3,5 +3,14 @@ use crate::load::MrbLoadSources;
 use crate::MrbError;
 
 pub fn init(interp: &Mrb) -> Result<(), MrbError> {
+    interp
+        .borrow_mut()
+        .def_class::<Thread>("Thread", None, None);
+    interp
+        .borrow_mut()
+        .def_class::<Mutex>("Mutex", None, None);
     interp.def_rb_source_file("thread.rb", include_str!("thread.rb"))
 }
+
+pub struct Thread;
+pub struct Mutex;
