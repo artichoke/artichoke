129e6e14e33db68e6cd1c48c79ff1187ae4cef0d
Ryan Lopopolo <rjl@hyperbo.la>
2019-05-31 00:13:11 -0700

    test that thread is loaded by default

--- mruby/src/extn/stdlib/thread.rs
+++ mruby/src/extn/stdlib/thread.rs
@@ -19,3 +19,17 @@ pub fn init(interp: &Mrb) -> Result<(), MrbError> {
 
 pub struct Thread;
 pub struct Mutex;
+
+#[cfg(test)]
+mod tests {
+    use crate::convert::TryFromMrb;
+    use crate::eval::MrbEval;
+    use crate::interpreter::Interpreter;
+
+    #[test]
+    fn thread_required_by_default() {
+        let interp = Interpreter::create().expect("mrb init");
+        let result = interp.eval("Object.const_defined?(:Thread)").expect("thread");
+        assert!(unsafe { bool::try_from_mrb(&interp, result) }.expect("convert"));
+    }
+}
