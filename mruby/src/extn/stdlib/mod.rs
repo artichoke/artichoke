71add756f2552a5ab5a5a4b29e40b76fb0ac067b
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-02 00:05:00 -0700

    Thread is part of Ruby core

--- mruby/src/extn/stdlib/mod.rs
+++ mruby/src/extn/stdlib/mod.rs
@@ -2,10 +2,8 @@ use crate::interpreter::Mrb;
 use crate::MrbError;
 
 pub mod monitor;
-pub mod thread;
 
 pub fn patch(interp: &Mrb) -> Result<(), MrbError> {
     monitor::init(interp)?;
-    thread::init(interp)?;
     Ok(())
 }
