581e2768647a37a1e491f516080158eadcc937fa
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-09 18:43:02 +0800

    Switch foolsgold structs to define C funcs as unsafe associated functions
    Instead of local functions in MrbFile::require.

--- foolsgold/src/foolsgold.rs
+++ foolsgold/src/foolsgold.rs
@@ -67,34 +67,32 @@ impl Gem for FoolsGold {
 
 struct Counter;
 
+impl Counter {
+    unsafe extern "C" fn get(mrb: *mut sys::mrb_state, _slf: sys::mrb_value) -> sys::mrb_value {
+        let interp = interpreter_or_raise!(mrb);
+
+        // We can probably relax the ordering constraint.
+        let value = SEEN_REQUESTS_COUNTER.load(Ordering::SeqCst);
+        unwrap_value_or_raise!(interp, Value::try_from_mrb(&interp, value))
+    }
+
+    unsafe extern "C" fn inc(mrb: *mut sys::mrb_state, _slf: sys::mrb_value) -> sys::mrb_value {
+        let interp = interpreter_or_raise!(mrb);
+        let total_requests = SEEN_REQUESTS_COUNTER.fetch_add(1, Ordering::SeqCst);
+        debug!(
+            "Logged request number {} in {}",
+            total_requests,
+            mrb.debug()
+        );
+        interp.nil().inner()
+    }
+}
+
 impl MrbFile for Counter {
     fn require(interp: Mrb) -> Result<(), MrbError> {
         // We do not need to define an initialize method since there is no need
         // to store any state on the `mrb_value`. The counter state is in a
         // static `AtomicI64`.
-
-        extern "C" fn get(mrb: *mut sys::mrb_state, _slf: sys::mrb_value) -> sys::mrb_value {
-            unsafe {
-                let interp = interpreter_or_raise!(mrb);
-
-                // We can probably relax the ordering constraint.
-                let value = SEEN_REQUESTS_COUNTER.load(Ordering::SeqCst);
-                unwrap_value_or_raise!(interp, Value::try_from_mrb(&interp, value))
-            }
-        }
-
-        extern "C" fn inc(mrb: *mut sys::mrb_state, _slf: sys::mrb_value) -> sys::mrb_value {
-            unsafe {
-                let total_requests = SEEN_REQUESTS_COUNTER.fetch_add(1, Ordering::SeqCst);
-                debug!(
-                    "Logged request number {} in {}",
-                    total_requests,
-                    mrb.debug()
-                );
-                sys::mrb_sys_nil_value()
-            }
-        }
-
         let spec = {
             let mut api = interp.borrow_mut();
             let scope = api
@@ -105,9 +103,9 @@ impl MrbFile for Counter {
             // any data in the `mrb_value`.
             let spec = api.def_class::<Self>("Counter", Some(scope), None);
             spec.borrow_mut()
-                .add_method("get", get, sys::mrb_args_none());
+                .add_method("get", Self::get, sys::mrb_args_none());
             spec.borrow_mut()
-                .add_method("inc", inc, sys::mrb_args_none());
+                .add_method("inc", Self::inc, sys::mrb_args_none());
             spec
         };
         spec.borrow().define(&interp)?;
@@ -136,49 +134,50 @@ struct RequestContext {
     trace_id: Uuid,
 }
 
+impl RequestContext {
+    unsafe extern "C" fn initialize(
+        mrb: *mut sys::mrb_state,
+        slf: sys::mrb_value,
+    ) -> sys::mrb_value {
+        let interp = interpreter_or_raise!(mrb);
+
+        let request_id = Uuid::new_v4();
+        let data = Self {
+            trace_id: request_id,
+        };
+        info!("initialized RequestContext with trace id {}", request_id);
+        unwrap_value_or_raise!(interp, data.try_into_ruby(&interp, Some(slf)))
+    }
+
+    unsafe extern "C" fn trace_id(mrb: *mut sys::mrb_state, slf: sys::mrb_value) -> sys::mrb_value {
+        let interp = interpreter_or_raise!(mrb);
+
+        let data = unwrap_or_raise!(
+            interp,
+            Self::try_from_ruby(&interp, &Value::new(&interp, slf)),
+            interp.nil().inner()
+        );
+        let trace_id = data.borrow().trace_id;
+        info!("Retrieved trace id {} in {:?}", trace_id, interp);
+        mem::forget(data);
+        unwrap_value_or_raise!(interp, Value::try_from_mrb(&interp, trace_id.to_string()))
+    }
+
+    unsafe extern "C" fn metrics(mrb: *mut sys::mrb_state, _slf: sys::mrb_value) -> sys::mrb_value {
+        let interp = interpreter_or_raise!(mrb);
+        let spec = module_spec_or_raise!(interp, Metrics);
+        let borrow = spec.borrow();
+        borrow
+            .value(&interp)
+            .unwrap_or_else(|| interp.nil())
+            .inner()
+    }
+}
+
 impl RustBackedValue for RequestContext {}
 
 impl MrbFile for RequestContext {
     fn require(interp: Mrb) -> Result<(), MrbError> {
-        extern "C" fn initialize(mrb: *mut sys::mrb_state, slf: sys::mrb_value) -> sys::mrb_value {
-            unsafe {
-                let interp = interpreter_or_raise!(mrb);
-
-                let request_id = Uuid::new_v4();
-                let data = RequestContext {
-                    trace_id: request_id,
-                };
-                info!("initialized RequestContext with trace id {}", request_id);
-                unwrap_value_or_raise!(interp, data.try_into_ruby(&interp, Some(slf)))
-            }
-        }
-
-        extern "C" fn trace_id(mrb: *mut sys::mrb_state, slf: sys::mrb_value) -> sys::mrb_value {
-            unsafe {
-                let interp = interpreter_or_raise!(mrb);
-
-                let data = unwrap_or_raise!(
-                    interp,
-                    RequestContext::try_from_ruby(&interp, &Value::new(&interp, slf)),
-                    interp.nil().inner()
-                );
-                let trace_id = data.borrow().trace_id;
-                info!("Retrieved trace id {} in {:?}", trace_id, interp);
-                mem::forget(data);
-                unwrap_value_or_raise!(interp, Value::try_from_mrb(&interp, trace_id.to_string()))
-            }
-        }
-
-        extern "C" fn metrics(mrb: *mut sys::mrb_state, _slf: sys::mrb_value) -> sys::mrb_value {
-            let interp = unsafe { interpreter_or_raise!(mrb) };
-            let spec = unsafe { module_spec_or_raise!(interp, Metrics) };
-            let borrow = spec.borrow();
-            borrow
-                .value(&interp)
-                .unwrap_or_else(|| interp.nil())
-                .inner()
-        }
-
         let spec = {
             let mut api = interp.borrow_mut();
             let scope = api
@@ -189,11 +188,11 @@ impl MrbFile for RequestContext {
                 api.def_class::<Self>("RequestContext", Some(scope), Some(rust_data_free::<Self>));
             spec.borrow_mut().mrb_value_is_rust_backed(true);
             spec.borrow_mut()
-                .add_method("initialize", initialize, sys::mrb_args_none());
+                .add_method("initialize", Self::initialize, sys::mrb_args_none());
             spec.borrow_mut()
-                .add_method("trace_id", trace_id, sys::mrb_args_none());
+                .add_method("trace_id", Self::trace_id, sys::mrb_args_none());
             spec.borrow_mut()
-                .add_method("metrics", metrics, sys::mrb_args_none());
+                .add_method("metrics", Self::metrics, sys::mrb_args_none());
             spec
         };
         spec.borrow().define(&interp)?;
