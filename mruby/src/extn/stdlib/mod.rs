69a9973882b13dff1fe3cff7c566224f30d67c17
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-11 22:55:22 +0800

    Set: load into mruby stdlib in Rust

--- mruby/src/extn/stdlib/mod.rs
+++ mruby/src/extn/stdlib/mod.rs
@@ -4,10 +4,12 @@ use crate::MrbError;
 pub mod delegate;
 pub mod forwardable;
 pub mod monitor;
+pub mod set;
 
 pub fn patch(interp: &Mrb) -> Result<(), MrbError> {
     delegate::init(interp)?;
     forwardable::init(interp)?;
     monitor::init(interp)?;
+    set::init(interp)?;
     Ok(())
 }
