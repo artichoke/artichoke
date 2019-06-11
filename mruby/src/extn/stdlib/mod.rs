988523a7631314e2afaf8b54ebfdbbe68c2ebfbb
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-12 01:10:37 +0800

    OpenStruct: load into mruby stdlib in Rust

--- mruby/src/extn/stdlib/mod.rs
+++ mruby/src/extn/stdlib/mod.rs
@@ -4,12 +4,14 @@ use crate::MrbError;
 pub mod delegate;
 pub mod forwardable;
 pub mod monitor;
+pub mod ostruct;
 pub mod set;
 
 pub fn patch(interp: &Mrb) -> Result<(), MrbError> {
     delegate::init(interp)?;
     forwardable::init(interp)?;
     monitor::init(interp)?;
+    ostruct::init(interp)?;
     set::init(interp)?;
     Ok(())
 }
