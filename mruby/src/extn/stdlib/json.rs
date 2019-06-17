88e088df9d2c10a40a15f26e78b64a3379df9825
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-17 17:39:21 +0100

    Import JSON sources

---
+++ mruby/src/extn/stdlib/json.rs
@@ -0,0 +1,17 @@
+use crate::interpreter::Mrb;
+use crate::load::MrbLoadSources;
+use crate::MrbError;
+
+pub fn init(interp: &Mrb) -> Result<(), MrbError> {
+    interp.def_rb_source_file("json.rb", include_str!("json.rb"))?;
+    interp.def_rb_source_file("json/common.rb", include_str!("json/common.rb"))?;
+    interp.def_rb_source_file("json/common.rb", include_str!("json/generic_object.rb"))?;
+    interp.def_rb_source_file("json/version.rb", include_str!("json/version.rb"))?;
+    interp.def_rb_source_file("json/pure.rb", include_str!("json/pure.rb"))?;
+    interp.def_rb_source_file(
+        "json/pure/generator.rb",
+        include_str!("json/pure/generator.rb"),
+    )?;
+    interp.def_rb_source_file("json/pure/parser.rb", include_str!("json/pure/parser.rb"))?;
+    Ok(())
+}
