02097ca0b6d74a29e79de682a6f151f601186cc5
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-17 17:39:35 +0100

    Add json implentation to stdlib in Rust

--- mruby/src/extn/stdlib/json.rs
+++ mruby/src/extn/stdlib/json.rs
@@ -5,7 +5,10 @@ use crate::MrbError;
 pub fn init(interp: &Mrb) -> Result<(), MrbError> {
     interp.def_rb_source_file("json.rb", include_str!("json.rb"))?;
     interp.def_rb_source_file("json/common.rb", include_str!("json/common.rb"))?;
-    interp.def_rb_source_file("json/common.rb", include_str!("json/generic_object.rb"))?;
+    interp.def_rb_source_file(
+        "json/generic_object.rb",
+        include_str!("json/generic_object.rb"),
+    )?;
     interp.def_rb_source_file("json/version.rb", include_str!("json/version.rb"))?;
     interp.def_rb_source_file("json/pure.rb", include_str!("json/pure.rb"))?;
     interp.def_rb_source_file(
