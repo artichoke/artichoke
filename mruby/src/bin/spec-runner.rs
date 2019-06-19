216d41d9ba9a0a14dfd6ae2049e88c656f0ba80f
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-19 23:33:56 +0100

    Suppress panics from MSpecRunner

--- mruby/src/bin/spec-runner.rs
+++ mruby/src/bin/spec-runner.rs
@@ -1,27 +1,32 @@
 use std::env;
 use std::fs;
+use std::panic;
+use std::process;
 
 use mruby::extn::test;
 use mruby::extn::test::mspec::MSpec;
 use mruby::interpreter::Interpreter;
 
 pub fn main() {
-    let interp = Interpreter::create().expect("mrb init");
-    test::init(&interp).expect("mspec init");
-    let mut mspec_runner = MSpec::runner(interp);
-
-    let mut args = env::args();
-    let mut specs = vec![];
-    // ignore binary name
-    args.next();
-    for spec in args {
-        let contents = fs::read_to_string(&spec).unwrap();
-        mspec_runner.add_spec(spec.as_str(), contents).unwrap();
-        specs.push(spec);
-    }
     // this will panic if a spec fails.
-    mspec_runner.run();
-    for spec in specs {
-        println!("OK {}", spec);
-    }
+    let result = panic::catch_unwind(|| {
+        let interp = Interpreter::create().expect("mrb init");
+        test::init(&interp).expect("mspec init");
+        let mut mspec_runner = MSpec::runner(interp);
+
+        let mut args = env::args();
+        let mut specs = vec![];
+        // ignore binary name
+        args.next();
+        for spec in args {
+            let contents = fs::read_to_string(&spec).unwrap();
+            mspec_runner.add_spec(spec.as_str(), contents).unwrap();
+            specs.push(spec);
+        }
+        mspec_runner.run()
+    });
+    process::exit(match result {
+        Ok(_) => 0,
+        Err(_) => 1,
+    });
 }
