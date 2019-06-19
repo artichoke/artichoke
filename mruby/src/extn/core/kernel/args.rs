b1c4f726f85eb70e8a07973a553535e420c9e90b
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-19 23:32:27 +0100

    Implement Kernel::print and Kernel::puts to use print! and println! macros to stdout

--- mruby/src/extn/core/kernel/args.rs
+++ mruby/src/extn/core/kernel/args.rs
@@ -25,3 +25,32 @@ impl Require {
         Ok(Self { filename })
     }
 }
+
+#[derive(Debug)]
+pub struct Rest {
+    pub rest: Vec<Value>,
+}
+
+impl Rest {
+    pub unsafe fn extract(interp: &Mrb) -> Result<Self, MrbError> {
+        let args = mem::uninitialized::<*const sys::mrb_value>();
+        let count = mem::uninitialized::<usize>();
+        let mut argspec = vec![];
+        argspec
+            .write_all(sys::specifiers::REST.as_bytes())
+            .map_err(|_| MrbError::ArgSpec)?;
+        argspec.write_all(b"\0").map_err(|_| MrbError::ArgSpec)?;
+        sys::mrb_get_args(
+            interp.borrow().mrb,
+            argspec.as_ptr() as *const i8,
+            &args,
+            &count,
+        );
+        let args = std::slice::from_raw_parts(args, count);
+        let args = args
+            .iter()
+            .map(|value| Value::new(&interp, *value))
+            .collect::<Vec<_>>();
+        Ok(Self { rest: args })
+    }
+}
