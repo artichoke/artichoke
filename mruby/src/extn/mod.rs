cc348f131369320040a3d6e1715de8b0df62d8d9
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-20 23:52:32 +0100

    Set global variable $/ input record separator

--- mruby/src/extn/mod.rs
+++ mruby/src/extn/mod.rs
@@ -7,6 +7,7 @@ pub mod stdlib;
 pub mod test;
 
 pub const RUBY_PLATFORM: &str = "x86_64-unknown-mruby";
+pub const INPUT_RECORD_SEPARATOR: &str = "\n";
 
 pub fn patch(interp: &Mrb) -> Result<(), MrbError> {
     unsafe {
@@ -21,6 +22,17 @@ pub fn patch(interp: &Mrb) -> Result<(), MrbError> {
             interp.borrow().mrb,
             b"RUBY_DESCRIPTION\0".as_ptr() as *const i8,
             ruby_description.inner(),
+        );
+        let global_input_record_separator = "$/";
+        let global_input_record_separator_name = sys::mrb_intern(
+            interp.borrow().mrb,
+            global_input_record_separator.as_ptr() as *const i8,
+            global_input_record_separator.len(),
+        );
+        sys::mrb_gv_set(
+            interp.borrow().mrb,
+            global_input_record_separator_name,
+            interp.string(INPUT_RECORD_SEPARATOR).inner(),
         );
     }
     core::patch(interp)?;
