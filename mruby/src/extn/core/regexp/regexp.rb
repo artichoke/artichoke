6162898e2dd615630b15d4df21b19ab6c3d28fbd
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-21 00:03:34 +0100

    Implement Regexp::try_convert

--- mruby/src/extn/core/regexp/regexp.rb
+++ mruby/src/extn/core/regexp/regexp.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Regexp
+  def self.try_convert(obj = nil)
+    raise ArgumentError if obj.nil?
+    return obj if obj.is_a?(Regexp)
+
+    obj.to_regexp
+  rescue StandardError
+    nil
+  end
+
   def ~
     self =~ $_ # rubocop:disable Style/SpecialGlobalVars
   end
