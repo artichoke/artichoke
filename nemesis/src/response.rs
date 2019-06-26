1cb2971418253c4e21a00b79c04cf73eef30e782
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-26 02:52:11 +0200

    Fixup borrowing in nemesis response

--- nemesis/src/response.rs
+++ nemesis/src/response.rs
@@ -33,9 +33,8 @@ impl Response {
             warn!("malformed rack response: {:?}", response);
             return Err(Error::RackResponse);
         }
-        let response = interp
-            .borrow()
-            .class_spec::<nemesis::Response>()
+        let spec = interp.borrow().class_spec::<nemesis::Response>();
+        let response = spec
             .and_then(|spec| spec.borrow().new_instance(interp, response))
             .ok_or_else(|| Error::Mrb(MrbError::NotDefined("Nemesis::Response".to_owned())))?;
         Ok(Self {
