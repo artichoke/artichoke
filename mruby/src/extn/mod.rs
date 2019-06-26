7e64e89e7a62a8cfc08c24b95e9ad32f6f8f441a
Ryan Lopopolo <rjl@hyperbo.la>
2019-06-26 02:50:45 +0200

    Intern global extn init symbol names

--- mruby/src/extn/mod.rs
+++ mruby/src/extn/mod.rs
@@ -9,29 +9,25 @@ pub const RUBY_PLATFORM: &str = "x86_64-unknown-mruby";
 pub const INPUT_RECORD_SEPARATOR: &str = "\n";
 
 pub fn patch(interp: &Mrb) -> Result<(), MrbError> {
+    let mrb = interp.borrow().mrb;
     unsafe {
         let ruby_platform = interp.string(RUBY_PLATFORM);
         sys::mrb_define_global_const(
-            interp.borrow().mrb,
+            mrb,
             b"RUBY_PLATFORM\0".as_ptr() as *const i8,
             ruby_platform.inner(),
         );
         let ruby_description = interp.string(sys::mruby_sys_version(true));
         sys::mrb_define_global_const(
-            interp.borrow().mrb,
+            mrb,
             b"RUBY_DESCRIPTION\0".as_ptr() as *const i8,
             ruby_description.inner(),
         );
-        let global_input_record_separator = "$/";
-        let global_input_record_separator_name = sys::mrb_intern(
-            interp.borrow().mrb,
-            global_input_record_separator.as_ptr() as *const i8,
-            global_input_record_separator.len(),
-        );
+        let input_record_separator = interp.string(INPUT_RECORD_SEPARATOR);
         sys::mrb_gv_set(
-            interp.borrow().mrb,
-            global_input_record_separator_name,
-            interp.string(INPUT_RECORD_SEPARATOR).inner(),
+            mrb,
+            interp.borrow_mut().sym_intern("$/"),
+            input_record_separator.inner(),
         );
     }
     core::patch(interp)?;
