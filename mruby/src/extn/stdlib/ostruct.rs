4e6fa0ba357469d64bcc71438d3885736a55d2ca
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-03 19:25:57 -0700

    Nuke Rust-implemented doc specs for Ruby stdlib packages
    These should be covered by ruby/spec or implemented in Ruby. See GH-142.

--- mruby/src/extn/stdlib/ostruct.rs
+++ mruby/src/extn/stdlib/ostruct.rs
@@ -11,33 +11,3 @@ pub fn init(interp: &Mrb) -> Result<(), MrbError> {
 }
 
 pub struct OpenStruct;
-
-// OpenStruct tests from Ruby stdlib docs
-// https://ruby-doc.org/stdlib-2.6.3/libdoc/ostruct/rdoc/OpenStruct.html
-#[cfg(test)]
-mod tests {
-    use crate::eval::MrbEval;
-    use crate::interpreter::{Interpreter, MrbApi};
-    use crate::value::{Value, ValueLike};
-
-    #[test]
-    fn ostruct() {
-        let interp = Interpreter::create().expect("mrb init");
-        interp.eval("require 'ostruct'").expect("require");
-        let person = interp.eval("person = OpenStruct.new").unwrap();
-        person
-            .funcall::<(), _, _>("name=", &[interp.string("John Smith")])
-            .unwrap();
-        person
-            .funcall::<(), _, _>("age=", &[interp.fixnum(70)])
-            .unwrap();
-        let name = person.funcall::<String, _, _>("name", &[]).unwrap();
-        assert_eq!(&name, "John Smith");
-        let age = person.funcall::<i64, _, _>("age", &[]).unwrap();
-        assert_eq!(age, 70);
-        let address = person
-            .funcall::<Option<Value>, _, _>("address", &[])
-            .unwrap();
-        assert!(address.is_none());
-    }
-}
