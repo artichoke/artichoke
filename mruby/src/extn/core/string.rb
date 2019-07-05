5a2150228d5f62dd541d4272b3230a26eb65192f
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-04 20:57:03 -0700

    Add missing encoding

--- mruby/src/extn/core/string.rb
+++ mruby/src/extn/core/string.rb
@@ -12,6 +12,7 @@ class Encoding
   US_ASCII = new('US-ASCII')
   ASCII = US_ASCII
   EUC_JP = new('EUC-JP')
+  ISO_8859_1 = new('ISO-8859-1')
   Shift_JIS = new('Shift_JIS')
   SHIFT_JIS = Shift_JIS
   UTF_8 = new('UTF-8')
