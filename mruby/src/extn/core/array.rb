6c75bf218284899ac68e64d02834053f39a02b87
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-06 18:49:16 -0700

    Fix recursion depth exceeded in Array#inspect

--- mruby/src/extn/core/array.rb
+++ mruby/src/extn/core/array.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 class Array
+  undef _inspect
+
+  def inspect
+    items = map do |item|
+      if object_id == item.object_id
+        '[...]'
+      else
+        item.inspect
+      end
+    end
+    "[#{items.join(', ')}]"
+  end
+
   def to_ary
     self
   end
