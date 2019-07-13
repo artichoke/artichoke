ef87cfd282f8ab00508209b20b149ba4f4e63ed5
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-13 11:00:19 -0700

    Inline the BFS into the cycle detection method
    Runtime for cycle detection is now O(links)

--- cactusref/src/cycle/mod.rs
+++ cactusref/src/cycle/mod.rs
@@ -50,48 +50,27 @@ pub(crate) fn reachable_links<T: ?Sized>(this: Link<T>) -> Links<T> {
     clique
 }
 
-pub(crate) fn graph_reachable_links<T: ?Sized>(this: Link<T>) -> Links<T> {
-    // Perform a breadth first search over all of the links to determine the
-    // clique of refs that self can reach.
-    let mut clique = Links::default();
-    clique.insert(this);
+pub(crate) fn cycle_refs<T: ?Sized>(this: Link<T>) -> HashMap<Link<T>, usize> {
+    // Perform a breadth first search over all of the forward and backward links
+    // to determine the clique of nodes in a cycle and their strong counts.
+    let mut cycle_owned_refs = HashMap::default();
+    cycle_owned_refs.insert(this, 0);
     loop {
-        let size = clique.len();
-        for item in clique.clone().iter() {
+        let size = cycle_owned_refs.len();
+        for item in cycle_owned_refs.clone().keys() {
             let links = unsafe { item.0.as_ref() }.links.borrow();
             for link in links.iter() {
-                clique.insert(*link);
+                *cycle_owned_refs.entry(*link).or_insert(0) += 1;
             }
             let links = unsafe { item.0.as_ref() }.back_links.borrow();
             for link in links.iter() {
-                clique.insert(*link);
+                cycle_owned_refs.entry(*link).or_insert(0);
             }
         }
         // BFS has found no new refs in the clique.
-        if size == clique.len() {
+        if size == cycle_owned_refs.len() {
             break;
         }
-    }
-    clique
-}
-
-pub(crate) fn cycle_refs<T: ?Sized>(this: Link<T>) -> HashMap<Link<T>, usize> {
-    // Iterate over the items in the clique. For each pair of nodes, find nodes
-    // that can reach each other. These nodes form a cycle.
-    let mut cycle_owned_refs = HashMap::default();
-    let clique = graph_reachable_links(this);
-    let reachable_by_node = clique
-        .iter()
-        .map(|link| (*link, reachable_links(*link).registry))
-        .collect::<HashMap<_, _>>();
-    for link in clique.iter() {
-        cycle_owned_refs.insert(
-            *link,
-            reachable_by_node
-                .get(link)
-                .map(|links| links.len())
-                .unwrap_or_default(),
-        );
     }
     cycle_owned_refs
 }
