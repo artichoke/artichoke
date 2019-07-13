0c87d766ba4544d2fb2f544b2f6c4fad3352e5bf
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-13 10:51:36 -0700

    use back refs

--- cactusref/src/cycle/mod.rs
+++ cactusref/src/cycle/mod.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use std::collections::HashMap;
 
 use crate::link::{Link, Links};
@@ -51,26 +50,48 @@ pub(crate) fn reachable_links<T: ?Sized>(this: Link<T>) -> Links<T> {
     clique
 }
 
+pub(crate) fn graph_reachable_links<T: ?Sized>(this: Link<T>) -> Links<T> {
+    // Perform a breadth first search over all of the links to determine the
+    // clique of refs that self can reach.
+    let mut clique = Links::default();
+    clique.insert(this);
+    loop {
+        let size = clique.len();
+        for item in clique.clone().iter() {
+            let links = unsafe { item.0.as_ref() }.links.borrow();
+            for link in links.iter() {
+                clique.insert(*link);
+            }
+            let links = unsafe { item.0.as_ref() }.back_links.borrow();
+            for link in links.iter() {
+                clique.insert(*link);
+            }
+        }
+        // BFS has found no new refs in the clique.
+        if size == clique.len() {
+            break;
+        }
+    }
+    clique
+}
+
 pub(crate) fn cycle_refs<T: ?Sized>(this: Link<T>) -> HashMap<Link<T>, usize> {
     // Iterate over the items in the clique. For each pair of nodes, find nodes
     // that can reach each other. These nodes form a cycle.
     let mut cycle_owned_refs = HashMap::default();
-    let clique = reachable_links(this);
-    let reachable = clique
+    let clique = graph_reachable_links(this);
+    let reachable_by_node = clique
         .iter()
-        .map(|link| (*link, reachable_links(*link)))
+        .map(|link| (*link, reachable_links(*link).registry))
         .collect::<HashMap<_, _>>();
-    for (left, right) in clique
-        .iter()
-        .cartesian_product(clique.iter())
-        .filter(|(left, right)| left != right)
-    {
-        let left_reaches_right = reachable[left].contains(right);
-        let right_reaches_left = reachable[right].contains(left);
-        if left_reaches_right && right_reaches_left {
-            let count = *cycle_owned_refs.entry(*right).or_insert(0);
-            cycle_owned_refs.insert(*right, count + 1);
-        }
+    for link in clique.iter() {
+        cycle_owned_refs.insert(
+            *link,
+            reachable_by_node
+                .get(link)
+                .map(|links| links.len())
+                .unwrap_or_default(),
+        );
     }
     cycle_owned_refs
 }
