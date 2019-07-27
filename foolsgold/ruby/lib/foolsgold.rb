907271d65af016160056b6d75d4a1aa4073d669c
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-27 11:31:28 -0700

    Remove crates that are not essential to implementing a Ruby
    - CactusRef lives in its own repo: https://github.com/artichoke/cactusref
    - Nemesis will be turned into a Gem (like thin)
    - FoolsGold is an application to be spun out of ferrocarril into its own repo
    - Hubris is an application to be spun out of ferrocarril into its own repo

--- foolsgold/ruby/lib/foolsgold.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module FoolsGold
-  VERSION = '0.0.1'
-  CONTEXT = 'FOOLSGOLD_REQUEST_CONTEXT'
-end
--- nemesis/src/rubygems.rs
+++
@@ -1,3 +0,0 @@
-//! [Ruby gems](mruby_gems) required by nemesis runtime.
-
-pub mod nemesis;
