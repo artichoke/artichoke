125783c1118b913b994bd72916ac9d3bc2f1c04e
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-27 18:47:44 -0700

    Remove foolsgold thin launcher

--- scripts/foolsgold-thin.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-set -x
-
-pushd foolsgold/ruby >/dev/null
-bundle install
-# without logging
-bundle exec thin -a 127.0.0.1 -p 9000 --threaded --threadpool-size 16 -R ../benches/config.ru start
