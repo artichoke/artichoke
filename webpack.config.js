232144f681c06c7aa7c2416a532e1e9622c67a25
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-28 23:24:55 -0700

    Add baseline webpack config

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: "[name].css",
+    chunkFilename: "[id].css"
+  }),
+  new HtmlWebPackPlugin({
+    template: "index.html",
+    filename: "index.html",
+    minify: {
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      removeComments: true,
+      useShortDoctype: true
+    }
+  }),
+  new HtmlWebpackInlineSourcePlugin()
+];
+
+module.exports = (env, argv) => {
+  let target = "debug";
+  let cssLoader = "style-loader";
+  if (argv.mode === "production") {
+    target = "release";
+    cssLoader = MiniCssExtractPlugin.loader;
+  }
+  return {
+    context: path.resolve(__dirname),
+    output: {
+      path: path.resolve(__dirname, `target/webpack/${target}`)
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [cssLoader, "css-loader"]
+        },
+        {
+          test: /\.(jpe?g|png|gif)$/,
+          use: ["url-loader", "image-webpack-loader"]
+        },
+        {
+          test: /\.svg$/,
+          use: ["svg-url-loader", "svgo-loader"]
+        },
+        {
+          test: /-wasm\.js$/,
+          use: ["uglify-loader", "file-loader"]
+        },
+        {
+          test: /\.wasm$/,
+          type: "javascript/auto",
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]"
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins,
+    optimization: {
+      minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()]
+    }
+  };
+};
