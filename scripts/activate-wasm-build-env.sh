ad7b55418da4a572e38aa41de3bde298beba9786
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-29 19:16:28 -0700

    Split emsdk setup into a sourceable shell script
    Similar to virtualenv/bin/activate. This avoids the macOS Java install
    popup for every build.

---
+++ scripts/activate-wasm-build-env.sh
@@ -0,0 +1,23 @@
+ensure_emsdk() {
+  yarn install >/dev/null 2>&1
+  # shellcheck disable=SC2091
+  cd "$($(yarn bin pkg-dir))" || return 1
+  if [ ! -f "target/emsdk/emsdk" ]; then
+    git clone https://github.com/emscripten-core/emsdk.git target/emsdk
+    ./target/emsdk/emsdk install latest
+  fi
+  ./target/emsdk/emsdk activate latest
+  cd - || return 1
+}
+
+clean_emsdk() {
+  yarn install >/dev/null 2>&1
+  # shellcheck disable=SC2091
+  cd "$($(yarn bin pkg-dir))" || return 1
+  rm -rf ./target/emsdk
+  cd - || return 1
+}
+
+ensure_emsdk
+# shellcheck disable=SC1091
+. target/emsdk/emsdk_env.sh
--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,41 +1,45 @@
 #!/usr/bin/env bash
 
+# This script requires sourcing scripts/activate-wasm-build-env.sh before
+# executing a build.
+
 set -euo pipefail
 
-yarn install &>/dev/null
 PATH="$(yarn bin):$PATH"
 export PATH
 cd "$(pkg-dir)"
 
 set -x
 
-ensure_emsdk() {
-  if [[ ! -f "target/emsdk/emsdk" ]]; then
-    git clone https://github.com/emscripten-core/emsdk.git target/emsdk
-    ./target/emsdk/emsdk install latest
-  fi
-  ./target/emsdk/emsdk activate latest
-}
+target="wasm32-unknown-emscripten"
+profile=""
+out="target/webpack/debug/"
+webpack_mode="development"
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  shift
+  case $key in
+    -t|--target)
+    target="$1"
+    shift
+    ;;
+    --release)
+    profile="$key"
+    out="target/webpack/release/"
+    webpack_mode="production"
+    ;;
+  esac
+done
 
 export CARGO_CFG_TARGET_FAMILY="wasm"
 # export CARGO_PROFILE_DEV_OPT_LEVEL="s"
 export CARGO_PROFILE_RELEASE_OPT_LEVEL="s"
 
-if [[ $# -gt 0 && $1 == '--development' ]]; then
-  ensure_emsdk
-  # shellcheck disable=SC1091
-  . target/emsdk/emsdk_env.sh
-  cargo build -Z config-profile --target wasm32-unknown-emscripten -p artichoke-wasm
-  yarn run webpack --mode development
-  yarn run webpack-dev-server --mode development --content-base target/webpack/debug/ --open
-elif [[ $# -gt 0 && $1 == '--production' ]]; then
-  ensure_emsdk
-  # shellcheck disable=SC1091
-  . target/emsdk/emsdk_env.sh
-  cargo build -Z config-profile --target wasm32-unknown-emscripten -p artichoke-wasm --release
-  yarn run webpack --mode production
-  yarn run webpack-dev-server --mode production --content-base target/webpack/release/ --open
+if [[ -z "$profile" ]]; then
+  cargo build -Z config-profile --target "$target" -p artichoke-wasm
 else
-  echo >&2 "Usage: $0 [ --development | --production ]"
-  exit 1
+  cargo build -Z config-profile --target "$target" -p artichoke-wasm "$profile"
 fi
+yarn run webpack --mode "$webpack_mode"
+yarn run webpack-dev-server --mode "$webpack_mode" --content-base "$out" --open
