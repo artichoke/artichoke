ca643fbc667db1a7915aa86796e4dd9285c4b688
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-30 00:49:18 -0700

    Prepopulate editor with some sample code

--- artichoke-wasm/src/playground.js
+++ artichoke-wasm/src/playground.js
@@ -5,8 +5,22 @@ import "ace-builds/webpack-resolver";
 import "artichoke-wasm/artichoke_wasm.wasm";
 import "artichoke-wasm/deps/artichoke-wasm.js";
 
+const sample = `
+class A
+  def bar(x)
+    x.to_s
+  end
+end
+
+10.times do |i|
+    puts A.new.bar(i)
+end
+`;
+
 ace.edit("editor", {
   mode: "ace/mode/ruby",
   theme: "ace/theme/monokai",
   fontSize: 14
 });
+
+ace.edit("editor").setValue(sample.trim(), -1);
