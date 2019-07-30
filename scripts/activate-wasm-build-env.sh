c824e23204cec8f578363ece3be2ef3cea1d6a7c
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-30 00:02:13 -0700

    Remove yarn from activate-wasm-build-env.sh so it can be used in CI

--- scripts/activate-wasm-build-env.sh
+++ scripts/activate-wasm-build-env.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
 ensure_emsdk() {
-  yarn install >/dev/null 2>&1
   # shellcheck disable=SC2091
-  cd "$($(yarn bin pkg-dir))" || return 1
   if [ ! -f "target/emsdk/emsdk" ]; then
     git clone https://github.com/emscripten-core/emsdk.git target/emsdk
     ./target/emsdk/emsdk install latest
   fi
   ./target/emsdk/emsdk activate latest
-  cd - || return 1
 }
 
 clean_emsdk() {
-  yarn install >/dev/null 2>&1
-  # shellcheck disable=SC2091
-  cd "$($(yarn bin pkg-dir))" || return 1
   rm -rf ./target/emsdk
-  cd - || return 1
 }
 
 ensure_emsdk
