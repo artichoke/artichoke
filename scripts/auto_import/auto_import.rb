40c538fb63b986a0d13a3112cad28034f9e9a217
John Wahba <john.a.wahba@gmail.com>
2019-08-03 15:30:54 -0700

    lint remove, generated file, add file generator to auto_import

--- scripts/auto_import/auto_import.rb
+++ scripts/auto_import/auto_import.rb
@@ -1,24 +1,26 @@
-#!ruby -w
+#!/usr/bin/env ruby
+# frozen_string_literal: true
 require 'erb'
 
 raise 'must provide a library to import' if ARGV[0].nil?
+raise 'must provide an output directory' if ARGV[1].nil?
+
 LIB = ARGV[0]
-RUBY = `which ruby`.strip
+OUT_DIR = ARGV[1]
+auto_import_dir = File.dirname(__FILE__)
 source_file = `gem which #{LIB}`.strip # used in erb
 filename = File.basename(source_file)
 rust_filename = filename.gsub(/.rb$/, '.rs')
 # Import the Ruby 2.6.3 sources.
-`cp #{source_file} ../../artichoke-backend/src/extn/stdlib/`
-constants = `#{RUBY} ./get_constants_loaded.rb "#{LIB}"`.split("\n")
+`cp #{source_file} #{OUT_DIR}`
+constants = `#{auto_import_dir}/get_constants_loaded.rb "#{LIB}"`.split("\n")
 
 # Add Rust glue, like this example for ostruct. Make a commit here.
-template = File.read('rust_glue.rs.erb')
+template = File.read("#{auto_import_dir}/rust_glue.rs.erb")
 renderer = ERB.new(template)
 output = renderer.result(binding)
-File.write("../../artichoke-backend/src/extn/stdlib/#{rust_filename}", output)
-
-
+File.write("#{OUT_DIR}#{rust_filename}", output)
 
 # Add test for spec compliance. Make a commit here.
 # Run spec compliance tests with yarn spec.
-# If the tests pass, 🎉. If they do not, there is likely a bug in Artichoke or an upstream bug in mruby. We can discuss in this issue how to proceed.
+# If the tests pass, great. If they do not, there is likely a bug in Artichoke or an upstream bug in mruby. We can discuss in this issue how to proceed.
--- scripts/auto_import/get_constants_loaded.rb
+++ scripts/auto_import/get_constants_loaded.rb
@@ -1,4 +1,8 @@
-#!ruby -w
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# The purpose of this script is to open a fresh interpreter, pull the constants,
+# require a library and figure out what constants were added.
 old_constants = Module.constants
 require ARGV[0]
 new_constants = Module.constants - old_constants
