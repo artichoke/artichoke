881da8da16568d631ac15e6e90374e39da1dec56
Ryan Lopopolo <rjl@hyperbo.la>
2019-08-11 16:49:15 -0700

    Artichoke Comparable passes ruby/spec

--- artichoke-backend/src/extn/core/comparable/comparable.rb
+++ artichoke-backend/src/extn/core/comparable/comparable.rb
@@ -2,56 +2,163 @@
 
 module Comparable
   def <(other)
-    return true if (self <=> other).negative?
+    cmp = (self <=> other)
+
+    if cmp.nil?
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+
+    return true if cmp.negative?
 
     false
   end
 
   def <=(other)
-    return false if (self <=> other).positive?
+    cmp = (self <=> other)
+
+    if cmp.nil?
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+
+    return false if cmp.positive?
 
     true
   end
 
   def ==(other)
-    return true if (self <=> other).zero?
+    return false unless respond_to?(:<=>)
 
+    cmp = (self <=> other)
+    return false if cmp.nil?
+
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+
+    return true if cmp.zero?
+
+    false
+  rescue NoMethodError
     false
   end
 
   def >(other)
-    return true if (self <=> other).positive?
+    cmp = (self <=> other)
+
+    if cmp.nil?
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+
+    return true if cmp.positive?
 
     false
   end
 
   def >=(other)
-    return false if (self <=> other).negative?
+    cmp = (self <=> other)
+
+    if cmp.nil?
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+
+    return false if cmp.negative?
 
     true
   end
 
   def between?(min, max)
-    return false if (min <=> max).positive?
+    cmp = (self <=> min)
+    if cmp.nil?
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
 
-    c = self <=> min
-    return false if c.negative?
+    return false if cmp.negative?
 
-    c = self <=> max
-    return false if c.positive?
+    cmp = (self <=> max)
+    if cmp.nil?
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+
+    return false if cmp.positive?
 
     true
   end
 
   def clamp(min, max)
-    raise ArgumentError, 'min argument must be smaller than max argument' if (min <=> max).positive?
+    paramcmp = (min <=> max)
+    raise ArgumentError, 'min argument must be smaller than max argument' if paramcmp.nil? || paramcmp > 0 # rubocop:disable Style/NumericPredicate
 
-    c = self <=> min
-    return self if c.zero?
-    return min if c.negative?
+    cmp = (self <=> min)
+    if cmp.nil?
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
 
-    c = self <=> max
-    return max if c.positive?
+    return min if cmp < 0 # rubocop:disable Style/NumericPredicate
+
+    cmp = (self <=> max)
+    if cmp.nil?
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+    unless cmp.is_a?(Numeric)
+      classname = other.class
+      classname = other.inspect if other.nil? || other.equal?(false) || other.equal?(true) || other.is_a?(Numeric)
+      raise ArgumentError, "Comparison of #{self.class} with #{classname} failed"
+    end
+
+    return max if cmp > 0 # rubocop:disable Style/NumericPredicate
 
     self
   end
