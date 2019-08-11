2fcaa8199ef79553371d63de0b1c3b8710c9f7ce
Ryan Lopopolo <rjl@hyperbo.la>
2019-08-10 22:33:54 -0700

    Refactor artichoke-backend build.rs
    - Use PathBuf instead of String concatenation.
    - Pass --disable-did_you_mean and --disable-gems to Ruby in external
      commands to ensure a clean interpeter with no loaded libs.
    - Support packages with multiple source files.
    - Build with URI patches.

--- artichoke-backend/build.rs
+++ artichoke-backend/build.rs
@@ -5,42 +5,48 @@
 use fs_extra::dir;
 use std::env;
 use std::fs;
+use std::path::PathBuf;
 use std::process::Command;
 
 /// Path helpers
 struct Build;
 
 impl Build {
-    fn root() -> String {
-        env::var("CARGO_MANIFEST_DIR").unwrap()
+    fn root() -> PathBuf {
+        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
     }
 
-    fn generated_dir() -> String {
-        format!("{}/src/generated", env::var("OUT_DIR").unwrap())
+    fn generated_dir() -> PathBuf {
+        PathBuf::from(env::var("OUT_DIR").unwrap())
+            .join("src")
+            .join("generated")
     }
 
-    fn ruby_source_dir() -> String {
-        format!("{}/ruby_2_6_3", env::var("OUT_DIR").unwrap())
+    fn ruby_source_dir() -> PathBuf {
+        PathBuf::from(env::var("OUT_DIR").unwrap()).join("ruby_2_6_3")
     }
 
-    fn ruby_vendored_dir() -> String {
-        format!("{}/vendor/ruby_2_6_3", &Build::root())
+    fn ruby_vendored_dir() -> PathBuf {
+        Build::root().join("vendor").join("ruby_2_6_3")
     }
 
-    fn ruby_vendored_lib_dir() -> String {
-        format!("{}/lib", &Build::ruby_vendored_dir())
+    fn ruby_vendored_lib_dir() -> PathBuf {
+        Build::ruby_vendored_dir().join("lib")
     }
 
-    fn patch(patch: &str) -> String {
-        format!("{}/vendor/{}", Build::root(), patch)
+    fn patch(patch: &str) -> PathBuf {
+        Build::root().join("vendor").join(patch)
     }
 
     fn get_package_files(package: &str) -> String {
+        let script = Build::root()
+            .join("scripts")
+            .join("auto_import")
+            .join("get_package_files.rb");
         let output = Command::new("ruby")
-            .arg(format!(
-                "{}/scripts/auto_import/get_package_files.rb",
-                Build::root()
-            ))
+            .arg("--disable-did_you_mean")
+            .arg("--disable-gems")
+            .arg(script)
             .arg(Build::ruby_vendored_lib_dir())
             .arg(package)
             .output()
@@ -55,17 +61,20 @@ impl Build {
         return String::from_utf8(output.stdout).unwrap();
     }
 
-    fn generated_package_out(package: &str) -> String {
-        format!("{}/{}.rs", Build::generated_dir(), package)
+    fn generated_package_out(package: &str) -> PathBuf {
+        Build::generated_dir().join(format!("{}.rs", package))
     }
 
     // The invoked Ruby script handles writing the output to disk
     fn generate_rust_glue(package: &str, sources: Vec<String>) {
+        let script = Build::root()
+            .join("scripts")
+            .join("auto_import")
+            .join("auto_import.rb");
         let output = Command::new("ruby")
-            .arg(format!(
-                "{}/scripts/auto_import/auto_import.rb",
-                Build::root()
-            ))
+            .arg("--disable-did_you_mean")
+            .arg("--disable-gems")
+            .arg(script)
             .arg(Build::ruby_vendored_lib_dir())
             .arg(&package)
             .arg(Build::generated_package_out(&package))
@@ -85,25 +94,34 @@ impl Build {
 fn main() {
     let _ = dir::remove(Build::ruby_source_dir());
     fs::create_dir_all(Build::generated_dir()).unwrap();
+    println!("{:?}", Build::generated_dir());
 
-    for package in vec!["benchmark"] {
+    for package in vec!["benchmark", "uri"] {
+        fs::create_dir_all(Build::generated_dir().join(package)).unwrap();
         let sources = Build::get_package_files(package)
             .trim()
             .split("\n")
             .map(String::from)
             .collect::<Vec<_>>();
-        for package_file in &sources {
-            let output_file = format!("{}/{}.rb", Build::generated_dir(), package);
-            fs::copy(package_file, &output_file).unwrap();
+        for source in &sources {
+            let package_source = PathBuf::from(source.to_owned());
+            let package_source = package_source.strip_prefix(Build::ruby_vendored_lib_dir());
+            let out = Build::generated_dir().join(package_source.unwrap());
+            fs::copy(source, &out).unwrap();
         }
         Build::generate_rust_glue(package, sources);
     }
-    println!("{}", Build::ruby_source_dir());
 
-    for patch in vec![] {
+    for patch in vec![
+        "00001-uri-brackets-for-ivar-interpolation.patch",
+        "00002-uri-defined-keyword.patch",
+    ] {
         if !Command::new("bash")
             .arg("-c")
-            .arg(format!("patch -p1 < '{}'", Build::patch(patch)))
+            .arg(format!(
+                "patch -p1 < '{}'",
+                Build::patch(patch).to_str().unwrap()
+            ))
             .current_dir(env::var("OUT_DIR").unwrap())
             .status()
             .unwrap()
