c40bc5a13994026ca86d876e64cffcb8f79599d7
cdeler <serj.krotov@gmail.com>
2019-08-13 19:11:04 +0300

    #156 added several Ruby based methods

--- artichoke-backend/src/extn/core/env/env.rb
+++ artichoke-backend/src/extn/core/env/env.rb
@@ -32,4 +32,65 @@ class EnvClass
         return self.to_h.empty?
     end
 
+    def has_key?(name)
+        ! self[name].nil?
+    end
+
+    def has_value?(value)
+        ! self.key(value).nil?
+    end
+
+    def include?(name)
+        self.has_key?(name)
+    end
+
+    def key(value)
+        result = nil
+
+        self.to_h.each do |var_name, var_value|
+            if var_value == value
+                result = var_name
+                break
+            end
+        end
+
+        result
+    end
+
+    def length
+        self.to_h.length
+    end
+
+    def size
+        self.length
+    end
+
+    def keys
+        self.to_h.map {|var_name, var_value| var_name}
+    end
+
+    def rehash
+        nil
+    end
+
+    def to_a
+        self.to_h.map {|var_name, var_value| [var_name, var_value]}
+    end
+
+    def to_s
+        'ENV'
+    end
+
+    def value?(name)
+        ! self.key(name).nil?
+    end
+
+    def values
+        self.to_h.map {|var_name, var_value| var_value}
+    end
+
+    def slice(*keys)
+        self.to_h.slice(*keys)
+    end
+
 end
