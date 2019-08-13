943bc7f011cbd54ad06e620c12f12aa4906de51e
cdeler <serj.krotov@gmail.com>
2019-08-13 16:16:31 +0300

    #156 added empty? , clear, assoc method. added block to delete method

--- artichoke-backend/src/extn/core/env/env.rb
+++ artichoke-backend/src/extn/core/env/env.rb
@@ -1,9 +1,35 @@
 # frozen_string_literal: true
 
 class EnvClass
+    def assoc(name)
+        value = self[name]
+
+        return value.nil? ? nil :  [name, self[name]]
+    end
+
+    def clear
+        self.to_h.each do |var_name, var_value|
+            if ! var_value.nil?
+                self[var_name] = nil
+            end
+        end
+
+        self.to_h
+    end
+
     def delete(name)
         value = self[name]
         self[name] = nil
+
+        if block_given? and value.nil?
+            yield name
+        end
+
         value
     end
+
+    def empty?
+        return self.to_h.empty?
+    end
+
 end
