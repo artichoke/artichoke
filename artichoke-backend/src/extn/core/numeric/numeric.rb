b2e5880c07f479d85dce5ab71ac7068687ccc8fd
KY <infcurious@gmail.com>
2019-08-22 09:25:02 +0800

    Lint fixes

--- artichoke-backend/src/extn/core/numeric/numeric.rb
+++ artichoke-backend/src/extn/core/numeric/numeric.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class Numeric
   def zero?
-    self == 0
+    self == 0 # rubocop:disable Style/NumericPredicate
   end
 
   def nonzero?
-    if self == 0
+    if self == 0 # rubocop:disable Style/NumericPredicate
       nil
     else
       self
@@ -12,10 +14,10 @@ class Numeric
   end
 
   def positive?
-    self > 0
+    self > 0 # rubocop:disable Style/NumericPredicate
   end
 
   def negative?
-    self < 0
+    self < 0 # rubocop:disable Style/NumericPredicate
   end
 end
