2a903f987d91e1e1a36e12969cf3d3c42ce30de0
KY <infcurious@gmail.com>
2019-08-27 16:21:18 +0800

    Impl. Integer#allbits?, Integer#anybits?, Integer#nobits?

--- artichoke-backend/src/extn/core/integer/integer.rb
+++ artichoke-backend/src/extn/core/integer/integer.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
 
 class Integer
+  def allbits?(mask)
+    mask = rb_to_int(mask)
+    self & mask == mask
+  end
+
+  def anybits?(mask)
+    mask = rb_to_int(mask)
+    !(self & mask).zero?
+  end
+
+  def nobits?(mask)
+    mask = rb_to_int(mask)
+    (self & mask).zero?
+  end
+
+  private
+
+  def rb_to_int(value)
+    unless value.respond_to?(:to_int)
+      value = case value
+              when true, false
+                value
+              else
+                value.class
+              end
+      raise TypeError, "no implicit conversion of #{value} into Integer"
+    end
+    value.to_int
+  end
 end
