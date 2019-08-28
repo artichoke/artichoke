f319dcd7e3357ff2ba086bfddd0f6601270e27d4
KY <infcurious@gmail.com>
2019-08-28 19:24:05 +0800

    Inline helper to not pollute Ruby class definition

--- artichoke-backend/src/extn/core/integer/integer.rb
+++ artichoke-backend/src/extn/core/integer/integer.rb
@@ -2,32 +2,35 @@
 
 class Integer
   def allbits?(mask)
-    mask = rb_to_int(mask)
-    self & mask == mask
+    if mask.respond_to?(:to_int)
+      mask = mask.to_int
+      return self & mask == mask
+    end
+
+    classname = mask.class
+    classname = mask.inspect if mask.nil? || mask.equal?(false) || mask.equal?(true)
+    raise TypeError, "no implicit conversion of #{classname} into #{self.class}"
   end
 
   def anybits?(mask)
-    mask = rb_to_int(mask)
-    !(self & mask).zero?
+    if mask.respond_to?(:to_int)
+      mask = mask.to_int
+      return !(self & mask).zero?
+    end
+
+    classname = mask.class
+    classname = mask.inspect if mask.nil? || mask.equal?(false) || mask.equal?(true)
+    raise TypeError, "no implicit conversion of #{classname} into #{self.class}"
   end
 
   def nobits?(mask)
-    mask = rb_to_int(mask)
-    (self & mask).zero?
-  end
-
-  private
-
-  def rb_to_int(value)
-    unless value.respond_to?(:to_int)
-      value = case value
-              when true, false
-                value
-              else
-                value.class
-              end
-      raise TypeError, "no implicit conversion of #{value} into Integer"
+    if mask.respond_to?(:to_int)
+      mask = mask.to_int
+      return (self & mask).zero?
     end
-    value.to_int
+
+    classname = mask.class
+    classname = mask.inspect if mask.nil? || mask.equal?(false) || mask.equal?(true)
+    raise TypeError, "no implicit conversion of #{classname} into #{self.class}"
   end
 end
