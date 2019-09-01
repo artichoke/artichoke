15ba611a4b8e3c71223d771ced47644f242d3f58
cdeler <serj.krotov@gmail.com>
2019-09-01 14:23:22 +0300

    #156 fixed rubocop remarks

--- artichoke-backend/src/extn/core/env/env.rb
+++ artichoke-backend/src/extn/core/env/env.rb
@@ -1,121 +1,119 @@
 # frozen_string_literal: true
 
 class EnvClass
-    def assoc(name)
-        value = self[name]
+  def assoc(name)
+    value = self[name]
 
-        return value.nil? ? nil :  [name, self[name]]
+    value.nil? ? nil : [name, self[name]]
+  end
+
+  def clear
+    to_h.each do |var_name, var_value|
+      self[var_name] = nil unless var_value.nil?
     end
 
-    def clear
-        self.to_h.each do |var_name, var_value|
-            if ! var_value.nil?
-                self[var_name] = nil
-            end
-        end
+    to_h
+  end
 
-        self.to_h
+  def delete(name)
+    value = self[name]
+    return nil if value.nil?
+
+    self[name] = nil
+    yield name if block_given?
+
+    value
+  end
+
+  def delete_if
+    return to_enum(:delete_if) unless block_given?
+
+    each do |key, value|
+      delete(key) if yield key, value
+    end
+    to_h
+  end
+
+  def empty?
+    to_h.empty?
+  end
+
+  def has_key?(name) # rubocop:disable PredicateName
+    to_h.key?(name)
+  end
+
+  def has_value?(value) # rubocop:disable PredicateName
+    to_h.value?(value)
+  end
+
+  def include?(name)
+    to_h.key?(name)
+  end
+
+  def key(value)
+    to_h.key(value)
+  end
+
+  def length
+    to_h.length
+  end
+
+  def size
+    to_h.size
+  end
+
+  def keys
+    to_h.keys
+  end
+
+  def rehash
+    nil
+  end
+
+  def to_a
+    to_h.to_a
+  end
+
+  def to_s
+    'ENV'
+  end
+
+  def value?(name)
+    to_h.value?(name)
+  end
+
+  def values
+    to_h.values
+  end
+
+  def slice(*keys)
+    to_h.slice(*keys)
+  end
+
+  def values_at(*names)
+    to_h.values_at(*names)
+  end
+
+  def to_hash
+    to_h
+  end
+
+  def shift
+    envs = to_h
+
+    return nil if envs.nil?
+
+    name, value = envs.shift
+
+    self[name] = nil
+    [name, value]
+  end
+
+  def update(hash)
+    hash.each do |key, value|
+      self[key] = value
     end
 
-    def delete(name)
-        value = self[name]
-        return nil if value.nil?
-
-        self[name] = nil
-        yield name if block_given?
-
-        value
-    end
-
-    def delete_if
-        return to_enum(:delete_if) unless block_given?
-      
-        each do |key, value|
-          delete(key) if yield key, value
-        end
-        to_h
-    end
-
-    def empty?
-        self.to_h.empty?
-    end
-
-    def has_key?(name)
-        self.to_h.has_key?(name)
-    end
-
-    def has_value?(value)
-        self.to_h.has_value?(value)
-    end
-
-    def include?(name)
-        self.to_h.has_key?(name)
-    end
-
-    def key(value)
-        self.to_h.key(value)
-    end
-
-    def length
-        self.to_h.length
-    end
-
-    def size
-        self.to_h.size
-    end
-
-    def keys
-        self.to_h.keys
-    end
-
-    def rehash
-        nil
-    end
-
-    def to_a
-        self.to_h.to_a
-    end
-
-    def to_s
-        'ENV'
-    end
-
-    def value?(name)
-        self.to_h.value?(name)
-    end
-
-    def values
-        self.to_h.values
-    end
-
-    def slice(*keys)
-        self.to_h.slice(*keys)
-    end
-
-    def values_at(*names)
-        self.to_h.values_at(*names)
-    end
-
-    def to_hash
-        self.to_h
-    end
-
-    def shift 
-        envs = self.to_h
-
-        return nil if envs.nil?
-        
-        name, value = envs.shift
-
-        self[name] = nil
-        return [name, value]
-    end
-
-    def update(hash)
-        hash.each do |key, value|
-            self[key] = value
-        end
-
-        self.to_h
-    end
+    to_h
+  end
 end
