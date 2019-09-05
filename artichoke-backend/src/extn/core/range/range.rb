06302c3ef32143841347121bbda8f023502bb4ad
KY <infcurious@gmail.com>
2019-09-05 20:28:04 +0800

    Fix rubocop offenses

--- artichoke-backend/src/extn/core/range/range.rb
+++ artichoke-backend/src/extn/core/range/range.rb
@@ -7,7 +7,7 @@ class Range
     range_begin = self.begin
     range_end = self.end
 
-    return false if exclude_end? && (range_begin <=> range_end) == 0
+    return false if exclude_end? && (range_begin <=> range_end).zero?
 
     val = args[0]
 
@@ -16,37 +16,38 @@ class Range
       val_end = val.end
 
       cmp_begin = range_begin <=> val_begin
-      return false if cmp_begin.nil? || cmp_begin > 0
+      return false if cmp_begin.nil? || cmp_begin.positive?
 
       cmp_end = range_end <=> val_end
-      if exclude_end? == val.exclude_end?
-        return cmp_end >= 0
-      elsif exclude_end?
-        return cmp_end > 0
-      elsif cmp_end >= 0
-        return true
-      end
+      return cmp_end >= 0 if exclude_end? == val.exclude_end?
+      return cmp_end.positive? if exclude_end?
+      return true if cmp_end >= 0
 
-      val_max = val.max rescue nil
+      val_max = begin
+                  val.max
+                rescue StandardError
+                  nil
+                end
       return false if val_max.nil?
 
       return (range_end <=> val_max) >= 0
     end
 
     cmp_begin = val <=> range_begin
-    return false if cmp_begin.nil? || cmp_begin < 0
+    return false if cmp_begin.nil? || cmp_begin.negative?
 
-    max = self.max rescue nil
+    max = begin
+            self.max
+          rescue StandardError
+            nil
+          end
     cmp_max = val <=> max
     cmp_end = val <=> range_end
 
-    if exclude_end?
-      return cmp_end < 0
-    elsif cmp_end <= 0
-      return true
-    end
+    return cmp_end.negative? if exclude_end?
+    return true if cmp_end <= 0
 
-    return cmp_max <= 0
+    cmp_max <= 0
   end
 
   def first(*args)
