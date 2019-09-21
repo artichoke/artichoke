7dbf59e6309765442dc37641fc5f5fb9d43e43ea
Ryan Lopopolo <rjl@hyperbo.la>
2019-09-20 23:40:05 -0700

    Define artichoke_ary_* prototypes
    These functions are defined in artichoke-backend and will be linked when building
    the airb executable.

--- mruby-sys/mruby-sys/include/mruby-sys/ext.h
+++ mruby-sys/mruby-sys/include/mruby-sys/ext.h
@@ -142,3 +142,40 @@ MRB_API _Bool mrb_sys_gc_enable(mrb_state *mrb);
 MRB_API _Bool mrb_sys_value_is_dead(mrb_state *_mrb, mrb_value value);
 
 MRB_API int mrb_sys_gc_live_objects(mrb_state *mrb);
+
+// Array overrides
+MRB_API mrb_value artichoke_assoc_new(mrb_state *mrb, mrb_value car,
+                                      mrb_value cdr);
+MRB_API mrb_value artichoke_value_to_ary(mrb_state *mrb, mrb_value value);
+MRB_API mrb_value artichoke_ary_new(mrb_state *mrb);
+MRB_API mrb_value artichoke_ary_new_capa(mrb_state *, mrb_int);
+MRB_API mrb_value artichoke_ary_new_from_values(mrb_state *mrb, mrb_int size,
+                                                const mrb_value *vals);
+MRB_API mrb_value artichoke_ary_splat(mrb_state *mrb, mrb_value value);
+MRB_API mrb_value artichoke_ary_clone(mrb_state *mrb, mrb_value value);
+MRB_API mrb_value artichoke_ary_clear(mrb_state *mrb, mrb_value self);
+MRB_API mrb_value artichoke_ary_entry(mrb_value ary, mrb_int offset);
+MRB_API mrb_value artichoke_ary_ref(mrb_state *mrb, mrb_value ary, mrb_int n);
+MRB_API mrb_value artichoke_ary_entry(mrb_value ary, mrb_int offset);
+MRB_API mrb_value artichoke_ary_ref(mrb_state *mrb, mrb_value ary, mrb_int n);
+MRB_API mrb_value artichoke_ary_join(mrb_state *mrb, mrb_value ary,
+                                     mrb_value sep);
+MRB_API mrb_value artichoke_ary_pop(mrb_state *mrb, mrb_value ary);
+MRB_API mrb_value artichoke_ary_resize(mrb_state *mrb, mrb_value ary,
+                                       mrb_int new_len);
+MRB_API mrb_value artichoke_ary_shift(mrb_state *mrb, mrb_value self);
+MRB_API mrb_value artichoke_ary_splice(mrb_state *mrb, mrb_value self,
+                                       mrb_int head, mrb_int len,
+                                       mrb_value rpl);
+MRB_API mrb_value artichoke_ary_shift(mrb_state *mrb, mrb_value self);
+MRB_API mrb_value artichoke_ary_unshift(mrb_state *mrb, mrb_value self,
+                                        mrb_value item);
+MRB_API mrb_int artichoke_ary_len(mrb_state *mrb, mrb_value self);
+MRB_API void artichoke_ary_concat(mrb_state *mrb, mrb_value self,
+                                  mrb_value other);
+MRB_API void artichoke_ary_push(mrb_state *mrb, mrb_value array,
+                                mrb_value value);
+MRB_API void artichoke_ary_replace(mrb_state *mrb, mrb_value self,
+                                   mrb_value other);
+MRB_API void artichoke_ary_set(mrb_state *mrb, mrb_value ary, mrb_int n,
+                               mrb_value val);
