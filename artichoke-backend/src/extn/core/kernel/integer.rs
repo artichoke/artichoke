7ea184713ceea02aba83856b6cde4d6eafdd6708
Ryan Lopopolo <rjl@hyperbo.la>
2019-10-14 08:33:24 -0500

    Use iterators and state machine in Kernel#Integer
    Clean up GH-293 to be more idiomatic Rust.
    
    This code passes the same specs as the old.

--- artichoke-backend/src/extn/core/kernel/integer.rs
+++ artichoke-backend/src/extn/core/kernel/integer.rs
@@ -1,4 +1,6 @@
-use std::convert::TryInto;
+use artichoke_core::value::Value as ValueLike;
+use std::convert::TryFrom;
+use std::iter::Iterator;
 use std::str::FromStr;
 
 use crate::convert::{Convert, TryConvert};
@@ -7,12 +9,12 @@ use crate::types::Int;
 use crate::value::Value;
 use crate::Artichoke;
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-pub enum Error {
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum Error<'a> {
     ContainsNullByte,
-    InvalidValue(String),
-    InvalidRadix(i64),
-    NoImplicitConversionToString(String),
+    InvalidValue(&'a str),
+    InvalidRadix(Int),
+    NoImplicitConversionToString(&'a str),
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -27,144 +29,173 @@ impl<'a> Args<'a> {
         arg: sys::mrb_value,
         base: Option<sys::mrb_value>,
     ) -> Result<Self, Error> {
-        let radix: Option<Int> = if let Some(base) = base {
-            match interp.try_convert(Value::new(interp, base)) {
-                Ok(base) => base,
-                _ => None,
-            }
-        } else {
-            None
-        };
-
+        let radix = base
+            .map(|base| Value::new(interp, base))
+            .and_then(|base| base.try_into::<Int>().ok());
         let value = Value::new(interp, arg);
         let value_type = value.pretty_name();
         if let Ok(arg) = interp.try_convert(value) {
             Ok(Args { arg, radix })
         } else {
-            Err(Error::NoImplicitConversionToString(value_type.to_owned()))
+            Err(Error::NoImplicitConversionToString(value_type))
         }
     }
 }
 
-pub fn method<'a>(interp: &'a Artichoke, args: &Args<'a>) -> Result<Value, Error> {
+pub fn method<'a>(interp: &'a Artichoke, args: &Args<'a>) -> Result<Value, Error<'a>> {
+    #[derive(Debug, Clone, Copy)]
+    enum Sign {
+        Pos,
+        Neg,
+    }
+    #[derive(Debug, Clone)]
+    enum ParseState {
+        Initial,
+        Sign(Sign),
+        Accumulate(Sign, String),
+    }
     let arg = args.arg;
-    let radix = args.radix;
+    let radix = match args.radix.map(u32::try_from) {
+        Some(Ok(radix)) => Some(radix),
+        Some(Err(_)) => return Err(Error::InvalidRadix(args.radix.unwrap_or_default())),
+        None => None,
+    };
 
-    let mut digits = String::new();
-    let mut sign = None;
-    let mut err = None;
-    for i in 0..arg.len() {
-        let c = arg.chars().nth(i).unwrap(); // we're sure that it won't be `None`
+    let mut state = ParseState::Initial;
+    let mut chars = arg.chars().skip_while(|c| c.is_whitespace()).peekable();
+    let mut prev = None::<char>;
+    while chars.peek().is_some() {
+        let current = chars.next();
 
-        // handle space between sign & digit, it should error!
-        if c.is_whitespace() {
-            if i == 0 {
-                continue; // ugly workaround for i - 1 index out of bound!
-            }
-
-            if let Some(prev_c) = arg.chars().nth(i - 1) {
-                if prev_c == '+' || prev_c == '-' {
-                    err = Some(Error::InvalidValue(arg.to_string()));
-                    break;
-                } else {
-                    continue;
-                }
-            }
+        if let Some('\0') = current {
+            return Err(Error::ContainsNullByte);
         }
 
-        // ignore an embedded `_`
-        if c == '_' && i > 0 && i < arg.len() - 1 {
-            let next_c = arg.chars().nth(i + 1).unwrap();
-            let prev_c = arg.chars().nth(i - 1).unwrap();
-
-            if next_c.is_numeric() && prev_c.is_numeric() {
+        // Ignore an embedded underscore (`_`).
+        if let Some('_') = current {
+            let valid_prev = prev
+                .map(|prev| prev.is_numeric() || prev.is_alphabetic())
+                .unwrap_or_default();
+            let next = chars.peek();
+            let valid_next = next
+                .map(|next| next.is_numeric() || next.is_alphabetic())
+                .unwrap_or_default();
+            if valid_prev && valid_next {
+                prev = current;
+                continue;
+            }
+        }
+        if current.map(char::is_whitespace).unwrap_or_default() {
+            if let Some('+') | Some('-') = prev {
+                return Err(Error::InvalidValue(arg));
+            } else {
+                prev = current;
                 continue;
             }
         }
 
-        if c == '\0' {
-            err = Some(Error::ContainsNullByte);
-            break;
-        }
-
-        if c == '+' || c == '-' {
-            // handle >1 consecutive sign
-            let next_c = arg.chars().nth(i + 1);
-            if next_c.is_none() || next_c == Some('+') || next_c == Some('-') {
-                err = Some(Error::InvalidValue(arg.to_string()));
-                break;
+        state = match current {
+            Some('+') => {
+                if let ParseState::Sign(_) | ParseState::Accumulate(_, _) = state {
+                    return Err(Error::InvalidValue(arg));
+                }
+                ParseState::Sign(Sign::Pos)
             }
-            sign = Some(c);
-        } else {
-            digits.push(c);
-        }
+            Some('-') => {
+                if let ParseState::Sign(_) | ParseState::Accumulate(_, _) = state {
+                    return Err(Error::InvalidValue(arg));
+                }
+                ParseState::Sign(Sign::Neg)
+            }
+            Some(digit) => match state {
+                ParseState::Initial => {
+                    let mut digits = String::new();
+                    digits.push(digit);
+                    ParseState::Accumulate(Sign::Pos, digits)
+                }
+                ParseState::Sign(sign) => {
+                    let mut digits = String::new();
+                    digits.push(digit);
+                    ParseState::Accumulate(sign, digits)
+                }
+                ParseState::Accumulate(sign, mut digits) => {
+                    digits.push(digit);
+                    ParseState::Accumulate(sign, digits)
+                }
+            },
+            None => return Err(Error::InvalidValue(arg)),
+        };
+        prev = current;
     }
 
-    if let Some(err) = err {
-        return Err(err);
-    }
-
-    let mut parsed_radix = None;
-    if digits.len() >= 2 {
-        match &digits[0..2] {
+    let (candidate, parsed_radix) = if let ParseState::Accumulate(sign, mut digits) = state {
+        let parsed_radix = match digits.chars().take(2).collect::<String>().as_str() {
             "0b" | "0B" => {
-                digits = digits[2..].to_string();
-                parsed_radix = Some(2);
+                digits.drain(..2);
+                Some(2)
             }
             "0o" | "0O" => {
-                digits = digits[2..].to_string();
-                parsed_radix = Some(8);
+                digits.drain(..2);
+                Some(8)
             }
             "0d" | "0D" => {
-                digits = digits[2..].to_string();
-                parsed_radix = Some(10);
+                digits.drain(..2);
+                Some(10)
             }
             "0x" | "0X" => {
-                digits = digits[2..].to_string();
-                parsed_radix = Some(16);
+                digits.drain(..2);
+                Some(16)
             }
-            prefix if &prefix[0..1] == "0" => {
-                digits = digits[1..].to_string();
-                parsed_radix = Some(8);
+            prefix => {
+                let mut chars = prefix.chars();
+                let first = chars.next();
+                let next = chars.next();
+                if let Some(next) = next {
+                    if !next.is_numeric() && !next.is_alphabetic() {
+                        return Err(Error::InvalidValue(arg));
+                    } else if let Some('0') = first {
+                        digits.drain(..1);
+                        Some(8)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
             }
-            _ => {}
         };
-    }
 
-    if let Some(sign) = sign {
-        digits.insert(0, sign);
-    }
+        if let Sign::Neg = sign {
+            digits.insert(0, '-');
+        }
+        (digits, parsed_radix)
+    } else {
+        return Err(Error::InvalidValue(arg));
+    };
 
     match (radix, parsed_radix) {
-        (Some(radix), Some(parsed_radix)) => {
-            if radix != parsed_radix {
-                return Err(Error::InvalidValue(digits));
-            }
-            if let Ok(v) = Int::from_str_radix(&digits, radix.try_into().unwrap()) {
-                let result = interp.convert(v);
-                Ok(result)
+        (Some(radix), Some(parsed_radix)) if radix == parsed_radix => {
+            if let Ok(integer) = Int::from_str_radix(candidate.as_str(), radix) {
+                Ok(interp.convert(integer))
             } else {
-                Err(Error::InvalidValue(digits))
+                Err(Error::InvalidValue(arg))
             }
         }
-        (Some(radix), None) | (None, Some(radix)) => {
-            if radix < 2 || radix > 36 {
-                return Err(Error::InvalidRadix(radix));
-            }
-            if let Ok(v) = Int::from_str_radix(&digits, radix.try_into().unwrap()) {
-                let result = interp.convert(v);
-                Ok(result)
+        (Some(radix), None) | (None, Some(radix)) if radix >= 2 && radix <= 36 => {
+            if let Ok(integer) = Int::from_str_radix(candidate.as_str(), radix) {
+                Ok(interp.convert(integer))
             } else {
-                Err(Error::InvalidValue(digits))
+                Err(Error::InvalidValue(arg))
             }
         }
         (None, None) => {
-            if let Ok(v) = i64::from_str(&digits) {
-                let result = interp.convert(v);
-                Ok(result)
+            if let Ok(integer) = Int::from_str(candidate.as_str()) {
+                Ok(interp.convert(integer))
             } else {
-                Err(Error::InvalidValue(digits))
+                Err(Error::InvalidValue(arg))
             }
         }
+        (Some(_), Some(_)) => Err(Error::InvalidValue(arg)),
+        (Some(radix), None) | (None, Some(radix)) => Err(Error::InvalidRadix(Int::from(radix))),
     }
 }
