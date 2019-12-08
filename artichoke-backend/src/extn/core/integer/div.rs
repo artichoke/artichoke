14b4af8292778f3139c4dd8a0227c868fcc7ab5c
Ryan Lopopolo <rjl@hyperbo.la>
2019-12-08 12:12:47 -0800

    Handle zero divisior, infinity, and NaN float cases

--- artichoke-backend/src/extn/core/integer/div.rs
+++ artichoke-backend/src/extn/core/integer/div.rs
@@ -1,8 +1,9 @@
 use artichoke_core::value::Value as _;
 
 use crate::convert::Convert;
-use crate::extn::core::exception::{Fatal, RubyException, TypeError};
-use crate::types::{Float, Int};
+use crate::extn::core::exception::{Fatal, RubyException, TypeError, ZeroDivisionError};
+use crate::extn::core::float::Float;
+use crate::types::{self, Int};
 use crate::value::Value;
 use crate::Artichoke;
 
@@ -19,10 +20,25 @@ pub fn method(
     })?;
     let pretty_name = other.pretty_name();
     if let Ok(y) = other.clone().try_into::<Int>() {
-        Ok(interp.convert(x / y))
-    } else if let Ok(y) = other.try_into::<Float>() {
-        #[allow(clippy::cast_precision_loss)]
-        Ok(interp.convert(x as Float / y))
+        if y == 0 {
+            Err(Box::new(ZeroDivisionError::new(interp, "divided by 0")))
+        } else {
+            Ok(interp.convert(x / y))
+        }
+    } else if let Ok(y) = other.try_into::<types::Float>() {
+        if y == 0.0 {
+            if x > 0 {
+                Ok(interp.convert(Float::INFINITY))
+            } else if x < 0 {
+                Ok(interp.convert(Float::NEG_INFINITY))
+            } else {
+                // x == 0
+                Ok(interp.convert(Float::NAN))
+            }
+        } else {
+            #[allow(clippy::cast_precision_loss)]
+            Ok(interp.convert(x as types::Float / y))
+        }
     } else {
         Err(Box::new(TypeError::new(
             interp,
