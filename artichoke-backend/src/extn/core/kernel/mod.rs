63198b1fa39dccf091bcbcaeda3670d19d82deaa
Ryan Lopopolo <rjl@hyperbo.la>
2020-01-15 22:26:55 -0800

    Buffer writes in Rust for Kernel::print and Kernel::puts

--- artichoke-backend/src/extn/core/kernel/mod.rs
+++ artichoke-backend/src/extn/core/kernel/mod.rs
@@ -79,22 +79,24 @@ impl Kernel {
         let args = mrb_get_args!(mrb, *args);
         let interp = unwrap_interpreter!(mrb);
 
+        let mut buf = vec![];
         for value in args.iter().copied() {
             let to_s = Value::new(&interp, value).to_s();
-            interp.0.borrow_mut().print(to_s.as_slice());
+            buf.extend(to_s);
         }
+        interp.0.borrow_mut().print(buf.as_slice());
         sys::mrb_sys_nil_value()
     }
 
     unsafe extern "C" fn puts(mrb: *mut sys::mrb_state, _slf: sys::mrb_value) -> sys::mrb_value {
-        fn do_puts(interp: &Artichoke, value: &Value) {
+        fn do_puts(interp: &Artichoke, value: &Value, buf: &mut Vec<u8>) {
             if let Ok(array) = value.clone().try_into::<Vec<Value>>() {
                 for value in array {
-                    do_puts(interp, &value);
+                    do_puts(interp, &value, buf);
                 }
             } else {
-                let to_s = value.to_s();
-                interp.0.borrow_mut().puts(to_s.as_slice());
+                buf.extend(value.to_s());
+                buf.push(b'\n');
             }
         }
 
@@ -102,9 +104,12 @@ impl Kernel {
         let interp = unwrap_interpreter!(mrb);
         if args.is_empty() {
             interp.0.borrow_mut().puts(&[]);
-        }
-        for value in args.iter() {
-            do_puts(&interp, &Value::new(&interp, *value));
+        } else {
+            let mut buf = vec![];
+            for value in args.iter().copied() {
+                do_puts(&interp, &Value::new(&interp, value), &mut buf);
+            }
+            interp.0.borrow_mut().print(buf.as_slice());
         }
         sys::mrb_sys_nil_value()
     }
