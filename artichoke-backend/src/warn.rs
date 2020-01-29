b3954ef9b756c8ad1f96666234fe26ec89860af6
Ryan Lopopolo <rjl@hyperbo.la>
2020-01-28 22:51:41 -0800

    Write warnings to stderr instead of using the log crate

--- artichoke-backend/src/warn.rs
+++ artichoke-backend/src/warn.rs
@@ -1,6 +1,7 @@
 use artichoke_core::value::Value as _;
 use artichoke_core::warn::Warn;
 use std::borrow::Cow;
+use std::io::{self, Write};
 
 use crate::convert::Convert;
 use crate::exception::Exception;
@@ -13,7 +14,9 @@ impl Warn for Artichoke {
     type Error = Exception;
 
     fn warn(&self, message: &[u8]) -> Result<(), Self::Error> {
-        warn!("rb warning: {}", String::from_utf8_lossy(message));
+        let _ = io::stderr().write_all(b"rb warning: ");
+        let _ = io::stderr().write_all(message);
+        let _ = io::stderr().write_all(b"\n");
         let warning = {
             let borrow = self.0.borrow();
             let spec = borrow
