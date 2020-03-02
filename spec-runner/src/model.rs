24bba464e66e9ff12dec126abc29f483476a819e
Ryan Lopopolo <rjl@hyperbo.la>
2020-03-01 17:45:33 -0800

    Add YAML models for enforced-specs config
    These models add an additional 'skip' collection to the spec suite
    that allows specs to be disabled. Specs in 'skip' take precedence over
    those included in 'specs'.

---
+++ spec-runner/src/model.rs
@@ -0,0 +1,51 @@
+use serde::{Deserialize, Serialize};
+use std::ffi::OsStr;
+
+/// Config file format for declaring the set of ruby/spec suites to run.
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Config {
+    /// Specs for core language features.
+    ///
+    /// For example, `Regexp` literal support.
+    pub language: Option<Vec<Suite>>,
+    /// Specs for the core objects and their API compatibility.
+    ///
+    /// For example, the behavior of `Comparable#==` when called with `self` as
+    /// the argument.
+    pub core: Option<Vec<Suite>>,
+    /// Specs for the packages in the standard library and their API
+    /// compatibility.
+    ///
+    /// For example, the behavior of `StringScanner` when dealing with invalid
+    /// UTF-8 `String`s.
+    pub library: Option<Vec<Suite>>,
+}
+
+impl Config {
+    pub fn suites_for_family(&self, suite: &OsStr) -> Option<&[Suite]> {
+        match suite {
+            suite if suite == OsStr::new("lanugage") => self.language.as_deref(),
+            suite if suite == OsStr::new("core") => self.core.as_deref(),
+            suite if suite == OsStr::new("library") => self.library.as_deref(),
+            _ => None,
+        }
+    }
+}
+
+/// The specs to run for a suite or API group.
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Suite {
+    /// Suite name.
+    ///
+    /// For example, `array`.
+    pub suite: String,
+    /// List of specs. Specs correspond to individual mspec files in `ruby/spec`.
+    ///
+    /// For example, `any`, `append`, and `assoc` for `array`.
+    pub specs: Option<Vec<String>>,
+    /// List of specs to always skip because they are known to fail.
+    ///
+    /// Specs in this list will override an explicit include in the `specs`
+    /// field.
+    pub skip: Option<Vec<String>>,
+}
