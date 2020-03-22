0a7d46a01e88865a6fb6de62a0018c97ab5f3c14
Ryan Lopopolo <rjl@hyperbo.la>
2020-03-21 18:07:07 -0700

    Improve crate-level documentation for artichoke

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,50 @@
 #![warn(missing_debug_implementations)]
 #![warn(rust_2018_idioms)]
 
-//!  `artichoke-frontend` crate provides binaries for interacting with the
-//!  artichoke interpreter in the [`artichoke-backend`](artichoke_backend)
-//!  crate.
+//! Artichoke Ruby
+//!
+//! This crate is a Rust, and Ruby implementation of the Ruby programming
+//! language. Artichoke is not production-ready, but intends to be a
+//! [MRI-compliant](https://github.com/ruby/spec) implementation of Ruby 2.6.3.
+//!
+//! This crate provides:
+//!
+//! - An embeddable Ruby interpreter, which can be crated with the
+//!   [`interpreter`] function.
+//! - A Rust and Ruby implementation of Ruby Core and Standard Library using
+//!   high-quality Rust dependencies and modern Ruby.
+//! - Support for injecting Rust code and types into the interpreter with a
+//!   rubygems-style [`File`](backend::File) API.
+//! - The ability to disable parts of the interpreter VM or library functions at
+//!   compile time. For example, access to the system environ can be denied by
+//!   disabling the `core-env-system` feature.
+//!
+//! ## Usage
+//!
+//! You can create an interpreter and begin executing code on it:
+//!
+//! ```
+//! # use artichoke::backend::{Eval, ValueLike};
+//! # fn main() -> Result<(), artichoke::backend::exception::Exception> {
+//! let mut interp = artichoke::interpreter()?;
+//! let result = interp.eval(b"2 + 5")?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! Artichoke supports calling Ruby functions from Rust and converting between
+//! Ruby boxed values and Rust native types:
+//!
+//! ```
+//! # use artichoke::backend::{ConvertMut, ValueLike};
+//! # fn main() -> Result<(), artichoke::backend::exception::Exception> {
+//! let mut interp = artichoke::interpreter()?;
+//! let s = interp.convert_mut("💎");
+//! let codepoint = s.funcall::<u32>("ord", &[] /* args */, None /* block parameter */)?;
+//! assert_eq!(128142, codepoint);
+//! # Ok(())
+//! # }
+//! ```
 
 #![doc(html_root_url = "https://docs.rs/artichoke/0.1.0-pre.0")]
 
