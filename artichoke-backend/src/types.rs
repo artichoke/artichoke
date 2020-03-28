258519f41458f2cb7e2ab667d27d0bf42c16270d
Ryan Lopopolo <rjl@hyperbo.la>
2020-03-27 17:44:04 -0700

    Improve comments and test coverage of types::ruby_from_mrb_value

--- artichoke-backend/src/types.rs
+++ artichoke-backend/src/types.rs
@@ -11,39 +11,42 @@ pub type Int = i32;
 
 pub use crate::core::types::{Ruby, Rust};
 
-// This conversion has to be from mrb_value instead of mrb_vtype to disambiguate
-// between `Ruby::Nil` and a false `Ruby::Bool`.
+/// Parse a [`Ruby`] type classifier from a [`sys::mrb_value`].
+///
+/// This function collapses some mruby types into a [`Ruby::Unreachable`] type
+/// that force an interaction with the VM to return an error.
 #[allow(non_upper_case_globals)]
 #[must_use]
 pub fn ruby_from_mrb_value(value: sys::mrb_value) -> Ruby {
-    // `nil` is implemented with the `MRB_TT_FALSE` type tag in mruby
-    // (since both values are falsy). The difference is that booleans are
-    // non-zero `Fixnum`s.
-    if unsafe { sys::mrb_sys_value_is_nil(value) } {
-        return Ruby::Nil;
-    }
+    use sys::mrb_vtype::*;
 
-    // switch on the type tag in the `mrb_value`
-    #[allow(clippy::match_same_arms)] // ignore lint to map to the `mrb_vtype` enum def
+    // Suppress lint to enumerate match arms in the same order they are defined
+    // in the `sys::mrb_vtype` enum C source.
+    #[allow(clippy::match_same_arms)]
     match value.tt {
-        sys::mrb_vtype::MRB_TT_FALSE => Ruby::Bool,
+        // `nil` is implemented with the `MRB_TT_FALSE` type tag in mruby
+        // (since both values are falsy). The difference is that booleans are
+        // non-zero `Fixnum`s.
+        MRB_TT_FALSE if unsafe { sys::mrb_sys_value_is_nil(value) } => Ruby::Nil,
+        MRB_TT_FALSE => Ruby::Bool,
         // `MRB_TT_FREE` is a marker type tag that indicates to the mruby
-        // VM that an object should be garbage collected.
-        sys::mrb_vtype::MRB_TT_FREE => Ruby::Unreachable,
-        sys::mrb_vtype::MRB_TT_TRUE => Ruby::Bool,
-        sys::mrb_vtype::MRB_TT_FIXNUM => Ruby::Fixnum,
-        sys::mrb_vtype::MRB_TT_SYMBOL => Ruby::Symbol,
+        // VM that an object is unreachable and should be deallocated by the
+        // garbage collector.
+        MRB_TT_FREE => Ruby::Unreachable,
+        MRB_TT_TRUE => Ruby::Bool,
+        MRB_TT_FIXNUM => Ruby::Fixnum,
+        MRB_TT_SYMBOL => Ruby::Symbol,
         // internal use: #undef; should not happen
-        sys::mrb_vtype::MRB_TT_UNDEF => Ruby::Unreachable,
-        sys::mrb_vtype::MRB_TT_FLOAT => Ruby::Float,
+        MRB_TT_UNDEF => Ruby::Unreachable,
+        MRB_TT_FLOAT => Ruby::Float,
         // `MRB_TT_CPTR` wraps a borrowed `void *` pointer.
-        sys::mrb_vtype::MRB_TT_CPTR => Ruby::CPointer,
-        sys::mrb_vtype::MRB_TT_OBJECT => Ruby::Object,
-        sys::mrb_vtype::MRB_TT_CLASS => Ruby::Class,
-        sys::mrb_vtype::MRB_TT_MODULE => Ruby::Module,
+        MRB_TT_CPTR => Ruby::CPointer,
+        MRB_TT_OBJECT => Ruby::Object,
+        MRB_TT_CLASS => Ruby::Class,
+        MRB_TT_MODULE => Ruby::Module,
         // `MRB_TT_ICLASS` is an internal use type tag meant for holding
         // mixed in modules.
-        sys::mrb_vtype::MRB_TT_ICLASS => Ruby::Unreachable,
+        MRB_TT_ICLASS => Ruby::Unreachable,
         // `MRB_TT_SCLASS` represents a singleton class, or a class that is
         // defined anonymously, e.g. `c1` or `c2` below:
         //
@@ -62,81 +65,206 @@ pub fn ruby_from_mrb_value(value: sys::mrb_value) -> Ruby {
         // obj = Foo.new
         // def obj.bar; 'bar'; end
         // ```
-        sys::mrb_vtype::MRB_TT_SCLASS => Ruby::SingletonClass,
-        sys::mrb_vtype::MRB_TT_PROC => Ruby::Proc,
-        sys::mrb_vtype::MRB_TT_ARRAY => Ruby::Array,
-        sys::mrb_vtype::MRB_TT_HASH => Ruby::Hash,
-        sys::mrb_vtype::MRB_TT_STRING => Ruby::String,
-        sys::mrb_vtype::MRB_TT_RANGE => Ruby::Range,
-        sys::mrb_vtype::MRB_TT_EXCEPTION => Ruby::Exception,
-        // TODO: Implement File, see GH-4.
-        sys::mrb_vtype::MRB_TT_FILE => unimplemented!("mruby type file"),
+        MRB_TT_SCLASS => Ruby::SingletonClass,
+        MRB_TT_PROC => Ruby::Proc,
+        // `MRB_TT_ARRAY` refers to the mruby `mrb_array` implementation.
+        // Artichoke implements its own `Array` as a `Ruby::Data`, so this
+        // variant is unreachable.
+        MRB_TT_ARRAY => Ruby::Unreachable,
+        MRB_TT_HASH => Ruby::Hash,
+        MRB_TT_STRING => Ruby::String,
+        MRB_TT_RANGE => Ruby::Range,
+        MRB_TT_EXCEPTION => Ruby::Exception,
+        // This is a special value type for the `mruby-io` implementation of
+        // the `File` class. Artichoke does not bundle `mruby-io`, so this
+        // variant is unreachable.
+        MRB_TT_FILE => Ruby::Unreachable,
         // ENV is currently implemented as a singleton object in Ruby.
-        sys::mrb_vtype::MRB_TT_ENV => unimplemented!("mruby type env"),
+        // NOTE(lopopolo): This might be an internal closure symbol table,
+        // rather than the `ENV` core object.
+        MRB_TT_ENV => Ruby::Unreachable,
         // `MRB_TT_DATA` is a type tag for wrapped C pointers. It is used
         // to indicate that an `mrb_value` has an owned pointer to an
         // external data structure stored in its `value.p` field.
-        sys::mrb_vtype::MRB_TT_DATA => Ruby::Data,
-        sys::mrb_vtype::MRB_TT_FIBER => Ruby::Fiber,
+        MRB_TT_DATA => Ruby::Data,
+        // NOTE(lopopolo): `Fiber`s are unimplemented in Artichoke.
+        MRB_TT_FIBER => Ruby::Fiber,
         // MRB_TT_ISTRUCT is an "inline structure", or a mrb_value that
         // stores data in a char* buffer inside an mrb_value. These
         // mrb_values cannot have a finalizer and cannot have instance
         // variables.
         //
         // See vendor/mruby-*/include/mruby/istruct.h
-        sys::mrb_vtype::MRB_TT_ISTRUCT => Ruby::InlineStruct,
-        // `MRB_TT_BREAK` is used internally to the mruby VM and appears to
-        // have something to do with resuming continuations from Fibers.
-        sys::mrb_vtype::MRB_TT_BREAK => Ruby::Unreachable,
+        MRB_TT_ISTRUCT => Ruby::InlineStruct,
+        // `MRB_TT_BREAK` is used internally to the mruby VM. BREAK is used as
+        // the return value of `mrb_yield` when the block has a non-local
+        // return.
+        //
+        // FIXME(lopopolo): The below "unreachable" designation is incorrect.
+        // BREAK should be handled by `sys::protect::block_yield`.
+        MRB_TT_BREAK => Ruby::Unreachable,
         // `MRB_TT_MAXDEFINE` is a marker enum value used by the mruby VM to
         // dynamically check if a type tag is valid using the less than
         // operator. It does not correspond to an instantiated type.
-        sys::mrb_vtype::MRB_TT_MAXDEFINE => Ruby::Unreachable,
+        MRB_TT_MAXDEFINE => Ruby::Unreachable,
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::ffi::CString;
+    use std::collections::HashMap;
 
-    use crate::sys;
-    use crate::types::{self, Ruby};
+    use crate::test::prelude::*;
+    use crate::types;
 
     #[test]
-    fn nil_type() {
-        unsafe {
-            let value = sys::mrb_sys_nil_value();
-            assert_eq!(types::ruby_from_mrb_value(value), Ruby::Nil);
-        }
+    fn parse_nil_ruby_type() {
+        let interp = crate::interpreter().unwrap();
+        let nil = interp.convert(None::<Value>);
+        assert_eq!(Ruby::Nil, types::ruby_from_mrb_value(nil.inner()));
     }
 
     #[test]
-    fn bool_type() {
-        unsafe {
-            let value = sys::mrb_sys_false_value();
-            assert_eq!(types::ruby_from_mrb_value(value), Ruby::Bool);
-            let value = sys::mrb_sys_true_value();
-            assert_eq!(types::ruby_from_mrb_value(value), Ruby::Bool);
-        }
+    fn parse_bool_ruby_type() {
+        let interp = crate::interpreter().unwrap();
+        let yes = interp.convert(true);
+        assert_eq!(Ruby::Bool, types::ruby_from_mrb_value(yes.inner()));
+        let no = interp.convert(false);
+        assert_eq!(Ruby::Bool, types::ruby_from_mrb_value(no.inner()));
     }
 
     #[test]
-    fn fixnum_type() {
-        unsafe {
-            let value = sys::mrb_sys_fixnum_value(17);
-            assert_eq!(types::ruby_from_mrb_value(value), Ruby::Fixnum);
-        }
+    fn parse_fixnum_ruby_type() {
+        let interp = crate::interpreter().unwrap();
+        let zero = interp.convert(0_i64);
+        assert_eq!(Ruby::Fixnum, types::ruby_from_mrb_value(zero.inner()));
+        let thousand = interp.convert(1000_i64);
+        assert_eq!(Ruby::Fixnum, types::ruby_from_mrb_value(thousand.inner()));
     }
 
     #[test]
-    fn string_type() {
-        unsafe {
-            let mrb = sys::mrb_open();
-            let literal = "dinner plate";
-            let cstr = CString::new(literal).unwrap();
-            let value = sys::mrb_str_new_cstr(mrb, cstr.as_ptr());
-            assert_eq!(types::ruby_from_mrb_value(value), Ruby::String);
-            sys::mrb_close(mrb);
-        }
+    fn parse_symbol_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let empty = interp.eval(b":''").unwrap();
+        assert_eq!(Ruby::Symbol, types::ruby_from_mrb_value(empty.inner()));
+        let utf8 = interp.eval(b":Artichoke").unwrap();
+        assert_eq!(Ruby::Symbol, types::ruby_from_mrb_value(utf8.inner()));
+        let binary = interp.eval(r#":"\xFE""#.as_bytes()).unwrap();
+        assert_eq!(Ruby::Symbol, types::ruby_from_mrb_value(binary.inner()));
+    }
+
+    #[test]
+    fn parse_float_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let zero = interp.convert_mut(0.0_f64);
+        assert_eq!(Ruby::Float, types::ruby_from_mrb_value(zero.inner()));
+        let float = interp.convert_mut(1.5_f64);
+        assert_eq!(Ruby::Float, types::ruby_from_mrb_value(float.inner()));
+    }
+
+    #[test]
+    fn parse_object_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let object = interp.eval(b"Object.new").unwrap();
+        assert_eq!(Ruby::Object, types::ruby_from_mrb_value(object.inner()));
+        let env = interp.eval(b"ENV").unwrap();
+        assert_eq!(Ruby::Object, types::ruby_from_mrb_value(env.inner()));
+    }
+
+    #[test]
+    fn parse_class_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let builtin = interp.eval(b"Object").unwrap();
+        assert_eq!(Ruby::Class, types::ruby_from_mrb_value(builtin.inner()));
+        let data = interp.eval(b"Array").unwrap();
+        assert_eq!(Ruby::Class, types::ruby_from_mrb_value(data.inner()));
+        let source = interp.eval(b"Math::DomainError").unwrap();
+        assert_eq!(Ruby::Class, types::ruby_from_mrb_value(source.inner()));
+    }
+
+    #[test]
+    fn parse_module_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let builtin = interp.eval(b"Comparable").unwrap();
+        assert_eq!(Ruby::Module, types::ruby_from_mrb_value(builtin.inner()));
+        let data = interp.eval(b"Math").unwrap();
+        assert_eq!(Ruby::Module, types::ruby_from_mrb_value(data.inner()));
+        let artichoke = interp.eval(b"Artichoke").unwrap();
+        assert_eq!(Ruby::Module, types::ruby_from_mrb_value(artichoke.inner()));
+    }
+
+    #[test]
+    fn parse_proc_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let literal = interp.eval(b"proc {}").unwrap();
+        assert_eq!(Ruby::Proc, types::ruby_from_mrb_value(literal.inner()));
+        let proc = interp.eval(b"Proc.new {}").unwrap();
+        assert_eq!(Ruby::Proc, types::ruby_from_mrb_value(proc.inner()));
+        let lambda = interp.eval(b"lambda {}").unwrap();
+        assert_eq!(Ruby::Proc, types::ruby_from_mrb_value(lambda.inner()));
+        let stabby = interp.eval(b"->() {}").unwrap();
+        assert_eq!(Ruby::Proc, types::ruby_from_mrb_value(stabby.inner()));
+    }
+
+    #[test]
+    fn parse_string_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let empty = interp.convert_mut("");
+        assert_eq!(Ruby::String, types::ruby_from_mrb_value(empty.inner()));
+        let utf8 = interp.convert_mut("Artichoke");
+        assert_eq!(Ruby::String, types::ruby_from_mrb_value(utf8.inner()));
+        let binary = interp.convert_mut(vec![0xFF_u8, 0x00, 0xFE]);
+        assert_eq!(Ruby::String, types::ruby_from_mrb_value(binary.inner()));
+    }
+
+    #[test]
+    fn parse_array_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let empty = interp.eval(b"[]").unwrap();
+        assert_eq!(Ruby::Data, types::ruby_from_mrb_value(empty.inner()));
+        let array = interp.eval(b"[1, /./, Object.new]").unwrap();
+        assert_eq!(Ruby::Data, types::ruby_from_mrb_value(array.inner()));
+        let ary = vec!["a", "b", "c"];
+        let converted = interp.convert_mut(ary);
+        assert_eq!(Ruby::Data, types::ruby_from_mrb_value(converted.inner()));
+    }
+
+    #[test]
+    fn parse_hash_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let empty = interp.eval(b"{}").unwrap();
+        assert_eq!(Ruby::Hash, types::ruby_from_mrb_value(empty.inner()));
+        let hash = interp.eval(b"{a: 1, b: [/./]}").unwrap();
+        assert_eq!(Ruby::Hash, types::ruby_from_mrb_value(hash.inner()));
+        let mut map = HashMap::default();
+        map.insert(b"a".to_vec(), vec![0_u8]);
+        map.insert(b"b".to_vec(), b"binary".to_vec());
+        let converted = interp.convert_mut(map);
+        assert_eq!(Ruby::Hash, types::ruby_from_mrb_value(converted.inner()));
+    }
+
+    #[test]
+    fn parse_range_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let dot2 = interp.eval(b"0..0").unwrap();
+        assert_eq!(Ruby::Range, types::ruby_from_mrb_value(dot2.inner()));
+        let dot3 = interp.eval(b"0...0").unwrap();
+        assert_eq!(Ruby::Range, types::ruby_from_mrb_value(dot3.inner()));
+    }
+
+    #[test]
+    fn parse_exception_ruby_type() {
+        let mut interp = crate::interpreter().unwrap();
+        let root = interp.eval(b"Exception.new").unwrap();
+        assert_eq!(Ruby::Exception, types::ruby_from_mrb_value(root.inner()));
+        let stderror = interp.eval(b"StandardError.new").unwrap();
+        assert_eq!(
+            Ruby::Exception,
+            types::ruby_from_mrb_value(stderror.inner())
+        );
+        let index = interp.eval(b"IndexError.new").unwrap();
+        assert_eq!(Ruby::Exception, types::ruby_from_mrb_value(index.inner()));
+        let domain = interp.eval(b"Math::DomainError.new").unwrap();
+        assert_eq!(Ruby::Exception, types::ruby_from_mrb_value(domain.inner()));
     }
 }
