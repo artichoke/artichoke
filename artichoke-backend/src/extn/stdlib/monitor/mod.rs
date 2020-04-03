0b6ef195cbd90db62eb91f39a314527e2b46f981
Ryan Lopopolo <rjl@hyperbo.la>
2020-04-02 21:34:53 -0700

    Update monitor stdlib package
    Move Monitor integration tests from inline snippets executed in individual
    Rust test functions to a single Ruby source file that executes all of the
    tests as a group an 'asserts' by raising.
    
    This pattern is the same as is used for testing the `InlineBuffer` Array
    implementation.
    
    vendorize monitor sources.

---
+++ artichoke-backend/src/extn/stdlib/monitor/mod.rs
@@ -0,0 +1,25 @@
+use crate::extn::prelude::*;
+
+pub fn init(interp: &mut Artichoke) -> InitializeResult<()> {
+    let spec = class::Spec::new("Monitor", None, None)?;
+    interp.0.borrow_mut().def_class::<Monitor>(spec);
+    interp.def_rb_source_file(b"monitor.rb", &include_bytes!("vendor/monitor.rb")[..])?;
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct Monitor;
+
+#[cfg(test)]
+mod tests {
+    use crate::test::prelude::*;
+
+    #[test]
+    fn integration_test() {
+        let mut interp = crate::interpreter().unwrap();
+        let _ = interp.eval(&include_bytes!("monitor_test.rb")[..]).unwrap();
+        let result = interp.eval(b"spec");
+        let result = result.unwrap().try_into::<bool>().unwrap();
+        assert!(result);
+    }
+}
