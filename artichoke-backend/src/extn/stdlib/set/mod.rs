c75b03bb7284cecd97d01ff30ba56860cef57370
Ryan Lopopolo <rjl@hyperbo.la>
2020-04-03 08:46:55 -0700

    Update set stdlib package
    vendorize set sources.

--- artichoke-backend/src/extn/stdlib/set/mod.rs
+++ artichoke-backend/src/extn/stdlib/set/mod.rs
@@ -5,7 +5,7 @@ pub fn init(interp: &mut Artichoke) -> InitializeResult<()> {
     interp.0.borrow_mut().def_class::<Set>(spec);
     let spec = class::Spec::new("SortedSet", None, None)?;
     interp.0.borrow_mut().def_class::<SortedSet>(spec);
-    interp.def_rb_source_file(b"set.rb", &include_bytes!("set.rb")[..])?;
+    interp.def_rb_source_file(b"set.rb", &include_bytes!("vendor/set.rb")[..])?;
     Ok(())
 }
 
