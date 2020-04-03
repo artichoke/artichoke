3a7710c6cbb39ae5af6420a12cbe4ee48bf3424c
Ryan Lopopolo <rjl@hyperbo.la>
2020-04-03 08:57:49 -0700

    Upgrade strscan tests to integration test format
    See GH-628.

---
+++ artichoke-backend/src/extn/stdlib/strscan/mod.rs
@@ -0,0 +1,25 @@
+use crate::extn::prelude::*;
+
+pub fn init(interp: &mut Artichoke) -> InitializeResult<()> {
+    let spec = class::Spec::new("StringScanner", None, None)?;
+    interp.0.borrow_mut().def_class::<StringScanner>(spec);
+    interp.def_rb_source_file(b"strscan.rb", &include_bytes!("strscan.rb")[..])?;
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct StringScanner;
+
+#[cfg(test)]
+mod tests {
+    use crate::test::prelude::*;
+
+    #[test]
+    fn integration_test() {
+        let mut interp = crate::interpreter().expect("init");
+        interp.eval(&include_bytes!("strscan_test.rb")[..]).unwrap();
+        let result = interp.eval(b"spec");
+        let result = result.unwrap().try_into::<bool>().unwrap();
+        assert!(result);
+    }
+}
