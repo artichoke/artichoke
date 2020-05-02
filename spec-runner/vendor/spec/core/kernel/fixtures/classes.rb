3339f56194494dfe4b48022bbf29271d5bfb532b
Ryan Lopopolo <rjl@hyperbo.la>
2020-05-02 12:10:01 -0700

    Add Kernel#Integer to enforced-specs.yaml
    All `Kernel#Integer` specs pass apart from 4 that reference either
    `Bignum` or `Rational`.
    
    The `Kernel` specs define two fixture classes as subclasses of
    `Struct.new`. Artichoke does not support `Struct`. These fixtures, which
    we do not use yet, are commented out.
    
    Artichoke passes 1685 specs now :party: :gem:

--- spec-runner/vendor/spec/core/kernel/fixtures/classes.rb
+++ spec-runner/vendor/spec/core/kernel/fixtures/classes.rb
@@ -425,19 +425,19 @@ module KernelSpecs
     def f3_call_lineno; method(:f4).source_location[1] + 1; end
   end
 
-  CustomRangeInteger = Struct.new(:value) do
-    def to_int; value; end
-    def <=>(other); to_int <=> other.to_int; end
-    def -(other); self.class.new(to_int - other.to_int); end
-    def +(other); self.class.new(to_int + other.to_int); end
-  end
+  # CustomRangeInteger = Struct.new(:value) do
+  #   def to_int; value; end
+  #   def <=>(other); to_int <=> other.to_int; end
+  #   def -(other); self.class.new(to_int - other.to_int); end
+  #   def +(other); self.class.new(to_int + other.to_int); end
+  # end
 
-  CustomRangeFloat = Struct.new(:value) do
-    def to_f; value; end
-    def <=>(other); to_f <=> other.to_f; end
-    def -(other); to_f - other.to_f; end
-    def +(other); self.class.new(to_f + other.to_f); end
-  end
+  # CustomRangeFloat = Struct.new(:value) do
+  #   def to_f; value; end
+  #   def <=>(other); to_f <=> other.to_f; end
+  #   def -(other); to_f - other.to_f; end
+  #   def +(other); self.class.new(to_f + other.to_f); end
+  # end
 end
 
 class EvalSpecs
