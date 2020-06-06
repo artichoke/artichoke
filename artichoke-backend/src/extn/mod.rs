75634ffcc1e9e191e7cb069917e895e89fa876d6
Ryan Lopopolo <rjl@hyperbo.la>
2020-06-05 20:56:40 -0700

    Wire ReleaseMetadata through interpreter factory and extn::init

--- artichoke-backend/src/extn/mod.rs
+++ artichoke-backend/src/extn/mod.rs
@@ -1,63 +1,55 @@
 #![allow(clippy::needless_pass_by_value)]
 
+use crate::core::ReleaseMetadata;
+
 pub mod core;
 pub mod prelude;
 pub mod stdlib;
 
 use prelude::*;
 
-pub const RUBY_COPYRIGHT: &str = env!("RUBY_COPYRIGHT");
-pub const RUBY_DESCRIPTION: &str = env!("RUBY_DESCRIPTION");
-pub const RUBY_ENGINE: &str = "artichoke";
-pub const RUBY_ENGINE_VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const RUBY_PATCHLEVEL: &str = "0";
-pub const RUBY_PLATFORM: &str = env!("RUBY_PLATFORM");
-pub const RUBY_RELEASE_DATE: &str = env!("RUBY_RELEASE_DATE");
-pub const RUBY_REVISION: &str = env!("RUBY_REVISION");
-pub const RUBY_VERSION: &str = "2.6.3";
-
-pub const ARTICHOKE_COMPILER_VERSION: &str = env!("ARTICHOKE_COMPILER_VERSION");
-
 pub const INPUT_RECORD_SEPARATOR: &str = "\n";
 
-pub fn init(interp: &mut Artichoke, backend_name: &str) -> InitializeResult<()> {
-    let copyright = interp.convert_mut(RUBY_COPYRIGHT);
+pub fn init<T>(interp: &mut Artichoke, config: T) -> InitializeResult<()>
+where
+    T: ReleaseMetadata,
+{
+    let copyright = interp.convert_mut(config.ruby_copyright());
     interp.define_global_constant("RUBY_COPYRIGHT", copyright)?;
 
-    let description = interp.convert_mut(RUBY_DESCRIPTION);
+    let description = interp.convert_mut(config.ruby_description());
     interp.define_global_constant("RUBY_DESCRIPTION", description)?;
 
-    let mut engine = String::from(RUBY_ENGINE);
-    engine.push('-');
-    engine.push_str(backend_name);
-    let engine = interp.convert_mut(engine);
+    let engine = interp.convert_mut(config.ruby_engine());
     interp.define_global_constant("RUBY_ENGINE", engine)?;
 
-    let engine_version = interp.convert_mut(RUBY_ENGINE_VERSION);
+    let engine_version = interp.convert_mut(config.ruby_engine_version());
     interp.define_global_constant("RUBY_ENGINE_VERSION", engine_version)?;
 
-    let patchlevel = RUBY_PATCHLEVEL
+    let patchlevel = config
+        .ruby_patchlevel()
         .parse::<Int>()
         .map_err(|_| NotDefinedError::global_constant("RUBY_PATCHLEVEL"))?;
     let patchlevel = interp.convert(patchlevel);
     interp.define_global_constant("RUBY_PATCHLEVEL", patchlevel)?;
 
-    let platform = interp.convert_mut(RUBY_PLATFORM);
+    let platform = interp.convert_mut(config.ruby_platform());
     interp.define_global_constant("RUBY_PLATFORM", platform)?;
 
-    let release_date = interp.convert_mut(RUBY_RELEASE_DATE);
+    let release_date = interp.convert_mut(config.ruby_release_date());
     interp.define_global_constant("RUBY_RELEASE_DATE", release_date)?;
 
-    let revision = RUBY_REVISION
+    let revision = config
+        .ruby_revision()
         .parse::<Int>()
         .map_err(|_| NotDefinedError::global_constant("RUBY_REVISION"))?;
     let revision = interp.convert(revision);
     interp.define_global_constant("RUBY_REVISION", revision)?;
 
-    let version = interp.convert_mut(RUBY_VERSION);
+    let version = interp.convert_mut(config.ruby_version());
     interp.define_global_constant("RUBY_VERSION", version)?;
 
-    let compiler_version = interp.convert_mut(ARTICHOKE_COMPILER_VERSION);
+    let compiler_version = interp.convert_mut(config.artichoke_compiler_version());
     interp.define_global_constant("ARTICHOKE_COMPILER_VERSION", compiler_version)?;
 
     core::init(interp)?;
