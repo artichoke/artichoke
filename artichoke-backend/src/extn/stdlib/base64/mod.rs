a5c47d630e6189789e5e07d9b88fe97e8ea7a96e
YUJI takahashi <jit.y.31@gmail.com>
2020-06-08 16:31:05 +0900

    Add glue for base64

---
+++ artichoke-backend/src/extn/stdlib/base64/mod.rs
@@ -0,0 +1,12 @@
+use crate::extn::prelude::*;
+
+pub fn init(interp: &mut Artichoke) -> InitializeResult<()> {
+    let spec = crate::module::Spec::new(interp, "Base64", None)?;
+    interp.def_module::<Base64>(spec)?;
+    interp.def_rb_source_file("base64.rb", &include_bytes!("vendor/base64.rb")[..])?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct Base64;
