21515489ac4a18fb8dfb5a996becd671cdba5fcc
Ryan Lopopolo <rjl@hyperbo.la>
2020-06-20 08:22:05 -0700

    Expand the API of TypeRegistry to expose more of the underlying HashMap
    - Add type parameter for customizing BuildHasher
    - Expose capacity and build hasher constructors
    - Expose memory management APIs - reserve, shrink to fit
    - Add capacity, len, is_empty APIs
    - Return old values when calling insert
    - document that TypeRegistry is append only.
    - add doc comments
    - derive default, debug, partialeq, eq
    - Add iterator over items in the registry
    - Add contains<K>()

--- artichoke-backend/src/state/type_registry.rs
+++ artichoke-backend/src/state/type_registry.rs
@@ -1,48 +1,201 @@
+//! A [`HashMap`] that uses Rust types as keys.
+//!
+//! Register data associated with a type (e.g. struct, enum, or primitive).
+//!
+//! `TypeRegistry` stores values in a [`Box`] to ensure pointers to values
+//! in the registry are not invalidated.
+//!
+//! Artichoke uses this module to associate Rust implementations of Ruby Core
+//! and standard library with their underlying [class spec](crate::class::Spec)
+//! or [module spec](crate::module::Spec).
+
 use std::any::{Any, TypeId};
+use std::collections::hash_map::{RandomState, Values};
 use std::collections::HashMap;
-use std::fmt;
+use std::hash::BuildHasher;
+use std::iter::FusedIterator;
 
-pub struct TypeRegistry<T>(HashMap<TypeId, Box<T>>);
+/// An iterator of all associated values stored in the [`TypeRegistry`].
+#[derive(Debug, Clone)]
+pub struct Types<'a, T>(Values<'a, TypeId, Box<T>>);
 
-impl<T> Default for TypeRegistry<T>
-where
-    T: Default,
-{
-    fn default() -> Self {
-        Self(HashMap::default())
+impl<'a, T> ExactSizeIterator for Types<'a, T> {}
+
+impl<'a, T> FusedIterator for Types<'a, T> {}
+
+impl<'a, T> Iterator for Types<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let value = self.0.next()?;
+        Some(value.as_ref())
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+
+    fn count(self) -> usize
+    where
+        Self: Sized,
+    {
+        self.0.count()
     }
 }
 
-impl<T> fmt::Debug for TypeRegistry<T>
+/// A [`HashMap`] that uses Rust types as keys.
+///
+/// Register data associated with a type (e.g. struct, enum, or primitive).
+///
+/// `TypeRegistry` is append-only; values cannot be removed or modified, only
+/// replaced.
+///
+/// Artichoke uses this module to associate Rust implementations of Ruby Core
+/// and standard library with their underlying [class spec](crate::class::Spec)
+/// or [module spec](crate::module::Spec).
+#[derive(Default, Debug)]
+pub struct TypeRegistry<T, S = RandomState>(HashMap<TypeId, Box<T>, S>);
+
+impl<T, S> PartialEq for TypeRegistry<T, S>
 where
-    T: fmt::Debug,
+    T: PartialEq,
+    S: BuildHasher,
 {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("TypeRegistry")
-            .field("registry", &self.0)
-            .finish()
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
     }
 }
 
-impl<T> TypeRegistry<T> {
+impl<T, S> Eq for TypeRegistry<T, S>
+where
+    T: Eq,
+    S: BuildHasher,
+{
+}
+
+impl<'a, T, S> IntoIterator for &'a TypeRegistry<T, S> {
+    type Item = &'a T;
+    type IntoIter = Types<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.types()
+    }
+}
+
+impl<T> TypeRegistry<T, RandomState> {
+    /// Construct a new, empty `TypeRegistry`.
     #[must_use]
     pub fn new() -> Self {
         Self(HashMap::new())
     }
 
-    pub fn insert<K>(&mut self, spec: Box<T>)
+    /// Construct a new `TypeRegistry` with the given `capacity`.
+    #[must_use]
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self(HashMap::with_capacity(capacity))
+    }
+}
+
+impl<T, S> TypeRegistry<T, S> {
+    /// Construct a new `TypeRegistry` with the given `hash_builder`.
+    #[must_use]
+    pub fn with_hasher(hash_builder: S) -> Self {
+        Self(HashMap::with_hasher(hash_builder))
+    }
+
+    /// Construct a new `TypeRegistry` with the given `capacity` and
+    /// `hash_builder`.
+    #[must_use]
+    pub fn with_capacity_and_hasher(capacity: usize, hash_builder: S) -> Self {
+        Self(HashMap::with_capacity_and_hasher(capacity, hash_builder))
+    }
+
+    /// Returns the number of elements the registry can hold without
+    /// reallocating.
+    ///
+    /// This number is a lower bound; the `TypeRegistry` might be able to hold
+    /// more, but is guaranteed to be able to hold at least this many.
+    #[must_use]
+    pub fn capacity(&self) -> usize {
+        self.0.capacity()
+    }
+
+    /// An iterator of all associated values stored in the [`TypeRegistry`] in
+    /// arbitrary order.
+    #[must_use]
+    pub fn types(&self) -> Types<'_, T> {
+        Types(self.0.values())
+    }
+
+    /// Returns the number of elements in the registry.
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Returns true if the registry contains no elements.
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    /// Returns a reference to the registry's [`BuildHasher`].
+    #[must_use]
+    pub fn hasher(&self) -> &S {
+        self.0.hasher()
+    }
+}
+
+impl<T, S> TypeRegistry<T, S>
+where
+    S: BuildHasher,
+{
+    /// Returns true if the registry contains a value for the specified type.
+    #[must_use]
+    pub fn contains<K>(&self) -> bool
     where
         K: Any,
     {
         let key = TypeId::of::<K>();
-        self.0.insert(key, spec);
+        self.0.contains_key(&key)
     }
 
+    /// Inserts a type-value pair into the map.
+    ///
+    /// If the registry did not have this key present, [`None`] is returned.
+    ///
+    /// If the registry did have this key present, the value is updated, and the
+    /// old value is returned.
+    pub fn insert<K>(&mut self, spec: Box<T>) -> Option<Box<T>>
+    where
+        K: Any,
+    {
+        let key = TypeId::of::<K>();
+        self.0.insert(key, spec)
+    }
+
+    /// Returns a reference to the value corresponding to the type key.
+    #[must_use]
     pub fn get<K>(&self) -> Option<&T>
     where
         K: Any,
     {
         let key = TypeId::of::<K>();
-        self.0.get(&key).map(Box::as_ref)
+        let value = self.0.get(&key)?;
+        Some(value.as_ref())
+    }
+
+    /// Reserves `capacity` for at least additional more elements to be inserted
+    /// in the `TypeRegistry`. The collection may reserve more space to avoid
+    /// frequent reallocations.
+    pub fn reserve(&mut self, additional: usize) {
+        self.0.reserve(additional)
+    }
+
+    /// Shrinks the capacity of the map as much as possible. It will drop down
+    /// as much as possible while maintaining the internal rules and possibly
+    /// leaving some space in accordance with the resize policy.
+    pub fn shrink_to_fit(&mut self) {
+        self.0.shrink_to_fit()
     }
 }
