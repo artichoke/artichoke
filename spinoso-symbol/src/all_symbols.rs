9df872f3d0718845687b2be2c6f03546a8b53886
Ryan Lopopolo <rjl@hyperbo.la>
2020-08-11 06:32:01 -0700

    Improve docs and performance of AllSymbols iterator
    Add additional documentation to `AllSymbols`, the `InternerAllSymbols`
    extension trait, and `From` converters.
    
    Implement more functions in the `Iterator` implementation to delegate to
    optimized versions in `Range`. Manually implement `<AllSymbols as Iterator>::count
    to work around a limitation in `std`.
    
    Add tests for `AllSymbols` with `SYMBOL_RANGE_START` starting offsets.
    And document the maximum number of elements that can be yielded from an
    `AllSymbols` iterator.

--- spinoso-symbol/src/all_symbols.rs
+++ spinoso-symbol/src/all_symbols.rs
@@ -5,17 +5,17 @@ use core::ops::Range;
 
 use crate::Symbol;
 
-/// Expose a method for returning an iterator that returns all symbol
-/// identifiers stored in an [interner] as [`Symbol`]s.
+/// Extension trait to return an iterator that returns all symbol identifiers
+/// stored in an [interner] as [`Symbol`]s.
 ///
 /// The returned iterator yields [`Symbol`] as its item type.
 ///
-/// This function requires that the interner issues symbol identifiers as an
-/// [arithmetic progression] with a common difference of 1. The sequence of
-/// symbol identifiers must be representable by a [`Range<u32>`].
+/// Implementors of this trait must issue symbol identifiers as an [arithmetic
+/// progression] with a common difference of 1. The sequence of symbol
+/// identifiers must be representable by a [`Range<u32>`].
 ///
 /// This trait is automatically implemented for all types that implement
-/// [`Intern`].
+/// [`Intern`] from [`artichoke_core`].
 ///
 /// # Examples
 ///
@@ -86,11 +86,15 @@ pub trait InternerAllSymbols: Intern {
     /// [arithmetic progression] with a common difference of 1. The sequence of
     /// symbol identifiers must be representable by a [`Range<u32>`].
     ///
+    /// [`AllSymbols`] supports yielding up to `u32::MAX - 1` `Symbol`s.
+    ///
     /// # Examples
     ///
     /// See trait-level documentation for examples.
     ///
     /// [interner]: Intern
+    /// [arithmetic progression]: https://en.wikipedia.org/wiki/Arithmetic_progression
+    /// [`Range<u32>`]: core::ops::Range
     fn all_symbols(&self) -> AllSymbols;
 }
 
@@ -111,6 +115,7 @@ where
     T: Intern<Symbol = U>,
     U: Copy + Into<u32>,
 {
+    /// Construct a [`AllSymbols`] iterator from an interner.
     #[inline]
     fn from(interner: &T) -> Self {
         let min = T::SYMBOL_RANGE_START.into();
@@ -124,11 +129,14 @@ where
 ///
 /// This iterator yields [`Symbol`] as its item type.
 ///
+/// This iterator supports yielding up to `u32::MAX - 1` `Symbol`s.
+///
 /// This struct is created by the [`all_symbols`] method in the
-/// `InternerAllSymbols` trait.  See its documentation for more.
+/// [`InternerAllSymbols`] trait.  See its documentation for more.
 ///
 /// [interner]: Intern
 /// [`all_symbols`]: InternerAllSymbols::all_symbols
+#[must_use = "Iterator"]
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 #[cfg_attr(docsrs, doc(cfg(feature = "artichoke")))]
 pub struct AllSymbols(Range<u32>);
@@ -148,7 +156,34 @@ impl Iterator for AllSymbols {
 
     #[inline]
     fn count(self) -> usize {
-        self.0.count()
+        // Inline implementation of `Step::steps_between` since
+        // `<Range as Iterator>::count` is not specialized to use it for integer
+        // ranges.
+        if self.0.start <= self.0.end {
+            (self.0.end - self.0.start) as usize
+        } else {
+            0
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+
+    #[inline]
+    fn last(self) -> Option<Self::Item> {
+        self.0.last().map(Symbol::from)
+    }
+
+    #[inline]
+    fn min(mut self) -> Option<Self::Item> {
+        self.0.next().map(Symbol::from)
+    }
+
+    #[inline]
+    fn max(self) -> Option<Self::Item> {
+        self.0.max().map(Symbol::from)
     }
 }
 
@@ -167,3 +202,212 @@ impl DoubleEndedIterator for AllSymbols {
 impl ExactSizeIterator for AllSymbols {}
 
 impl FusedIterator for AllSymbols {}
+
+#[cfg(test)]
+mod tests {
+    use super::InternerAllSymbols;
+    use crate::Symbol;
+    use alloc::borrow::Cow;
+    use artichoke_core::intern::Intern;
+
+    #[derive(Default, Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    struct Interner(u32);
+
+    impl Intern for Interner {
+        type Symbol = u32;
+        type Error = &'static str;
+        const SYMBOL_RANGE_START: Self::Symbol = 0;
+
+        fn intern_bytes<T>(&mut self, symbol: T) -> Result<Self::Symbol, Self::Error>
+        where
+            T: Into<Cow<'static, [u8]>>,
+        {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn check_interned_bytes(&self, symbol: &[u8]) -> Result<Option<Self::Symbol>, Self::Error> {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn lookup_symbol(&self, symbol: Self::Symbol) -> Result<Option<&[u8]>, Self::Error> {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn symbol_count(&self) -> usize {
+            self.0 as usize
+        }
+    }
+
+    #[derive(Default, Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    struct OffByOneInterner(u32);
+
+    impl Intern for OffByOneInterner {
+        type Symbol = u32;
+        type Error = &'static str;
+        const SYMBOL_RANGE_START: Self::Symbol = 1;
+
+        fn intern_bytes<T>(&mut self, symbol: T) -> Result<Self::Symbol, Self::Error>
+        where
+            T: Into<Cow<'static, [u8]>>,
+        {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn check_interned_bytes(&self, symbol: &[u8]) -> Result<Option<Self::Symbol>, Self::Error> {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn lookup_symbol(&self, symbol: Self::Symbol) -> Result<Option<&[u8]>, Self::Error> {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn symbol_count(&self) -> usize {
+            self.0 as usize
+        }
+    }
+
+    #[derive(Default, Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    struct NonZeroInterner(u32);
+
+    impl Intern for NonZeroInterner {
+        type Symbol = u32;
+        type Error = &'static str;
+        const SYMBOL_RANGE_START: Self::Symbol = u32::MAX - 16;
+
+        fn intern_bytes<T>(&mut self, symbol: T) -> Result<Self::Symbol, Self::Error>
+        where
+            T: Into<Cow<'static, [u8]>>,
+        {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn check_interned_bytes(&self, symbol: &[u8]) -> Result<Option<Self::Symbol>, Self::Error> {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn lookup_symbol(&self, symbol: Self::Symbol) -> Result<Option<&[u8]>, Self::Error> {
+            let _ = symbol;
+            Err("not implemented")
+        }
+
+        fn symbol_count(&self) -> usize {
+            self.0 as usize
+        }
+    }
+
+    #[test]
+    fn zero_offset_count() {
+        let interner = Interner::default();
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), 0);
+
+        let interner = Interner(100);
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), 100);
+
+        let interner = Interner(u32::MAX);
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), u32::MAX as usize);
+    }
+
+    #[test]
+    fn zero_offset_yielded() {
+        let interner = Interner::default();
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), None);
+
+        let interner = Interner(5);
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), Some(Symbol::new(0)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(1)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(2)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(3)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(4)));
+        assert_eq!(all_symbols.next(), None);
+
+        let interner = Interner(u32::MAX);
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), Some(Symbol::new(0)));
+        assert_eq!(all_symbols.next_back(), Some(Symbol::new(u32::MAX - 1)));
+    }
+
+    #[test]
+    fn one_offset_count() {
+        let interner = OffByOneInterner::default();
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), 0);
+
+        let interner = OffByOneInterner(100);
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), 100);
+
+        let interner = OffByOneInterner(u32::MAX);
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), (u32::MAX - 1) as usize);
+    }
+
+    #[test]
+    fn one_offset_yielded() {
+        let interner = OffByOneInterner::default();
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), None);
+
+        let interner = OffByOneInterner(5);
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), Some(Symbol::new(1)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(2)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(3)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(4)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(5)));
+        assert_eq!(all_symbols.next(), None);
+
+        let interner = OffByOneInterner(u32::MAX);
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), Some(Symbol::new(1)));
+        assert_eq!(all_symbols.next_back(), Some(Symbol::new(u32::MAX - 1)));
+    }
+
+    #[test]
+    fn high_offset_count() {
+        let interner = NonZeroInterner::default();
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), 0);
+
+        let interner = NonZeroInterner(100);
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), 16);
+
+        let interner = NonZeroInterner(u32::MAX);
+        let all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.count(), 16);
+    }
+
+    #[test]
+    fn high_offset_yielded() {
+        let interner = NonZeroInterner::default();
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), None);
+
+        let interner = NonZeroInterner(5);
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), Some(Symbol::new(u32::MAX - 16)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(u32::MAX - 15)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(u32::MAX - 14)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(u32::MAX - 13)));
+        assert_eq!(all_symbols.next(), Some(Symbol::new(u32::MAX - 12)));
+        assert_eq!(all_symbols.next(), None);
+
+        let interner = NonZeroInterner(u32::MAX);
+        let mut all_symbols = interner.all_symbols();
+        assert_eq!(all_symbols.next(), Some(Symbol::new(u32::MAX - 16)));
+        assert_eq!(all_symbols.next_back(), Some(Symbol::new(u32::MAX - 1)));
+    }
+}
