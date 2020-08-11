5f4397a588ded2dc509c5311d06451a16b66f78e
Ryan Lopopolo <rjl@hyperbo.la>
2020-08-11 06:36:37 -0700

    Add additional PartialEq implementations for Symbol and u32

--- spinoso-symbol/src/eq.rs
+++ spinoso-symbol/src/eq.rs
@@ -27,3 +27,17 @@ impl PartialEq<&Symbol> for u32 {
         *self == other.id()
     }
 }
+
+impl PartialEq<u32> for &Symbol {
+    #[inline]
+    fn eq(&self, other: &u32) -> bool {
+        self.id() == *other
+    }
+}
+
+impl PartialEq<Symbol> for &u32 {
+    #[inline]
+    fn eq(&self, other: &Symbol) -> bool {
+        **self == other.id()
+    }
+}
