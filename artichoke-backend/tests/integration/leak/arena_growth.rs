d67743ed5a9b43c911c36d49ab10c6b6782180ef
Ryan Lopopolo <rjl@hyperbo.la>
2021-02-28 06:42:11 -0800

    Refactor artichoke-backend integration tests to reduce compile/run time
    This commit refactors the integration tests for `artichoke-backend` into
    a single crate. On my machine this reduces compilation time for
    `cargo test --workspace --no-run` by 3% and reduces the runtime of
    testing the entire workspace following a build with `cargo test --workspace`
    by 12%. These two improvements total about 9 seconds in saved build and
    run time.
    
    https://www.reddit.com/r/rust/comments/lto0qa/blog_post_delete_cargo_integration_tests/
    https://matklad.github.io/2021/02/27/delete-cargo-integration-tests.html
    
    This refactor takes an opportunity to unify crate-level attributes,
    remove `deny` attributes, and update the comments associated with the
    tests to refer to APIs and structs with their current names.

--- artichoke-backend/tests/integration/leak/arena_growth.rs
+++ artichoke-backend/tests/integration/leak/arena_growth.rs
@@ -1,23 +1,17 @@
-#![deny(clippy::all)]
-#![deny(clippy::pedantic)]
-
-//! This integration test checks for memory leaks that stem from improper
-//! handling of `mrb_state`.
-//!
-//! Checks for memory leaks stemming from improperly grabage collecting Ruby
-//! objects created in C functions, like the call to exc.inspect in
-//! [`ArtichokeApi::current_exception`].
-//!
-//! The test exposes a function that raises an exception with a 1MB `String`
-//! message. The test reuses one artichoke interpreter for all `ITERATIONS`.
-//!
-//! This test calls [`Value::to_s`] and [`Value::to_s_debug'] on a 1MB `String`.
-//!
-//! If resident memory increases more than 10MB during the test, we likely are
-//! leaking memory.
-//!
-//! This test fails before commit
-//! `a450ca7c458d0a4db6fdc60375d8c2c8482c85a7` with a fairly massive leak.
+// These integration test checks for memory leaks that stem from improper
+// handling of `mrb_state`.
+//
+// Checks for memory leaks stemming from improperly grabage collecting Ruby
+// objects created in C functions, like the call to exc.inspect in
+// [`Artichoke::last_error`].
+//
+// The test exposes a function that raises an exception with a 1MB `String`
+// message. The test reuses one artichoke interpreter for all `ITERATIONS`.
+//
+// This test calls [`Value::to_s`] and [`Value::to_s_debug'] on a 1MB `String`.
+//
+// This test fails before commit
+// `a450ca7c458d0a4db6fdc60375d8c2c8482c85a7` with a fairly massive leak.
 
 use artichoke_backend::prelude::*;
 
@@ -26,16 +20,11 @@ const ITERATIONS: usize = 100;
 #[test]
 fn unbounded_arena_growth_leak_current_exception() {
     let mut interp = artichoke_backend::interpreter().unwrap();
-    let code = r#"
-def bad_code
-  raise ArgumentError.new("n" * 1024 * 1024)
-end
-    "#;
+    let code = r#"def bad_code; raise ArgumentError.new("n" * 1024 * 1024); end"#;
     let _ = interp.eval(code.trim().as_bytes()).unwrap();
-    let expected = Some(vec![
-        Vec::from(&b"(eval):2:in bad_code"[..]),
-        Vec::from(&b"(eval):1"[..]),
-    ]);
+
+    let expected = Some(vec![b"(eval):1:in bad_code".to_vec(), b"(eval):1".to_vec()]);
+
     for _ in 0..ITERATIONS {
         let code = b"bad_code";
         let mut arena = interp.create_arena_savepoint().unwrap();
--- artichoke-backend/tests/integration/leak/boxing.rs
+++ artichoke-backend/tests/integration/leak/boxing.rs
@@ -1,22 +1,14 @@
-#![deny(clippy::all)]
-#![deny(clippy::pedantic)]
-
-//! This integration test checks for memory leaks that stem from not
-//! deallocating `ArtichokeApi` objects, embedded `mrb_value` data pointers, and
-//! linked Rust data.
-//!
-//! The test exposes a `Container` class to mruby which is initialized with a
-//! 1MB `String`. The test creates a new mruby interpreter, loads the Container
-//! class into the interpreter, and initializes one instance `ITERATIONS` times.
-//!
-//! If resident memory increases more than 10MB during the test, we likely are
-//! leaking memory.
-//!
-//! This test fails before commit
-//! `34ee3ddc1c5f4eb1d20f19dd772b0ca348391b2f` with a fairly massive leak.
-
-#[macro_use]
-extern crate artichoke_backend;
+// These integration tests check for memory leaks that stem from not
+// deallocating Rust objects that are embedded into `mrb_value` data pointers,
+// and the linked Rust-native structs.
+//
+// The test exposes a `Container` class to mruby which is initialized with a
+// 1MB `String`. The test creates a new mruby interpreter, loads the Container
+// class into the interpreter, and initializes one instance `ITERATIONS` times.
+//
+//
+// This test fails before commit
+// `34ee3ddc1c5f4eb1d20f19dd772b0ca348391b2f` with a fairly massive leak.
 
 use artichoke_backend::extn::prelude::*;
 
