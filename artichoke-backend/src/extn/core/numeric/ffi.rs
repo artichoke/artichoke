2dd5171c6138d0e3afe5fd5efbf8748e0ae98353
Ryan Lopopolo <rjl@hyperbo.la>
2021-03-07 18:44:59 -0800

    Remove Rust implementation of `mrb_int_value`
    As part of Integer reorganization, this function now has a different
    purpose and is implemented as an inline function in `mruby/mruby.h`.
    
    This extern "C" fn was added in #1121. The function in `numeric.c` it
    replaces has been removed in mruby 3.0.0.

--- artichoke-backend/src/extn/core/numeric/ffi.rs
+++ artichoke-backend/src/extn/core/numeric/ffi.rs
@@ -18,18 +18,3 @@ unsafe extern "C" fn mrb_to_flo(mrb: *mut sys::mrb_state, value: sys::mrb_value)
         Err(exception) => error::raise(guard, exception),
     }
 }
-
-// ```c
-// MRB_API mrb_value mrb_int_value(mrb_state *mrb, mrb_float f);
-// ```
-#[no_mangle]
-unsafe extern "C" fn mrb_int_value(mrb: *mut sys::mrb_state, f: sys::mrb_float) -> sys::mrb_value {
-    unwrap_interpreter!(mrb, to => guard);
-    let f = Float::with_f64(f);
-    let value = if let Some(fixnum) = f.try_into_fixnum() {
-        guard.convert(fixnum)
-    } else {
-        guard.convert_mut(f)
-    };
-    value.inner()
-}
