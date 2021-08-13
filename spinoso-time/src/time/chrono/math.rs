dd5291ac5544620708505471faac068b8fd739c0
Ryan Lopopolo <rjl@hyperbo.la>
2021-08-12 19:39:48 -0700

    Implement `Time#-` for `Time` arguments
    Allow computing the difference between two `Time` objects as a `Float`.
    
    This commit fleshes out `Time#-`'s trampoline implementation to prepare
    for other permissible argument types, but adds in `NotImplementedError`
    stubs.
    
    This commit bumps the version of `spinoso-time` crate to 0.2.0.

--- spinoso-time/src/time/chrono/math.rs
+++ spinoso-time/src/time/chrono/math.rs
@@ -1,4 +1,5 @@
 use crate::time::chrono::Time;
+use crate::NANOS_IN_SECOND;
 
 impl Time {
     /// Returns a new Time object, one second later than time.
@@ -26,6 +27,18 @@ impl Time {
             timestamp: timestamp + 1,
             sub_second_nanos,
             offset,
+        }
+    }
+
+    /// Returns the difference between two `Time` objects as an `f64` of seconds.
+    #[must_use]
+    #[allow(clippy::cast_precision_loss)]
+    pub fn difference(self, other: Self) -> f64 {
+        if let Some(sub_second_nanos) = self.sub_second_nanos.checked_sub(other.sub_second_nanos) {
+            (self.timestamp - other.timestamp) as f64 + f64::from(sub_second_nanos) / f64::from(NANOS_IN_SECOND)
+        } else {
+            let sub_second_nanos = NANOS_IN_SECOND - (other.sub_second_nanos - self.sub_second_nanos);
+            (self.timestamp - other.timestamp - 1) as f64 + f64::from(sub_second_nanos) / f64::from(NANOS_IN_SECOND)
         }
     }
 }
