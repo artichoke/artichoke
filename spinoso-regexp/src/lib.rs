84b3d197589991401f791cc570e23368dca05887
Ryan Lopopolo <rjl@hyperbo.la>
2021-09-05 20:53:05 -0700

    Add lint pragmas to `spinoso-regexp` crate

--- spinoso-regexp/src/lib.rs
+++ spinoso-regexp/src/lib.rs
@@ -1,4 +1,24 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::cargo)]
+#![allow(clippy::option_if_let_else)]
+#![allow(unknown_lints)]
+// TODO: warn on missing docs once crate is API-complete.
+// #![warn(missing_docs)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_copy_implementations)]
+#![warn(rust_2018_idioms)]
 #![warn(rust_2021_compatibility)]
+#![warn(trivial_casts, trivial_numeric_casts)]
+#![warn(unused_qualifications)]
+#![warn(variant_size_differences)]
+#![forbid(unsafe_code)]
+// Enable feature callouts in generated documentation:
+// https://doc.rust-lang.org/beta/unstable-book/language-features/doc-cfg.html
+//
+// This approach is borrowed from tokio.
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_alias))]
 
 // Ensure code blocks in README.md compile
 #[cfg(doctest)]
