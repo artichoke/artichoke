517e989ec4d4f6dcea7416a36f69663145ff2644
Ryan Lopopolo <rjl@hyperbo.la>
2021-09-13 06:22:12 -0700

    Complete docs for `ClassRegistry` and `ModuleRegistry`
    In `artichoke-core`.

--- artichoke-core/src/class_registry.rs
+++ artichoke-core/src/class_registry.rs
@@ -1,5 +1,11 @@
+//! Define and store class specs on an interpreter.
+
 use core::any::Any;
 
+/// Define and store class specs on an interpreter.
+///
+/// A class spec is a static set of information the interpreter requires to
+/// define a Ruby `Class` object.
 pub trait ClassRegistry {
     /// Concrete value type for boxed Ruby values.
     type Value;
@@ -12,9 +18,11 @@ pub trait ClassRegistry {
 
     /// Create a class definition bound to a Rust type `T`.
     ///
-    /// Class definitions have the same lifetime as the
-    /// [`State`](crate::state::State) because the class def owns the
-    /// `mrb_data_type` for the type, which must be long-lived.
+    /// Class definitions have the same lifetime as the interpreter.
+    ///
+    /// # Errors
+    ///
+    /// If the class registry state is inaccessible, an error is returned.
     fn def_class<T>(&mut self, spec: Self::Spec) -> Result<(), Self::Error>
     where
         T: Any;
@@ -23,11 +31,19 @@ pub trait ClassRegistry {
     ///
     /// This function returns `None` if type `T` has not had a class spec
     /// registered for it using [`ClassRegistry::def_class`].
+    ///
+    /// # Errors
+    ///
+    /// If the class registry state is inaccessible, an error is returned.
     fn class_spec<T>(&self) -> Result<Option<&Self::Spec>, Self::Error>
     where
         T: Any;
 
     /// Retrieve whether a class definition exists from the state bound to Rust type `T`.
+    ///
+    /// # Errors
+    ///
+    /// If the class registry state is inaccessible, an error is returned.
     fn is_class_defined<T>(&self) -> bool
     where
         T: Any,
@@ -35,10 +51,32 @@ pub trait ClassRegistry {
         matches!(self.class_spec::<T>(), Ok(Some(_)))
     }
 
+    /// Retrieve a boxed Ruby value containing a `Class` object for the `Class`
+    /// bound to Rust type `T`.
+    ///
+    /// If the interpreter cannot find or load a class associated with `T`,
+    /// `Ok(None)` is returned.
+    ///
+    /// # Errors
+    ///
+    /// If the class registry state is inaccessible, an error is returned.
     fn class_of<T>(&mut self) -> Result<Option<Self::Value>, Self::Error>
     where
         T: Any;
 
+    /// Create a new instance of the class bound to the Rust type `T`.
+    ///
+    /// This method resolves the class referenced by the type `T` and calls
+    /// `new` on this Ruby class with the given arguments.
+    ///
+    /// If the interpreter cannot find or load a class associated with `T`,
+    /// `Ok(None)` is returned.
+    ///
+    /// # Errors
+    ///
+    /// If the class registry state is inaccessible, an error is returned.
+    ///
+    /// If an exception is raised on the interpreter, then an error is returned.
     fn new_instance<T>(&mut self, args: &[Self::Value]) -> Result<Option<Self::Value>, Self::Error>
     where
         T: Any;
--- artichoke-core/src/module_registry.rs
+++ artichoke-core/src/module_registry.rs
@@ -1,5 +1,11 @@
+//! Define and store module specs on an interpreter.
+
 use core::any::Any;
 
+/// Define and store module specs on an interpreter.
+///
+/// A module spec is a static set of information the interpreter requires to
+/// define a Ruby `Module` object.
 pub trait ModuleRegistry {
     /// Concrete value type for boxed Ruby values.
     type Value;
@@ -12,9 +18,11 @@ pub trait ModuleRegistry {
 
     /// Create a module definition bound to a Rust type `T`.
     ///
-    /// Module definitions have the same lifetime as the interpreter because the
-    /// module def owns the `mrb_data_type` for the type, which must be
-    /// long-lived.
+    /// Module definitions have the same lifetime as the interpreter.
+    ///
+    /// # Errors
+    ///
+    /// If the module registry state is inaccessible, an error is returned.
     fn def_module<T>(&mut self, spec: Self::Spec) -> Result<(), Self::Error>
     where
         T: Any;
@@ -23,11 +31,23 @@ pub trait ModuleRegistry {
     ///
     /// This function returns `None` if type `T` has not had a module spec
     /// registered for it using [`ModuleRegistry::def_module`].
+    ///
+    /// # Errors
+    ///
+    /// If the module registry state is inaccessible, an error is returned.
     fn module_spec<T>(&self) -> Result<Option<&Self::Spec>, Self::Error>
     where
         T: Any;
 
-    /// Retrieve whether a module definition exists from the interpreter bound to Rust type `T`
+    /// Retrieve whether a module definition exists from the interpreter bound
+    /// to Rust type `T`
+    ///
+    /// If the interpreter cannot find or load a module associated with `T`,
+    /// `Ok(None)` is returned.
+    ///
+    /// # Errors
+    ///
+    /// If the module registry state is inaccessible, an error is returned.
     fn is_module_defined<T>(&self) -> bool
     where
         T: Any,
@@ -35,6 +55,15 @@ pub trait ModuleRegistry {
         matches!(self.module_spec::<T>(), Ok(Some(_)))
     }
 
+    /// Retrieve a boxed Ruby value containing a `Module` object for the
+    /// `Module` bound to Rust type `T`.
+    ///
+    /// If the interpreter cannot find or load a module associated with `T`,
+    /// `Ok(None)` is returned.
+    ///
+    /// # Errors
+    ///
+    /// If the module registry state is inaccessible, an error is returned.
     fn module_of<T>(&mut self) -> Result<Option<Self::Value>, Self::Error>
     where
         T: Any;
