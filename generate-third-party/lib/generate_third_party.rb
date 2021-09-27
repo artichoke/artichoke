4294bd44509af6aefd43e562f8e9406bca65f123
Ryan Lopopolo <rjl@hyperbo.la>
2021-09-26 18:55:45 -0700

    Add local `generate_third_party` gem
    Add a local RubyGem that installs executables used to generate
    `THIRDPARTY` flat files for inclusion in nightly build artifacts.
    
    This gem exposes three commands:
    
    - `generate-third-party-text-file`: generate a `THIRDPARTY` text file
      that is a flat listing of all dependencies, versions, and their
      licesnes for all targets Artichoke supports.
    - `generate-third-party-text-file-single-target`: generate a
      `THIRDPARTY` text file that is a flat listing of all dependencies,
      versions, and their licenses for a single target triple.
    - `generate-third-party-html`: generate an HTML page that lists all
      dependencies, bersions, and their licesnes for all targets Artichoke
      supports. This output is meant to be embedded in
      www.artichokeruby.org.
    
    Two additional licenses are included for C libraries Artichoke
    transitively depends on through Rust crates:
    
    - Oniguruma 6.9.7 (via `onig` -> `onig_sys`)
    - mruby 3.0.0 (via `artichoke-backend`)

---
+++ generate-third-party/lib/generate_third_party.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'generate_third_party/cargo_about'
+require_relative 'generate_third_party/deps'
+
+require_relative 'generate_third_party/all_targets'
+require_relative 'generate_third_party/one_target'
---
+++ generate-third-party/lib/generate_third_party/cargo_about.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+require 'open3'
+require 'shellwords'
+require 'stringio'
+
+module Artichoke
+  module Generate
+    module ThirdParty
+      class CargoAbout
+        def self.present?
+          _out, status = Open3.capture2e('cargo about --version')
+
+          status.success?
+        end
+
+        def initialize(config:, template: nil)
+          template = File.join(__dir__, 'cargo_about', 'about.hbs') if template.nil?
+
+          @template = template
+          @config = config
+        end
+
+        def manifest_path
+          path = File.join(__dir__, '..', '..', '..', 'Cargo.toml')
+          File.expand_path(path)
+        end
+
+        def root_config_path
+          path = File.join(__dir__, '..', '..', '..', 'about.toml')
+          File.expand_path(path)
+        end
+
+        def invoke
+          # setup `about.toml`
+          config = File.read(@config)
+          File.open(root_config_path, 'w') do |f|
+            f.write config
+          end
+
+          command = ['cargo', 'about', '--manifest-path', manifest_path, 'generate', @template]
+          out, err, status = Open3.capture3(command.shelljoin)
+
+          File.delete(root_config_path)
+
+          unless status.success?
+            warn 'Generate failed'
+            warn err
+            exit 1
+          end
+
+          Deps.parse(out)
+        end
+
+        def self.third_party_flatfile
+          cmd = CargoAbout.new(
+            cwd: File.join(__dir__, 'all_targets')
+          )
+
+          deps = cmd.invoke
+          deps.sort_by!(&:name)
+
+          s = StringIO.new
+          first = true
+          deps.each do |dep|
+            s.puts unless first
+
+            s.puts "#{dep.name} #{dep.version}"
+            s.puts ''
+            s.puts dep.url
+            s.puts
+            s.puts dep.license_full_text
+
+            first = false
+          end
+        end
+      end
+    end
+  end
+end
