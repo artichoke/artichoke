081357dc8ff1e62f4419cd8096c3851dee657c81
Mason Fischer <mason@kissr.co>
2021-11-06 13:03:56 -0400

    Make artichoke_core::hash::BuildHasher generic
    .. over std::hash::BuildHasher
    
    Other changes:
    * Return a referece from BuildHasher#build_hasher

--- artichoke-backend/src/hash.rs
+++ artichoke-backend/src/hash.rs
@@ -4,11 +4,11 @@ use std::collections::hash_map::RandomState;
 use crate::ffi::InterpreterExtractError;
 use crate::Artichoke;
 
-impl BuildHasher for Artichoke {
+impl BuildHasher<RandomState> for Artichoke {
     type Error = Error;
 
-    fn build_hasher(&mut self) -> Result<RandomState, Self::Error> {
+    fn build_hasher(&mut self) -> Result<&RandomState, Self::Error> {
         let state = self.state.as_deref_mut().ok_or_else(InterpreterExtractError::new)?;
-        Ok(state.hash_builder.clone())
+        Ok(&state.hash_builder)
     }
 }
--- artichoke-core/src/hash.rs
+++ artichoke-core/src/hash.rs
@@ -1,11 +1,9 @@
 //! Build hashers and hash values
 
-use std::collections::hash_map::RandomState;
-
 /// A trait for creating instances of RandomState
-pub trait BuildHasher {
+pub trait BuildHasher<B: std::hash::BuildHasher> {
     /// Concrete error type for errors encountered when outputting hash errors.
     type Error;
     /// Build a RandomState hasher
-    fn build_hasher(&mut self) -> Result<RandomState, Self::Error>;
+    fn build_hasher(&mut self) -> Result<&B, Self::Error>;
 }
