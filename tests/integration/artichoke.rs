9991575d43ccfe47c2c136c1c8f4029d39b3e57d
Ben Naylor <nayben@gmail.com>
2021-12-28 21:33:29 +0100

    Better test harness is better (we can now do require_relative etc)

--- tests/integration/artichoke.rs
+++ tests/integration/artichoke.rs
@@ -1,24 +1,18 @@
-use std::fs;
-use std::path::Path;
-
 use crate::run;
 
+const BINARY: &str = "artichoke";
+const FIXTURES_ROOT: &str = "./tests/integration/apps/";
+
 #[test]
-fn test_ruby_apps() -> Result<(), String> {
-    let binary_name = "artichoke";
-    let app_paths = Path::new("./tests/integration/apps");
+fn test_hello_world() {
+    let app_name = "hello_world.rb";
+    let path = format!("{}{}", FIXTURES_ROOT, app_name);
+    insta::assert_debug_snapshot!(run(BINARY, vec![&path]));
+}
 
-    let paths = fs::read_dir(app_paths).unwrap();
-
-    for path in paths {
-        let file_path = path.unwrap().path();
-
-        let str_path = file_path.clone().into_os_string().into_string().unwrap();
-        let args = vec![&*str_path];
-
-        let snapshot_name = format!("ruby_app__{}", file_path.strip_prefix(app_paths).unwrap().display());
-        insta::assert_debug_snapshot!(snapshot_name, run(binary_name, args))
-    }
-
-    Ok(())
+#[test]
+fn test_fizz_buzz() {
+    let app_name = "fizz_buzz.rb";
+    let path = format!("{}{}", FIXTURES_ROOT, app_name);
+    insta::assert_debug_snapshot!(run(BINARY, vec![&path]));
 }
