8717829a3fc3d668c97e1310c6d5d654468ba772
Ben Naylor <nayben@gmail.com>
2021-12-29 13:59:03 +0100

    Add test for help

--- tests/integration/artichoke.rs
+++ tests/integration/artichoke.rs
@@ -3,6 +3,18 @@ use crate::run;
 const BINARY: &str = "artichoke";
 const FIXTURES_ROOT: &str = "./tests/integration/fixtures/";
 
+#[cfg(target_family = "windows")]
+#[test]
+fn help_windows() {
+    insta::assert_toml_snapshot!(run(BINARY, &["--help"]).unwrap());
+}
+
+#[cfg(target_family = "unix")]
+#[test]
+fn help_unix() {
+    insta::assert_toml_snapshot!(run(BINARY, &["--help"]).unwrap());
+}
+
 #[test]
 fn test_hello_world() {
     let app_name = "hello_world.rb";
