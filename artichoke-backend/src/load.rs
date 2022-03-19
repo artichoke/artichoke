ce6a06cf8fd824887df34b6fb161b41f459ffc54
Ryan Lopopolo <rjl@hyperbo.la>
2022-03-19 14:01:36 -0700

    Fix Kernel#load so it only tries the exact path as given
    Fixes #1730.

--- artichoke-backend/src/load.rs
+++ artichoke-backend/src/load.rs
@@ -5,9 +5,11 @@ use std::path::Path;
 use artichoke_core::eval::Eval;
 use artichoke_core::file::File;
 use artichoke_core::load::{LoadSources, Loaded, Required};
+use spinoso_exception::LoadError;
 
 use crate::error::Error;
 use crate::ffi::InterpreterExtractError;
+use crate::platform_string::os_str_to_bytes;
 use crate::Artichoke;
 
 const RUBY_EXTENSION: &str = "rb";
@@ -84,37 +86,26 @@ impl LoadSources for Artichoke {
         P: AsRef<Path>,
     {
         let path = path.as_ref();
-        let mut alternate_path;
-        let path = {
-            let state = self.state.as_deref_mut().ok_or_else(InterpreterExtractError::new)?;
-            // Require Rust `File` first because an File may define classes and
-            // modules with `LoadSources` and Ruby files can require arbitrary
-            // other files, including some child sources that may depend on these
-            // module definitions.
-            match state.load_path_vfs.get_extension(path) {
-                Some(hook) => {
-                    // dynamic, Rust-backed `File` require
-                    hook(self)?;
-                    path
+        let state = self.state.as_deref_mut().ok_or_else(InterpreterExtractError::new)?;
+        // Require Rust `File` first because an File may define classes and
+        // modules with `LoadSources` and Ruby files can require arbitrary
+        // other files, including some child sources that may depend on these
+        // module definitions.
+        if let Some(hook) = state.load_path_vfs.get_extension(path) {
+            // dynamic, Rust-backed `File` require
+            hook(self)?;
+        }
+        let contents = self
+            .read_source_file_contents(path)
+            .map_err(|_| {
+                let mut message = b"cannot load such file".to_vec();
+                if let Ok(bytes) = os_str_to_bytes(path.as_os_str()) {
+                    message.extend_from_slice(b" -- ");
+                    message.extend_from_slice(bytes);
                 }
-                None if matches!(path.extension(), Some(ext) if *ext == *OsStr::new(RUBY_EXTENSION)) => path,
-                None => {
-                    alternate_path = path.to_owned();
-                    alternate_path.set_extension(RUBY_EXTENSION);
-                    if let Some(hook) = state.load_path_vfs.get_extension(&alternate_path) {
-                        // dynamic, Rust-backed `File` require
-                        hook(self)?;
-                        // This ensures that if we load the hook at an alternate
-                        // path, we use that alternate path to load the Ruby
-                        // source.
-                        &alternate_path
-                    } else {
-                        path
-                    }
-                }
-            }
-        };
-        let contents = self.read_source_file_contents(path)?.into_owned();
+                LoadError::from(message)
+            })?
+            .into_owned();
         self.eval(contents.as_ref())?;
         Ok(Loaded::Success)
     }
@@ -293,7 +284,7 @@ LoadSources::Counter.instance.inc!
         assert_eq!(count, 11);
 
         let exc = interp.load_source("./counter").unwrap_err();
-        assert_eq!(exc.message().as_bstr(), b"cannot load such file".as_bstr());
+        assert_eq!(exc.message().as_bstr(), b"cannot load such file -- ./counter".as_bstr());
         assert_eq!(exc.name(), "LoadError");
     }
 }
