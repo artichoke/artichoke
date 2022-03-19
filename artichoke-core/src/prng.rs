a501cb70b70a03683655a6a49c4f0f363944a551
Ryan Lopopolo <rjl@hyperbo.la>
2022-03-19 11:55:55 -0700

    Expand trait-level documentation for `artichoke_core::prng::Prng`

--- artichoke-core/src/prng.rs
+++ artichoke-core/src/prng.rs
@@ -1,10 +1,18 @@
 //! Interpreter global pseudorandom number generator.
 
-/// Interpreter global pseudorandom number generator.
+/// Interpreter global pseudorandom number generator (PRNG).
 ///
-/// Implementors of this trait back the `Random::DEFAULT` PRNG.
+/// Implementations of this trait may be used as the backing pseudorandom number
+/// generator for [`Random::DEFAULT`].
+///
+/// The PRNG used to implement this trait is **not** required to be
+/// cryptographically secure. For example, MRI's PRNG is a variant of Mersenne
+/// Twister.
+///
+/// [`Random::DEFAULT`]: https://ruby-doc.org/core-2.6.3/Random.html#DEFAULT
 pub trait Prng {
-    /// Concrete type for PRNG errors.
+    /// Concrete type for errors when retrieving the pseudorandom number
+    /// generator.
     type Error;
 
     /// Concrete type for the interpreter pseudorandom number generator.
