703e0b8f980d0584e46975b718eae943be43c8cd
Ryan Lopopolo <rjl@hyperbo.la>
2022-03-25 22:50:25 -0700

    Remove obsolete mruby build scripts and config

--- artichoke-backend/scripts/noop.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-# This script is an empty script that always exits successfully.
-#
-# This script is meant to emulate the Unix command `true`. `noop.rb` exits with
-# a success status code and swallows all arguments.
-#
-# This script is a compatibility shim to enable the NOOP mruby build on Windows.
