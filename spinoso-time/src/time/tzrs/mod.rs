90db289446798d44f82b61740d156a672316c354
Ben Naylor <nayben@gmail.com>
2022-05-22 15:03:04 +0200

    Add functions for mutating the timezone of the _time_ in place

--- spinoso-time/src/time/tzrs/mod.rs
+++ spinoso-time/src/time/tzrs/mod.rs
@@ -1,5 +1,5 @@
 use tz::datetime::DateTime;
-use tz::timezone::TimeZoneRef;
+use tz::timezone::{TimeZoneRef, LocalTimeType};
 use tzdb::local_tz;
 use tzdb::time_zone::etc::{GMT};
 
@@ -302,9 +302,89 @@ impl Time {
 
 // Mutators
 impl Time {
-    // for Time#localtime, Time#[gmtime|utc]
-    pub fn set_timezone(&mut self) {
-        todo!()
+    /// Converts _time_ to the provided time zone, modifying the receiver
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// assert!(false)
+    /// ```
+    pub fn set_timezone(&mut self, tz: TimeZoneRef<'static>) {
+        // TODO: ProjectionErrors from project() are propogated from `Time::from_timespec` which
+        // generally come from an error on checked_add overflowing the seconds component of the
+        // unix time. Need to decide how to handle these kinds of errors (e.g. panic?)
+        match self.inner.project(tz) {
+            Ok(time) => self.inner = time,
+            Err(_) => (),
+        }
+    }
+
+    /// Converts _time_ to local time (using the local time zone in effective at the creation time
+    /// of _time_) modifying the receiver
+    ///
+    /// Can be used to implement [`Time#localtime`] without a parameter
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use spinoso_time::Time;
+    /// let mut now = Time::utc(2022, 7, 8, 12, 34, 56, 0);
+    /// let now_utc_unix = now.to_int();
+    /// now.set_local();
+    /// let now_local_unix = now.to_int();
+    /// assert_eq!(now_utc_unix, now_local_unix);
+    /// ```
+    ///
+    /// [`Time#localtime`]: https://ruby-doc.org/core-2.6.3/Time.html#method-i-localtime
+    pub fn set_local(&mut self) {
+        let tz = local_time_zone();
+        self.set_timezone(tz)
+    }
+
+    /// Converts _time_ to UTC (GMT), modifying the receiver
+    ///
+    /// Can be used to implement [`Time#utc`] and [`Time#gmtime`]
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use spinoso_time::Time;
+    /// let mut now = Time::local(2022, 7, 8, 12, 34, 56, 0);
+    /// let now_local_unix = now.to_int();
+    /// now.set_local();
+    /// let now_utc_unix = now.to_int();
+    /// assert_eq!(now_local_unix, now_utc_unix);
+    /// ```
+    ///
+    /// [`Time#utc`]: https://ruby-doc.org/core-2.6.3/Time.html#method-i-utc
+    /// [`Time#gmtime`]: https://ruby-doc.org/core-2.6.3/Time.html#method-i-gmtime
+    pub fn set_utc(&mut self) {
+        self.set_timezone(UTC)
+    }
+
+    /// Converts _time_ to the GMT time zone with the provided offset
+    ///
+    /// Can be used to implement [`Time#localtime`] with an offset parameter
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use spinoso_time::Time;
+    /// let mut now = Time::utc(2022, 7, 8, 12, 34, 56, 0);
+    /// assert!(now.is_utc());
+    /// now.set_offset_from_utc(3600);
+    /// assert!(!now.is_utc());
+    /// assert_eq!(now.utc_offset(), 3600);
+    /// ```
+    ///
+    /// [`Time#localtime`]: https://ruby-doc.org/core-2.6.3/Time.html#method-i-localtime
+    pub fn set_offset_from_utc(&mut self, offset: i32) {
+        let local_time_type = LocalTimeType::new(offset, false, Some(b"GMT")).unwrap();
+        self.inner = DateTime::from_timespec_and_local(
+            self.to_int(),
+            self.nanoseconds(),
+            local_time_type
+        ).unwrap()
     }
 
     pub fn round(&mut self, digits: u32) {
