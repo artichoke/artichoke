2164ededde9aea3bdd28c6a69d3b61bd7a264ee0
Ryan Lopopolo <rjl@hyperbo.la>
2022-07-22 16:02:56 -0700

    Fixup array args handling
    Move implicit conversion to int into "not a range" branch and propagate
    implicit conversion errors.

--- artichoke-backend/src/extn/core/array/args.rs
+++ artichoke-backend/src/extn/core/array/args.rs
@@ -83,27 +83,28 @@ pub fn element_assignment(
             write!(&mut message, "negative length ({})", slice_len).map_err(WriteError::from)?;
             Err(IndexError::from(message).into())
         }
-    } else if let Ok(index) = implicitly_convert_to_int(interp, first) {
-        if let Ok(index) = usize::try_from(index) {
-            Ok((index, None, second))
-        } else {
-            let idx = index
-                .checked_neg()
-                .and_then(|index| usize::try_from(index).ok())
-                .and_then(|index| len.checked_sub(index));
-            if let Some(idx) = idx {
-                Ok((idx, None, second))
-            } else {
-                let mut message = String::new();
-                write!(&mut message, "index {} too small for array; minimum: -{}", index, len)
-                    .map_err(WriteError::from)?;
-                Err(IndexError::from(message).into())
-            }
-        }
     } else {
         let rangelen = i64::try_from(len).map_err(|_| Fatal::from("Range length exceeds Integer max"))?;
         match first.is_range(interp, rangelen)? {
-            None => {}
+            None => {
+                let index = implicitly_convert_to_int(interp, first)?;
+                if let Ok(index) = usize::try_from(index) {
+                    Ok((index, None, second))
+                } else {
+                    let idx = index
+                        .checked_neg()
+                        .and_then(|index| usize::try_from(index).ok())
+                        .and_then(|index| len.checked_sub(index));
+                    if let Some(idx) = idx {
+                        Ok((idx, None, second))
+                    } else {
+                        let mut message = String::new();
+                        write!(&mut message, "index {} too small for array; minimum: -{}", index, len)
+                            .map_err(WriteError::from)?;
+                        Err(IndexError::from(message).into())
+                    }
+                }
+            }
             // ```
             // [3.1.2] > a = []
             // => []
@@ -114,46 +115,12 @@ pub fn element_assignment(
             //         from /usr/local/var/rbenv/versions/3.1.2/bin/irb:25:in `load'
             //         from /usr/local/var/rbenv/versions/3.1.2/bin/irb:25:in `<main>'
             // ```
-            Some(protect::Range::Out) => return Err(RangeError::with_message("out of range").into()),
+            Some(protect::Range::Out) => Err(RangeError::with_message("out of range").into()),
             Some(protect::Range::Valid { start, len }) => {
                 let start = usize::try_from(start)
                     .unwrap_or_else(|_| unimplemented!("should throw RangeError (-11..1 out of range)"));
                 let len = usize::try_from(len).unwrap_or_else(|_| unreachable!("Range can't have negative length"));
-                return Ok((start, Some(len), second));
-            }
-        };
-        let start = first.funcall(interp, "begin", &[], None)?;
-        let start = implicitly_convert_to_int(interp, start)?;
-        let end = first.funcall(interp, "last", &[], None)?;
-        let end = implicitly_convert_to_int(interp, end)?;
-        // TODO: This conditional is probably not doing the right thing
-        if start + (end - start) < 0 {
-            let mut message = String::new();
-            write!(&mut message, "{}..{} out of range", start, end).map_err(WriteError::from)?;
-            return Err(RangeError::from(message).into());
-        }
-        match (usize::try_from(start), usize::try_from(end)) {
-            (Ok(start), Ok(end)) => Ok((start, end.checked_sub(start), second)),
-            (Err(_), Ok(end)) => {
-                let pos = start
-                    .checked_neg()
-                    .and_then(|start| usize::try_from(start).ok())
-                    .and_then(|start| len.checked_sub(start));
-                if let Some(start) = pos {
-                    Ok((start, end.checked_sub(start), second))
-                } else {
-                    let mut message = String::new();
-                    write!(&mut message, "index {} too small for array; minimum: -{}", start, len)
-                        .map_err(WriteError::from)?;
-                    Err(IndexError::from(message).into())
-                }
-            }
-            (Ok(start), Err(_)) => Ok((start, None, second)),
-            (Err(_), Err(_)) => {
-                let mut message = String::new();
-                write!(&mut message, "index {} too small for array; minimum: -{}", start, len)
-                    .map_err(WriteError::from)?;
-                Err(IndexError::from(message).into())
+                Ok((start, Some(len), second))
             }
         }
     }
