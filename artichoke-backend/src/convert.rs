267aab6f719f1d001ee6b1b70562575249d57b19
Ryan Lopopolo <rjl@hyperbo.la>
2022-08-10 22:30:40 -0700

    Add conversion function for `f64` to `i64`
    Ensure the value is finite, non-NaN, and in range.
    
    Round toward zero.

--- artichoke-backend/src/convert.rs
+++ artichoke-backend/src/convert.rs
@@ -16,12 +16,14 @@ mod boxing;
 mod bytes;
 mod fixnum;
 mod float;
+mod float_to_int;
 mod hash;
 mod implicit;
 mod nilable;
 mod string;
 
 pub use boxing::{BoxUnboxVmValue, HeapAllocated, HeapAllocatedData, Immediate, UnboxedValueGuard};
+pub use float_to_int::float_to_int;
 pub use implicit::{
     implicitly_convert_to_int, implicitly_convert_to_nilable_string, implicitly_convert_to_spinoso_string,
     implicitly_convert_to_string,
