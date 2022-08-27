a7b56059e7e549ecc898b3402a53aef1d5254aba
Ryan Lopopolo <rjl@hyperbo.la>
2022-08-27 13:55:40 -0700

    Fix broken intradoc links in scolapasta-path
    This was missed when splitting these functions out of mezzaluna-feature-loader.

--- scolapasta-path/src/paths.rs
+++ scolapasta-path/src/paths.rs
@@ -60,8 +60,8 @@ pub fn memory_loader_ruby_load_path() -> &'static Path {
 /// Explicit relative paths start with `.` or `..` followed immediately by a
 /// directory separator.
 ///
-/// Some [loaders] have special handling for explicit relative paths where
-/// explicit relative paths are resolved relative to the process's [current
+/// Some Ruby source loaders have special handling for explicit relative paths
+/// where explicit relative paths are resolved relative to the process's [current
 /// working directory] rather than the load path.
 ///
 /// # Compatibility
@@ -99,7 +99,6 @@ pub fn memory_loader_ruby_load_path() -> &'static Path {
 /// }
 /// ```
 ///
-/// [loaders]: crate::loaders
 /// [current working directory]: std::env::current_dir
 /// [reference implementation]: https://github.com/artichoke/ruby/blob/v3_0_2/file.c#L6287-L6293
 #[must_use]
@@ -114,8 +113,8 @@ pub fn is_explicit_relative<P: AsRef<Path>>(path: P) -> bool {
 /// Explicit relative paths start with `.` or `..` followed immediately by a
 /// directory separator.
 ///
-/// Some [loaders] have special handling for explicit relative paths where
-/// explicit relative paths are resolved relative to the process's [current
+/// Some Ruby source loaders have special handling for explicit relative paths
+/// where explicit relative paths are resolved relative to the process's [current
 /// working directory] rather than the load path.
 ///
 /// # Usage
@@ -154,7 +153,6 @@ pub fn is_explicit_relative<P: AsRef<Path>>(path: P) -> bool {
 /// }
 /// ```
 ///
-/// [loaders]: crate::loaders
 /// [current working directory]: std::env::current_dir
 /// [ruby-string]: https://ruby-doc.org/core-3.1.2/String.html
 /// [reference implementation]: https://github.com/artichoke/ruby/blob/v3_0_2/file.c#L6287-L6293
