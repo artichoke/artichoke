927be5c53b6d0fcd8d39f94201f6273a0ba0ddb5
Ryan Lopopolo <rjl@hyperbo.la>
2022-08-27 13:09:22 -0700

    Remove clippy allow pragma from scolapasta-aref

--- scolapasta-aref/src/lib.rs
+++ scolapasta-aref/src/lib.rs
@@ -1,7 +1,6 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 #![warn(clippy::cargo)]
-#![cfg_attr(test, allow(clippy::non_ascii_literal))]
 #![allow(unknown_lints)]
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
