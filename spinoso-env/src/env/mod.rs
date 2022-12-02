6cfcfdd015a01d10a8897454f833f97c40b3d085
Ryan Lopopolo <rjl@hyperbo.la>
2022-12-01 23:04:29 -0800

    Fix intradoc link resolution errors when building spinoso-env with no default features

--- spinoso-env/src/env/mod.rs
+++ spinoso-env/src/env/mod.rs
@@ -1,17 +1,17 @@
-//! ENV is a hash-like accessor for environment variables.
-//!
-//! This module contains implementations of an environ accessor and mutator.
-//!
-//! [`Memory`] is based on a Rust [`HashMap`] and offers a virtualized `ENV` API
-//! that cannot modify the host system.
-//!
-//! [`System`] is based on [`env::var_os`] and directly accesses and modifies
-//! the host system environ via platform-specific APIs.
-//!
-//! [`Memory`]: memory::Memory
-//! [`HashMap`]: std::collections::HashMap
-//! [`System`]: system::System
-//! [`env::var_os`]: std::env::var_os
+// ENV is a hash-like accessor for environment variables.
+//
+// This module contains implementations of an environ accessor and mutator.
+//
+// [`Memory`] is based on a Rust [`HashMap`] and offers a virtualized `ENV` API
+// that cannot modify the host system.
+//
+// [`System`] is based on [`env::var_os`] and directly accesses and modifies
+// the host system environ via platform-specific APIs.
+//
+// [`Memory`]: memory::Memory
+// [`HashMap`]: std::collections::HashMap
+// [`System`]: system::System
+// [`env::var_os`]: std::env::var_os
 
 pub mod memory;
 #[cfg(feature = "system-env")]
