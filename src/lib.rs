9b0c5e1432570019ca844b6c6794c761dce0487c
Ryan Lopopolo <rjl@hyperbo.la>
2023-02-01 23:28:51 -0800

    Allow `clippy::multiple_crate_versions` lint
    Due to multiple copies of windows-sys in the lockfile.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 #![warn(clippy::cargo)]
+#![allow(clippy::multiple_crate_versions)]
 #![allow(unknown_lints)]
 #![allow(clippy::manual_let_else)]
 #![warn(missing_docs)]
