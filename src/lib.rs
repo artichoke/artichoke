413d328addda67ffb338c9a8acbea91ac2a02d1e
Ryan Lopopolo <rjl@hyperbo.la>
2023-02-20 09:10:13 -0800

    Address clippy lint violations

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![warn(clippy::cargo)]
 #![allow(unknown_lints)]
 #![allow(clippy::manual_let_else)]
+#![allow(clippy::module_name_repetitions)]
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
 #![warn(missing_copy_implementations)]
