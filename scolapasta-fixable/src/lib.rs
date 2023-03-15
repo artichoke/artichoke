2065dbc7d6137265a963e0f246541ad7ded47c77
Ryan Lopopolo <rjl@hyperbo.la>
2023-03-15 15:18:58 +0800

    Add standard crate prelude

--- scolapasta-fixable/src/lib.rs
+++ scolapasta-fixable/src/lib.rs
@@ -1,3 +1,29 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::cargo)]
+#![allow(unknown_lints)]
+#![allow(clippy::manual_let_else)]
+#![warn(missing_docs)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_copy_implementations)]
+#![warn(rust_2018_idioms)]
+#![warn(rust_2021_compatibility)]
+#![warn(trivial_casts, trivial_numeric_casts)]
+#![warn(unused_qualifications)]
+#![warn(variant_size_differences)]
+#![forbid(unsafe_code)]
+// Enable feature callouts in generated documentation:
+// https://doc.rust-lang.org/beta/unstable-book/language-features/doc-cfg.html
+//
+// This approach is borrowed from tokio.
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_alias))]
+
+//! Functions for converting numeric immediates to integer or "fixnum"
+//! immediates.
+
+#![no_std]
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
