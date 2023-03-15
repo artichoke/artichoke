e0ee9c2a6a7e513ee6f1a2b5017554b9321bfdc8
Ryan Lopopolo <rjl@hyperbo.la>
2023-03-15 15:48:29 +0800

    Add RB_FIXABLE impls for i{8,16,32} and u{8,16,32}

--- scolapasta-fixable/src/lib.rs
+++ scolapasta-fixable/src/lib.rs
@@ -24,8 +24,124 @@
 
 #![no_std]
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// The maximum possible value that a fixnum can represent, 63 bits of an
+/// [`i64`].
+///
+/// # C Declaration
+/// ```c
+/// /** Maximum possible value that a fixnum can represent. */
+/// #define RUBY_FIXNUM_MAX  (LONG_MAX / 2)
+/// ```
+pub const RUBY_FIXNUM_MAX: i64 = i64::MAX / 2;
+
+/// The minimum possible value that a fixnum can represent, 63 bits of an
+/// [`i64`].
+///
+/// ```c
+/// /** Minimum possible value that a fixnum can represent. */
+/// #define RUBY_FIXNUM_MIN  (LONG_MIN / 2)
+/// ```
+pub const RUBY_FIXNUM_MIN: i64 = i64::MIN / 2;
+
+mod private {
+    pub trait Sealed {}
+
+    impl Sealed for i8 {}
+    impl Sealed for i16 {}
+    impl Sealed for i32 {}
+
+    impl Sealed for u8 {}
+    impl Sealed for u16 {}
+    impl Sealed for u32 {}
+}
+
+/// Marker trait for numeric values which can be converted to a "fixnum", or
+/// Integer, representation.
+///
+/// A numeric value is fixable if its integral portion can fit within 63 bits of
+/// an [`i64`].
+///
+/// See [`RUBY_FIXNUM_MIN`] and [`RUBY_FIXNUM_MAX`] for more details on the range
+/// of values yielded by implementers of this trait.
+///
+/// This trait is sealed and cannot be implmented outside of this crate.
+pub trait Fixable: private::Sealed {
+    /// Convert a fixable numeric value to its integral part.
+    ///
+    /// This method returns [`None`] if `self` is out of range.
+    fn to_fix(self) -> Option<i64>;
+}
+
+impl Fixable for i8 {
+    fn to_fix(self) -> Option<i64> {
+        Some(self.into())
+    }
+}
+
+impl Fixable for i16 {
+    fn to_fix(self) -> Option<i64> {
+        Some(self.into())
+    }
+}
+
+impl Fixable for i32 {
+    fn to_fix(self) -> Option<i64> {
+        Some(self.into())
+    }
+}
+
+impl Fixable for u8 {
+    fn to_fix(self) -> Option<i64> {
+        Some(self.into())
+    }
+}
+
+impl Fixable for u16 {
+    fn to_fix(self) -> Option<i64> {
+        Some(self.into())
+    }
+}
+
+impl Fixable for u32 {
+    fn to_fix(self) -> Option<i64> {
+        Some(self.into())
+    }
+}
+
+/// Check whether the given numeric is in the range of fixnum.
+///
+/// FIXABLE can be applied to any numeric type. See the implementersof the
+/// [`Fixable`] trait for more details on which numeric types are fixable.
+///
+/// # C Declaration
+///
+/// ```c
+/// /**
+///  * Checks if the passed value is in  range of fixnum, assuming it is a positive
+///  * number.  Can sometimes be useful for C's unsigned integer types.
+///  *
+///  * @internal
+///  *
+///  * FIXABLE can be applied to anything, from double to intmax_t.  The problem is
+///  * double.   On a  64bit system  RUBY_FIXNUM_MAX is  4,611,686,018,427,387,903,
+///  * which is not representable by a double.  The nearest value that a double can
+///  * represent  is   4,611,686,018,427,387,904,  which   is  not   fixable.   The
+///  * seemingly-strange "< FIXNUM_MAX + 1" expression below is due to this.
+///  */
+/// #define RB_POSFIXABLE(_) ((_) <  RUBY_FIXNUM_MAX + 1)
+///
+/// /**
+///  * Checks if the passed value is in  range of fixnum, assuming it is a negative
+///  * number.  This is an implementation of #RB_FIXABLE.  Rarely used stand alone.
+///  */
+/// #define RB_NEGFIXABLE(_) ((_) >= RUBY_FIXNUM_MIN)
+///
+/// /** Checks if the passed value is in  range of fixnum */
+/// #define RB_FIXABLE(_)    (RB_POSFIXABLE(_) && RB_NEGFIXABLE(_))
+/// ```
+#[allow(non_snake_case)] // match MRI macro name
+pub fn RB_FIXABLE<T: Fixable>(x: T) -> bool {
+    x.to_fix().is_some()
 }
 
 #[cfg(test)]
@@ -33,8 +149,68 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn all_i8_are_fixable() {
+        for x in i8::MIN..=i8::MAX {
+            assert!(RB_FIXABLE(x), "{x} should be fixable");
+        }
+    }
+
+    #[test]
+    fn all_i16_are_fixable() {
+        for x in i16::MIN..=i16::MAX {
+            assert!(RB_FIXABLE(x), "{x} should be fixable");
+        }
+    }
+
+    #[test]
+    fn all_neg_i32_are_fixable() {
+        for x in i32::MIN..0 {
+            assert!(RB_FIXABLE(x), "{x} should be fixable");
+        }
+    }
+
+    #[test]
+    fn zero_i32_is_fixable() {
+        assert!(RB_FIXABLE(0_i32), "0 should be fixable");
+    }
+
+    #[test]
+    fn all_pos_i32_are_fixable() {
+        for x in 1..=i32::MAX {
+            assert!(RB_FIXABLE(x), "{x} should be fixable");
+        }
+    }
+
+    #[test]
+    fn all_u8_are_fixable() {
+        for x in u8::MIN..=u8::MAX {
+            assert!(RB_FIXABLE(x), "{x} should be fixable");
+        }
+    }
+
+    #[test]
+    fn all_u16_are_fixable() {
+        for x in u16::MIN..=u16::MAX {
+            assert!(RB_FIXABLE(x), "{x} should be fixable");
+        }
+    }
+
+    #[test]
+    fn all_neg_u32_are_fixable() {
+        for x in u32::MIN..0 {
+            assert!(RB_FIXABLE(x), "{x} should be fixable");
+        }
+    }
+
+    #[test]
+    fn zero_u32_is_fixable() {
+        assert!(RB_FIXABLE(0_u32), "0 should be fixable");
+    }
+
+    #[test]
+    fn all_pos_u32_are_fixable() {
+        for x in 1..=u32::MAX {
+            assert!(RB_FIXABLE(x), "{x} should be fixable");
+        }
     }
 }
