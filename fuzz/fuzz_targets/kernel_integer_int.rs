da7dc149c697b46d3a44162fcc9863f87481b4f1
Ryan Lopopolo <rjl@hyperbo.la>
2023-09-02 10:58:31 -0700

    Address lint violations in fuzz workspace

--- fuzz/fuzz_targets/kernel_integer_int.rs
+++ fuzz/fuzz_targets/kernel_integer_int.rs
@@ -5,12 +5,12 @@ use scolapasta_int_parse::parse;
 fuzz_target!(|data: i64| {
     let data = data.to_string();
     // We're looking for panics, not correctness, so drop the results.
-    drop(parse(&data, None));
+    let _ignored = parse(&data, None);
     for radix in -500..500 {
-        drop(parse(&data, Some(radix)));
+        let _ignored = parse(&data, Some(radix));
     }
-    drop(parse(&data, Some(i32::MAX.into())));
-    drop(parse(&data, Some(i32::MIN.into())));
-    drop(parse(&data, Some(i64::MAX)));
-    drop(parse(&data, Some(i64::MAX)));
+    let _ignored = parse(&data, Some(i32::MAX.into()));
+    let _ignored = parse(&data, Some(i32::MIN.into()));
+    let _ignored = parse(&data, Some(i64::MAX));
+    let _ignored = parse(&data, Some(i64::MAX));
 });
